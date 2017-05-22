e4fbddb310978db934c5ad261502e33d6da663bb
Juergen Kappler <jurgen.kappler@liferay.com>
2017-05-22 10:03:08 +0200

    LPS-72208 Create new instance configuration for asset publisher since and move the configuration values that should be at portlet instance level

---
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/configuration/AssetPublisherPortletInstanceConfiguration.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.web.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+
+/**
+ * @author Juergen Kappler
+ */
+@ExtendedObjectClassDefinition(
+	category = "web-experience",
+	scope = ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE
+)
+@Meta.OCD(
+	id = "com.liferay.asset.publisher.web.configuration.AssetPublisherPortletInstanceConfiguration",
+	localization = "content/Language",
+	name = "asset.publisher.portlet.instance.configuration.name"
+)
+public interface AssetPublisherPortletInstanceConfiguration {
+
+	/**
+	 * Set the name of the display style which will be used by default.
+	 *
+	 * @return default display style.
+	 */
+	@Meta.AD(
+		deflt = "abstracts",
+		description = "default.display.style.key.description", required = false
+	)
+	public String defaultDisplayStyle();
+
+	/**
+	 * Input a list of comma delimited display styles that will be available in
+	 * the configuration screen of the Asset Publisher portlet.
+	 *
+	 * @return available display styles.
+	 */
+	@Meta.AD(
+		deflt = "table|title-list|abstracts|full-content",
+		description = "display.styles.key.description", required = false
+	)
+	public String[] displayStyles();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/asset/publisher/web/portlet/email/dependencies/email_asset_entry_added_body.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap emailAssetEntryAddedBody();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean emailAssetEntryAddedEnabled();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/asset/publisher/web/portlet/email/dependencies/email_asset_entry_added_subject.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap emailAssetEntryAddedSubject();
+
+	@Meta.AD(deflt = "", required = false)
+	public String emailFromAddress();
+
+	@Meta.AD(deflt = "", required = false)
+	public String emailFromName();
+
+	/**
+	 * Set this to true to search assets in Asset Publisher from the index. Set
+	 * this to false to search assets in Asset Publisher from the database.
+	 *
+	 * @return <code>true</code> search with index is enabled.
+	 */
+	@Meta.AD(
+		deflt = "true", description = "search.with.index.key.description",
+		required = false
+	)
+	public boolean searchWithIndex();
+
+	/**
+	 * Set this to true to allow users to configure Asset Publisher, Most Viewed
+	 * Assets, and Highest Rated Assets to skip the permissions checking on the
+	 * displayed assets. Enabling this property will allow regular users to view
+	 * assets that they do not have permission to view.
+	 *
+	 * @return <code>true</code> if permission checking is configurable.
+	 */
+	@Meta.AD(
+		deflt = "false",
+		description = "permission.checking.configurable.key.description",
+		required = false
+	)
+	public boolean permissionCheckingConfigurable();
+
+}
--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/configuration/AssetPublisherWebConfiguration.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/configuration/AssetPublisherWebConfiguration.java
@@ -17,7 +17,6 @@ package com.liferay.asset.publisher.web.configuration;
 import aQute.bnd.annotation.metatype.Meta;
 
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
-import com.liferay.portal.kernel.settings.LocalizedValuesMap;
 
 /**
  * @author Pavel Savinov
@@ -42,29 +41,6 @@ public interface AssetPublisherWebConfiguration {
 		required = false
 	)
 	public int checkInterval();
-
-	/**
-	 * Set the name of the display style which will be used by default.
-	 *
-	 * @return default display style.
-	 */
-	@Meta.AD(
-		deflt = "abstracts",
-		description = "default.display.style.key.description", required = false
-	)
-	public String defaultDisplayStyle();
-
-	/**
-	 * Input a list of comma delimited display styles that will be available in
-	 * the configuration screen of the Asset Publisher portlet.
-	 *
-	 * @return available display styles.
-	 */
-	@Meta.AD(
-		deflt = "table|title-list|abstracts|full-content",
-		description = "display.styles.key.description", required = false
-	)
-	public String[] displayStyles();
 
 	/**
 	 * Set this to true to enable exporting contents related to asset entries
@@ -103,27 +79,6 @@ public interface AssetPublisherWebConfiguration {
 	)
 	public int dynamicSubscriptionLimit();
 
-	@Meta.AD(
-		deflt = "${resource:com/liferay/asset/publisher/web/portlet/email/dependencies/email_asset_entry_added_body.tmpl}",
-		required = false
-	)
-	public LocalizedValuesMap emailAssetEntryAddedBody();
-
-	@Meta.AD(deflt = "true", required = false)
-	public boolean emailAssetEntryAddedEnabled();
-
-	@Meta.AD(
-		deflt = "${resource:com/liferay/asset/publisher/web/portlet/email/dependencies/email_asset_entry_added_subject.tmpl}",
-		required = false
-	)
-	public LocalizedValuesMap emailAssetEntryAddedSubject();
-
-	@Meta.AD(deflt = "", required = false)
-	public String emailFromAddress();
-
-	@Meta.AD(deflt = "", required = false)
-	public String emailFromName();
-
 	/**
 	 * Set this to true to enable exporting contents related to asset entries
 	 * for manual selection.
@@ -135,32 +90,5 @@ public interface AssetPublisherWebConfiguration {
 		required = false
 	)
 	public boolean manualExportEnabled();
-
-	/**
-	 * Set this to true to allow users to configure Asset Publisher, Most Viewed
-	 * Assets, and Highest Rated Assets to skip the permissions checking on the
-	 * displayed assets. Enabling this property will allow regular users to view
-	 * assets that they do not have permission to view.
-	 *
-	 * @return <code>true</code> if permission checking is configurable.
-	 */
-	@Meta.AD(
-		deflt = "false",
-		description = "permission.checking.configurable.key.description",
-		required = false
-	)
-	public boolean permissionCheckingConfigurable();
-
-	/**
-	 * Set this to true to search assets in Asset Publisher from the index. Set
-	 * this to false to search assets in Asset Publisher from the database.
-	 *
-	 * @return <code>true</code> search with index is enabled.
-	 */
-	@Meta.AD(
-		deflt = "true", description = "search.with.index.key.description",
-		required = false
-	)
-	public boolean searchWithIndex();
 
 }
