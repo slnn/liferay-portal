75856693af869710155f9f99ceb4c86173596c68
Lily.chi <lily.chi@liferay.com>
2018-01-04 13:18:24 +0800

    Revert "LPS-73189 Using a single regex to improve performance"
    This reverts commit f9f14999c180b24c371aca0a42d34aae2e6615b5.

--- modules/apps/collaboration/upload/upload-web/src/main/java/com/liferay/upload/web/internal/ImageAttachmentElementHandler.java
+++ modules/apps/collaboration/upload/upload-web/src/main/java/com/liferay/upload/web/internal/ImageAttachmentElementHandler.java
@@ -24,6 +24,8 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.upload.AttachmentElementHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,50 +52,53 @@ public class ImageAttachmentElementHandler implements AttachmentElementHandler {
 			UnsafeFunction<FileEntry, FileEntry, PortalException> saveFile)
 		throws PortalException {
 
-		Matcher matcher = _TEMP_ATTACHMENT_PATTERN.matcher(content);
-
-		StringBuffer sb = new StringBuffer(content.length());
-
-		while (matcher.find()) {
-			FileEntry tempAttachmentFileEntry = _getFileEntry(matcher);
+		for (FileEntry tempAttachmentFileEntry :
+				_getAttachmentFileEntries(content)) {
 
 			FileEntry attachmentFileEntry = saveFile.apply(
 				tempAttachmentFileEntry);
 
-			matcher.appendReplacement(
-				sb,
-				Matcher.quoteReplacement(getElementTag(attachmentFileEntry)));
+			StringBundler sb = new StringBundler(8);
+
+			sb.append("<\\s*?img");
+			sb.append(_ATTRIBUTE_LIST_REGEXP);
+			sb.append(EditorConstants.ATTRIBUTE_DATA_IMAGE_ID);
+			sb.append("\\s*?=\\s*?\"");
+			sb.append(tempAttachmentFileEntry.getFileEntryId());
+			sb.append("\"");
+			sb.append(_ATTRIBUTE_LIST_REGEXP);
+			sb.append("/>");
+
+			content = content.replaceAll(
+				sb.toString(), getElementTag(attachmentFileEntry));
 		}
 
-		matcher.appendTail(sb);
-
-		return sb.toString();
+		return content;
 	}
 
-	private FileEntry _getFileEntry(Matcher matcher) throws PortalException {
-		long fileEntryId = GetterUtil.getLong(matcher.group(1));
+	private List<FileEntry> _getAttachmentFileEntries(String content)
+		throws PortalException {
 
-		return PortletFileRepositoryUtil.getPortletFileEntry(fileEntryId);
+		List<FileEntry> tempAttachmentFileEntries = new ArrayList<>();
+
+		Matcher matcher = _ATTRIBUTE_DATA_IMAGE_ID_PATTERN.matcher(content);
+
+		while (matcher.find()) {
+			long fileEntryId = GetterUtil.getLong(matcher.group(1));
+
+			FileEntry tempFileEntry =
+				PortletFileRepositoryUtil.getPortletFileEntry(fileEntryId);
+
+			tempAttachmentFileEntries.add(tempFileEntry);
+		}
+
+		return tempAttachmentFileEntries;
 	}
+
+	private static final Pattern _ATTRIBUTE_DATA_IMAGE_ID_PATTERN =
+		Pattern.compile(EditorConstants.ATTRIBUTE_DATA_IMAGE_ID + "=.(\\d+)");
 
 	private static final String _ATTRIBUTE_LIST_REGEXP =
-		"(?:\\s*?\\w+\\s*?=\\s*?\"[^\"]*\")*?\\s*?";
-
-	private static final Pattern _TEMP_ATTACHMENT_PATTERN;
-
-	static {
-		StringBundler sb = new StringBundler(8);
-
-		sb.append("<\\s*?img");
-		sb.append(_ATTRIBUTE_LIST_REGEXP);
-		sb.append(EditorConstants.ATTRIBUTE_DATA_IMAGE_ID);
-		sb.append("\\s*?=\\s*?\"");
-		sb.append("([^\"]*)");
-		sb.append("\"");
-		sb.append(_ATTRIBUTE_LIST_REGEXP);
-		sb.append("/>");
-
-		_TEMP_ATTACHMENT_PATTERN = Pattern.compile(sb.toString());
-	}
+		"(\\s*?\\w+\\s*?=\\s*?\"[^\"]*\")*?\\s*?";
 
 }
