9b57b4814a4c397ba7bf808a5d104f07d48b1219
Lily.chi <lily.chi@liferay.com>
2018-01-04 13:18:35 +0800

    Revert "LPS-73189 Compiling the pattern just once"
    This reverts commit 6672459826aa7d9b66389ef23db9eeaea05e1a08.

--- modules/apps/collaboration/upload/upload-web/src/main/java/com/liferay/upload/web/internal/ImageAttachmentElementHandler.java
+++ modules/apps/collaboration/upload/upload-web/src/main/java/com/liferay/upload/web/internal/ImageAttachmentElementHandler.java
@@ -81,7 +81,10 @@ public class ImageAttachmentElementHandler implements AttachmentElementHandler {
 
 		List<FileEntry> tempAttachmentFileEntries = new ArrayList<>();
 
-		Matcher matcher = _ATTRIBUTE_DATA_IMAGE_ID_PATTERN.matcher(content);
+		Pattern pattern = Pattern.compile(
+			EditorConstants.ATTRIBUTE_DATA_IMAGE_ID + "=.(\\d+)");
+
+		Matcher matcher = pattern.matcher(content);
 
 		while (matcher.find()) {
 			long fileEntryId = GetterUtil.getLong(matcher.group(1));
@@ -94,9 +97,6 @@ public class ImageAttachmentElementHandler implements AttachmentElementHandler {
 
 		return tempAttachmentFileEntries;
 	}
-
-	private static final Pattern _ATTRIBUTE_DATA_IMAGE_ID_PATTERN =
-		Pattern.compile(EditorConstants.ATTRIBUTE_DATA_IMAGE_ID + "=.(\\d+)");
 
 	private static final String _ATTRIBUTE_LIST_REGEXP =
 		"(\\s*?\\w+\\s*?=\\s*?\"[^\"]*\")*?\\s*?";
