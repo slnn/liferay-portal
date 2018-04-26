c670a694f77dc198590fd772a71e78373e4ff254
Lily.Chi <lily.chi@liferay.com>
2018-04-26 10:04:41 +0800

    Revert "LPS-74171 Set session.cookie.use.full.hostname to true only for Tomcat because Tomcat 8.5 has adopted RFC 6265 as default"
    This reverts commit de4f2e6f8dd0f0d18b53c76a49632e646c85ebc0.

--- portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -16,7 +16,6 @@ package com.liferay.portal.kernel.util;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.configuration.Filter;
 import com.liferay.portal.kernel.exception.CookieNotSupportedException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -332,9 +331,7 @@ public class CookieKeys {
 
 	private static final boolean _SESSION_COOKIE_USE_FULL_HOSTNAME =
 		GetterUtil.getBoolean(
-			PropsUtil.get(
-				PropsKeys.SESSION_COOKIE_USE_FULL_HOSTNAME,
-				new Filter(ServerDetector.getServerId())));
+			PropsUtil.get(PropsKeys.SESSION_COOKIE_USE_FULL_HOSTNAME));
 
 	private static final boolean _SESSION_ENABLE_PERSISTENT_COOKIES =
 		GetterUtil.getBoolean(
