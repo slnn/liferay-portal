381dca72da475619888a110848d4c595bc5d8e0f
Lily.Chi <lily.chi@liferay.com>
2018-06-04 17:31:07 +0800

    Revert "LPS-64398 portal-search-test-util: use new AssetTagNamesFacetFactory"
    This reverts commit 64dcfed60fadbc3dac8b2ddbdd000b9cb6a462ae.

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/facet/BaseAssetTagNamesFacetTestCase.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/facet/BaseAssetTagNamesFacetTestCase.java
@@ -16,8 +16,7 @@ package com.liferay.portal.search.test.util.facet;
 
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.search.facet.tag.AssetTagNamesFacetFactory;
-import com.liferay.portal.search.internal.facet.tag.AssetTagNamesFacetFactoryImpl;
+import com.liferay.portal.search.internal.facet.tag.AssetTagNamesFacet;
 import com.liferay.portal.search.test.util.indexing.QueryContributor;
 import com.liferay.portal.search.test.util.indexing.QueryContributors;
 
@@ -33,12 +32,8 @@ public abstract class BaseAssetTagNamesFacetTestCase extends BaseFacetTestCase {
 			QueryContributor queryContributor, List<String> expectedTerms)
 		throws Exception {
 
-		AssetTagNamesFacetFactory assetTagNamesFacetFactory =
-			new AssetTagNamesFacetFactoryImpl();
-
 		assertFacet(
-			searchContext -> initFacet(
-				assetTagNamesFacetFactory.newInstance(searchContext)),
+			searchContext -> initFacet(new AssetTagNamesFacet(searchContext)),
 			queryContributor, expectedTerms);
 	}
 
