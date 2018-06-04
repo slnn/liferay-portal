9bc6de0f6073e911f85b4e3e5eb33521022c3c42
Lily.Chi <lily.chi@liferay.com>
2018-06-04 17:31:56 +0800

    Revert "LPS-64398 portal-search: Update AssetTagNamesFacetFactory to use improved Facet with selections"
    This reverts commit 5c341411595fc3ea0318a25cad309a885bbf0441.

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/tag/AssetTagNamesFacetFactoryImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/tag/AssetTagNamesFacetFactoryImpl.java
@@ -14,12 +14,10 @@
 
 package com.liferay.portal.search.internal.facet.tag;
 
-import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.search.facet.Facet;
-import com.liferay.portal.search.facet.FacetFactory;
+import com.liferay.portal.kernel.search.facet.Facet;
+import com.liferay.portal.kernel.search.facet.util.FacetFactory;
 import com.liferay.portal.search.facet.tag.AssetTagNamesFacetFactory;
-import com.liferay.portal.search.internal.facet.FacetImpl;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -31,16 +29,16 @@ import org.osgi.service.component.annotations.Component;
 	service = {AssetTagNamesFacetFactory.class, FacetFactory.class}
 )
 public class AssetTagNamesFacetFactoryImpl
-	implements AssetTagNamesFacetFactory {
+	implements AssetTagNamesFacetFactory, FacetFactory {
 
 	@Override
 	public String getFacetClassName() {
-		return Field.ASSET_TAG_NAMES + ".raw";
+		return AssetTagNamesFacetFactory.class.getName();
 	}
 
 	@Override
 	public Facet newInstance(SearchContext searchContext) {
-		return new FacetImpl(Field.ASSET_TAG_NAMES + ".raw", searchContext);
+		return new AssetTagNamesFacet(searchContext);
 	}
 
 }
