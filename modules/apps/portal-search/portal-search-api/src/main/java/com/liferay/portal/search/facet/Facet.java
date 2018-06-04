ea2ff75575f8173d4e9aaa958d8c54e196b3ff12
Lily.Chi <lily.chi@liferay.com>
2018-06-04 17:33:59 +0800

    Revert "LPS-64398 portal-search-api: Facet: add getter for selections"
    This reverts commit fa742f46e186cc4437f8651b3018f9775170e5c3.

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/facet/Facet.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/facet/Facet.java
@@ -22,8 +22,6 @@ import aQute.bnd.annotation.ProviderType;
 @ProviderType
 public interface Facet extends com.liferay.portal.kernel.search.facet.Facet {
 
-	public String[] getSelections();
-
 	public void select(String... selections);
 
 	public void setAggregationName(String aggregationName);
