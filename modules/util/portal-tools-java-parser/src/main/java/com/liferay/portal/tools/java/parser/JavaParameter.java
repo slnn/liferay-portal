6618ca10dca7237041c999fcb48612e4c31b98da
Lily.Chi <lily.chi@liferay.com>
2019-03-29 15:15:29 +0800

    Revert "LPS-86806 Fix logic"
    This reverts commit 2c4dbb2847b4642bf4982be7a98f1537abb5cc43.

--- modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaParameter.java
+++ modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaParameter.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.tools.java.parser;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.ListUtil;
 
 import java.util.List;
 
@@ -45,12 +46,12 @@ public class JavaParameter extends BaseJavaTerm {
 
 		sb.append(prefix);
 
-		for (JavaAnnotation javaAnnotation : _javaAnnotations) {
-			indent = append(sb, javaAnnotation, indent, "", " ", maxLineLength);
+		if (ListUtil.isNotEmpty(_javaAnnotations)) {
+			appendSingleLine(sb, _javaAnnotations, " ", "", " ", maxLineLength);
 		}
 
-		for (JavaSimpleValue modifier : _modifiers) {
-			indent = append(sb, modifier, indent, "", " ", maxLineLength);
+		if (ListUtil.isNotEmpty(_modifiers)) {
+			appendSingleLine(sb, _modifiers, " ", "", " ", maxLineLength);
 		}
 
 		indent = append(sb, _javaType, indent, "", " ", maxLineLength, false);
