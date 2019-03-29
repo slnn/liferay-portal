b29df06b35e904d2096616ba0bdca2598b802219
Lily.Chi <lily.chi@liferay.com>
2019-03-29 15:26:44 +0800

    Revert "LPS-91056 SF"
    This reverts commit 4b88a3646e6ffb1c9ebd14418d9e07853d655cea.

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/events/SegmentsServicePreAction.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/events/SegmentsServicePreAction.java
@@ -91,6 +91,7 @@ public class SegmentsServicePreAction extends Action {
 		long[] segmentsExperienceIds = null;
 
 		Layout layout = themeDisplay.getLayout();
+
 		long classNameId = _classNameLocalService.getClassNameId(
 			Layout.class.getName());
 
@@ -106,6 +107,7 @@ public class SegmentsServicePreAction extends Action {
 				segmentsExperienceIds = _getSegmentsExperienceIds(
 					layout.getGroupId(), segmentsEntryIds, classNameId,
 					layout.getPrimaryKey());
+
 			}
 			catch (PortalException pe) {
 				if (_log.isWarnEnabled()) {
