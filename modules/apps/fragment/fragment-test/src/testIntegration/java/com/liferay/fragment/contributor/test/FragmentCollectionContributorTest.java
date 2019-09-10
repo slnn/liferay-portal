a93fe8ab683031525de7aa8a2a32acd7496da053
lily.chi <lily.chi@liferay.com>
2019-09-10 18:06:52 +0800

    Revert "LPS-99947 Adds new test to check if a the fragments of a contributor are registered correctly"
    This reverts commit c9254438f1d35142e6883b09e2b6a4ce86bc40d7.

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
@@ -15,22 +15,18 @@
 package com.liferay.fragment.contributor.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.contributor.FragmentCollectionContributor;
 import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
 import com.liferay.fragment.model.FragmentEntry;
-import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -52,7 +48,7 @@ public class FragmentCollectionContributorTest {
 		new LiferayIntegrationTestRule();
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
@@ -66,26 +62,7 @@ public class FragmentCollectionContributorTest {
 	}
 
 	@Test
-	public void testRegisterContributedFragmentEntries() {
-		Map<String, FragmentEntry> fragmentEntries =
-			_fragmentCollectionContributorTracker.getFragmentEntries();
-
-		Assert.assertNotNull(
-			fragmentEntries.get(
-				TestFragmentCollectionContributor.
-					TEST_COMPONENT_FRAGMENT_ENTRY));
-		Assert.assertNotNull(
-			fragmentEntries.get(
-				TestFragmentCollectionContributor.TEST_SECTION_FRAGMENT_ENTRY));
-
-		Assert.assertNull(
-			fragmentEntries.get(
-				TestFragmentCollectionContributor.
-					TEST_UNSUPORTED_FRAGMENT_ENTRY));
-	}
-
-	@Test
-	public void testRegisterFragmentCollectionContributor() {
+	public void testRegisterFragmentCollectionContributor() throws Exception {
 		_fragmentCollectionContributorTracker.getFragmentCollectionContributor(
 			TestFragmentCollectionContributor.TEST_FRAGMENT_COLLECTION_KEY);
 
@@ -106,17 +83,8 @@ public class FragmentCollectionContributorTest {
 	private static class TestFragmentCollectionContributor
 		implements FragmentCollectionContributor {
 
-		public static final String TEST_COMPONENT_FRAGMENT_ENTRY =
-			"test-component-fragment-entry";
-
 		public static final String TEST_FRAGMENT_COLLECTION_KEY =
 			"test-fragment-collection-contributor";
-
-		public static final String TEST_SECTION_FRAGMENT_ENTRY =
-			"test-section-fragment-entry";
-
-		public static final String TEST_UNSUPORTED_FRAGMENT_ENTRY =
-			"test-unsuported-fragment-entry";
 
 		@Override
 		public String getFragmentCollectionKey() {
@@ -125,44 +93,12 @@ public class FragmentCollectionContributorTest {
 
 		@Override
 		public List<FragmentEntry> getFragmentEntries(int type) {
-			List<FragmentEntry> fragmentEntries = new ArrayList<>();
-
-			if (type == FragmentConstants.TYPE_COMPONENT) {
-				fragmentEntries.add(
-					_getFragmentEntry(TEST_COMPONENT_FRAGMENT_ENTRY, type));
-			}
-			else if (type == FragmentConstants.TYPE_SECTION) {
-				fragmentEntries.add(
-					_getFragmentEntry(TEST_SECTION_FRAGMENT_ENTRY, type));
-			}
-			else {
-				fragmentEntries.add(
-					_getFragmentEntry(
-						TEST_UNSUPORTED_FRAGMENT_ENTRY,
-						RandomTestUtil.randomInt()));
-			}
-
-			return fragmentEntries;
+			return Collections.emptyList();
 		}
 
 		@Override
 		public String getName() {
 			return "Test Fragment Collection Contributor";
-		}
-
-		private FragmentEntry _getFragmentEntry(String key, int type) {
-			FragmentEntry fragmentEntry =
-				FragmentEntryLocalServiceUtil.createFragmentEntry(0L);
-
-			fragmentEntry.setFragmentEntryKey(key);
-			fragmentEntry.setName(RandomTestUtil.randomString());
-			fragmentEntry.setCss(null);
-			fragmentEntry.setHtml(RandomTestUtil.randomString());
-			fragmentEntry.setJs(null);
-			fragmentEntry.setConfiguration(null);
-			fragmentEntry.setType(type);
-
-			return fragmentEntry;
 		}
 
 	}
