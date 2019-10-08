2646977c01ca9bd1a1ae905dd97ef46e86af6444
lily.chi <lily.chi@liferay.com>
2019-10-08 14:07:46 +0800

    Revert "LPS-93762 as used"
    This reverts commit e8ff1ad4be3c0a8ff64efa3477c4b44f094d9357.

--- modules/apps/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/portlet/action/EditMVCActionCommand.java
+++ modules/apps/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/portlet/action/EditMVCActionCommand.java
@@ -103,15 +103,16 @@ public class EditMVCActionCommand extends BaseMVCActionCommand {
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
+		Map<String, Serializable> taskContextMap = new HashMap<>();
+
+		String className = ParamUtil.getString(actionRequest, "className");
+
 		long[] companyIds = _portalInstancesLocalService.getCompanyIds();
 
 		if (!ArrayUtil.contains(companyIds, CompanyConstants.SYSTEM)) {
 			companyIds = ArrayUtil.append(
 				new long[] {CompanyConstants.SYSTEM}, companyIds);
 		}
-
-		String className = ParamUtil.getString(actionRequest, "className");
-		Map<String, Serializable> taskContextMap = new HashMap<>();
 
 		if (!ParamUtil.getBoolean(actionRequest, "blocking")) {
 			_indexWriterHelper.reindex(
