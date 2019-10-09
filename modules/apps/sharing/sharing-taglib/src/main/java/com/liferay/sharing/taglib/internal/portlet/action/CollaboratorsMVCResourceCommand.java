ade36e0c95f09977a67d176fdc74ce40e6aad917
lily.chi <lily.chi@liferay.com>
2019-10-09 13:30:54 +0800

    Revert "LPS-101411 Making it static to use it in the tag and avoid creating another immediate component"
    This reverts commit a066887fa00bce024d8e30e946eb49f061a8d935.

--- modules/apps/sharing/sharing-taglib/src/main/java/com/liferay/sharing/taglib/internal/portlet/action/CollaboratorsMVCResourceCommand.java
+++ modules/apps/sharing/sharing-taglib/src/main/java/com/liferay/sharing/taglib/internal/portlet/action/CollaboratorsMVCResourceCommand.java
@@ -26,7 +26,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.sharing.configuration.SharingConfiguration;
 import com.liferay.sharing.configuration.SharingConfigurationFactory;
 import com.liferay.sharing.constants.SharingPortletKeys;
-import com.liferay.sharing.taglib.util.CollaboratorsUtil;
+import com.liferay.sharing.taglib.util.CollaboratorsHelper;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -81,12 +81,15 @@ public class CollaboratorsMVCResourceCommand extends BaseMVCResourceCommand {
 
 		JSONPortletResponseUtil.writeJSON(
 			resourceRequest, resourceResponse,
-			CollaboratorsUtil.getCollaboratorsJSONObject(
+			_collaboratorsHelper.getCollaboratorsJSONObject(
 				classNameId, classPK, themeDisplay));
 	}
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private CollaboratorsHelper _collaboratorsHelper;
 
 	@Reference
 	private Portal _portal;
