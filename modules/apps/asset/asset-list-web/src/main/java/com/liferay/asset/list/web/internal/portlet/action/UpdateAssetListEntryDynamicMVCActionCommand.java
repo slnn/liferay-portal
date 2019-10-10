36611d1781fe4e1761bcfddccf5821c980edc418
lily.chi <lily.chi@liferay.com>
2019-10-10 13:52:56 +0800

    Revert "LPS-101228 Implements new filter for keywords for Asset List"
    This reverts commit a89bb46ae51d59393eb3a0b8c29d8264a81bb511.

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/UpdateAssetListEntryDynamicMVCActionCommand.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/UpdateAssetListEntryDynamicMVCActionCommand.java
@@ -116,10 +116,6 @@ public class UpdateAssetListEntryDynamicMVCActionCommand
 			values = ParamUtil.getStringValues(
 				actionRequest, "queryTagNames" + index);
 		}
-		else if (name.equals("keywords")) {
-			values = ParamUtil.getStringValues(
-				actionRequest, "keywords" + index);
-		}
 		else {
 			values = ParamUtil.getStringValues(
 				actionRequest, "queryCategoryIds" + index);
