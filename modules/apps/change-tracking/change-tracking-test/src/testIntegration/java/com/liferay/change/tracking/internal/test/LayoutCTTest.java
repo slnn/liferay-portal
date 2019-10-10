5b50ca36a05bdf373a9cbc5ed20240ca636e7319
lily.chi <lily.chi@liferay.com>
2019-10-10 14:59:27 +0800

    Revert "LPS-99915 Use DeleteAfterTestRun"
    This reverts commit a753e1855ed8e4f241f8545a7e61977870903c8f.

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/LayoutCTTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/LayoutCTTest.java
@@ -51,6 +51,7 @@ import org.apache.log4j.spi.ThrowableInformation;
 
 import org.hibernate.util.JDBCExceptionReporter;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -86,6 +87,11 @@ public class LayoutCTTest {
 			Layout.class);
 
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_ctCollectionLocalService.deleteCTCollection(_ctCollection);
 	}
 
 	@Test
@@ -479,7 +485,6 @@ public class LayoutCTTest {
 	@Inject
 	private static LayoutLocalService _layoutLocalService;
 
-	@DeleteAfterTestRun
 	private CTCollection _ctCollection;
 
 	@Inject
