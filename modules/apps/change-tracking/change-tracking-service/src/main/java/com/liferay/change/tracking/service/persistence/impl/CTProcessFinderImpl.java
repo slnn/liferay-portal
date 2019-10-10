6793ce41fc1592cedc16ee93f3fa7b1b46dad229
lily.chi <lily.chi@liferay.com>
2019-10-10 14:42:59 +0800

    Revert "LPS-100263 Clean up and fix finder impl."
    This reverts commit 6de0420f094a00b265ef0fd2af74fca3309a2a92.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
@@ -31,6 +31,8 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -59,8 +61,8 @@ public class CTProcessFinderImpl
 			String sql = _customSQL.get(getClass(), FIND_BY_C_U_N_D_S);
 
 			if (userId <= 0) {
-				sql = StringUtil.removeSubstring(
-					sql, "AND (CTProcess.userId = ?)");
+				sql = StringUtil.replace(
+					sql, "(CTProcess.userId = ?) AND", StringPool.BLANK);
 			}
 
 			String[] names = _customSQL.keywords(
@@ -68,14 +70,10 @@ public class CTProcessFinderImpl
 			String[] descriptions = _customSQL.keywords(
 				keywords, true, WildcardMode.SURROUND);
 
-			boolean keywordsEmpty = Validator.isBlank(keywords);
+			boolean keywordsEmpty = _isKeywordsEmpty(names);
 
 			if (keywordsEmpty) {
-				int x = sql.indexOf("AND (BackgroundTask.status = ?)") + 32;
-
-				int y = sql.indexOf("ORDER BY", x);
-
-				sql = sql.substring(0, x) + sql.substring(y);
+				sql = _replaceKeywordConditionsWithBlank(sql);
 			}
 			else {
 				sql = _customSQL.replaceKeywords(
@@ -88,8 +86,8 @@ public class CTProcessFinderImpl
 			}
 
 			if (status == WorkflowConstants.STATUS_ANY) {
-				sql = StringUtil.removeSubstring(
-					sql, "AND (BackgroundTask.status = ?)");
+				sql = StringUtil.replace(
+					sql, "AND (BackgroundTask.status = ?)", StringPool.BLANK);
 			}
 
 			sql = _customSQL.replaceOrderBy(sql, orderByComparator);
@@ -125,6 +123,31 @@ public class CTProcessFinderImpl
 			closeSession(session);
 		}
 	}
+
+	private boolean _isKeywordsEmpty(String[] keywords) {
+		boolean emptyKeywords = false;
+		boolean nonEmptyKeywords = false;
+
+		for (String keyword : keywords) {
+			emptyKeywords = Validator.isNull(keyword);
+			nonEmptyKeywords = Validator.isNotNull(keyword);
+		}
+
+		if (emptyKeywords && !nonEmptyKeywords) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private String _replaceKeywordConditionsWithBlank(String sql) {
+		Matcher matcher = _pattern.matcher(sql);
+
+		return matcher.replaceAll(StringPool.BLANK);
+	}
+
+	private static final Pattern _pattern = Pattern.compile(
+		"AND\\s*\\(\\s*\\([A-Za-z\\(\\)\\.\\s\\?\\[\\]$_]*\\)\\s*\\)");
 
 	@Reference
 	private CustomSQL _customSQL;
