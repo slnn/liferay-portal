e66bfe340121427ba7b897cf63d73c573d5e3804
lily.chi <lily.chi@liferay.com>
2019-10-10 14:43:29 +0800

    Revert "LPS-100263 Remove old change tracking system."
    This reverts commit e893ed1f44f635c421ba916527b52c5da77c492f.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTProcessLocalServiceImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTProcessLocalServiceImpl.java
@@ -75,6 +75,7 @@ public class CTProcessLocalServiceImpl extends CTProcessLocalServiceBaseImpl {
 
 		taskContextMap.put("ctCollectionId", ctCollectionId);
 		taskContextMap.put("ctProcessId", ctProcessId);
+		taskContextMap.put("ignoreCollision", ignoreCollision);
 
 		BackgroundTask backgroundTask =
 			_backgroundTaskLocalService.addBackgroundTask(
