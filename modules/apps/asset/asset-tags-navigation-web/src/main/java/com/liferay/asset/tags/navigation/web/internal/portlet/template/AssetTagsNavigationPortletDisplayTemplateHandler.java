baa97f1f882a4ce4fde07193fb73197933609713
lily.chi <lily.chi@liferay.com>
2019-10-10 14:05:19 +0800

    Revert "LPS-102793 Remove unused AssetTagStats model."
    This reverts commit de62c699187d1d5a3af4d1d0f85e64adeb4467a2.

--- modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/template/AssetTagsNavigationPortletDisplayTemplateHandler.java
+++ modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/template/AssetTagsNavigationPortletDisplayTemplateHandler.java
@@ -17,6 +17,7 @@ package com.liferay.asset.tags.navigation.web.internal.portlet.template;
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.asset.kernel.service.AssetTagService;
+import com.liferay.asset.tag.stats.service.AssetTagStatsLocalService;
 import com.liferay.asset.tags.navigation.constants.AssetTagsNavigationPortletKeys;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
@@ -91,7 +92,8 @@ public class AssetTagsNavigationPortletDisplayTemplateHandler
 		assetServicesTemplateVariableGroup.setAutocompleteEnabled(false);
 
 		assetServicesTemplateVariableGroup.addServiceLocatorVariables(
-			AssetTagLocalService.class, AssetTagService.class);
+			AssetTagLocalService.class, AssetTagService.class,
+			AssetTagStatsLocalService.class);
 
 		templateVariableGroups.put(
 			assetServicesTemplateVariableGroup.getLabel(),
