7075e018f09f7f803948f9c67721954432c12ed8
lily.chi <lily.chi@liferay.com>
2019-10-10 15:02:30 +0800

    Revert "LPS-102437 Build Service."
    This reverts commit 01dd12d55729c10b0e42fd7c748879edc7c8ef6c.

--- portal-kernel/src/com/liferay/asset/kernel/model/AssetVocabularySoap.java
+++ portal-kernel/src/com/liferay/asset/kernel/model/AssetVocabularySoap.java
@@ -31,7 +31,6 @@ public class AssetVocabularySoap implements Serializable {
 	public static AssetVocabularySoap toSoapModel(AssetVocabulary model) {
 		AssetVocabularySoap soapModel = new AssetVocabularySoap();
 
-		soapModel.setMvccVersion(model.getMvccVersion());
 		soapModel.setUuid(model.getUuid());
 		soapModel.setExternalReferenceCode(model.getExternalReferenceCode());
 		soapModel.setVocabularyId(model.getVocabularyId());
@@ -103,14 +102,6 @@ public class AssetVocabularySoap implements Serializable {
 
 	public void setPrimaryKey(long pk) {
 		setVocabularyId(pk);
-	}
-
-	public long getMvccVersion() {
-		return _mvccVersion;
-	}
-
-	public void setMvccVersion(long mvccVersion) {
-		_mvccVersion = mvccVersion;
 	}
 
 	public String getUuid() {
@@ -225,7 +216,6 @@ public class AssetVocabularySoap implements Serializable {
 		_lastPublishDate = lastPublishDate;
 	}
 
-	private long _mvccVersion;
 	private String _uuid;
 	private String _externalReferenceCode;
 	private long _vocabularyId;
