c840551a03d154a28cc45ed6a400dd01dcc35b7e
lily.chi <lily.chi@liferay.com>
2019-10-10 13:52:46 +0800

    Revert "LPS-101228 Adds new methods to allow filtering by keywords, like categories and tags"
    This reverts commit 45e11d8359f051c0279cc2349670a82f4936728b.

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -366,26 +366,13 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			String[] overrideAllAssetTagNames)
 		throws PortalException {
 
-		return getAssetEntryQuery(
-			portletPreferences, groupId, layout, overrideAllAssetCategoryIds,
-			null, overrideAllAssetTagNames);
-	}
-
-	@Override
-	public AssetEntryQuery getAssetEntryQuery(
-			PortletPreferences portletPreferences, long groupId, Layout layout,
-			long[] overrideAllAssetCategoryIds, String[] overrideAllKeywords,
-			String[] overrideAllAssetTagNames)
-		throws PortalException {
-
 		long[] groupIds = getGroupIds(portletPreferences, groupId, layout);
 
 		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
 
-		_setCategoriesAndKeywordsAndTags(
+		_setCategoriesAndTags(
 			assetEntryQuery, portletPreferences, groupIds,
-			overrideAllAssetCategoryIds, overrideAllKeywords,
-			overrideAllAssetTagNames);
+			overrideAllAssetCategoryIds, overrideAllAssetTagNames);
 
 		assetEntryQuery.setGroupIds(groupIds);
 
@@ -504,7 +491,8 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			String queryName = portletPreferences.getValue(
 				"queryName" + i, StringPool.BLANK);
 
-			if (Objects.equals(queryName, "assetTags") && queryContains &&
+			if (!Objects.equals(queryName, "assetCategories") &&
+				queryContains &&
 				(queryAndOperator || (queryValues.length == 1))) {
 
 				allAssetTagNames = queryValues;
@@ -741,37 +729,6 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 		}
 
 		return ArrayUtil.toLongArray(groupIds);
-	}
-
-	@Override
-	public String[] getKeywords(PortletPreferences portletPreferences) {
-		String[] allKeywords = new String[0];
-
-		for (int i = 0; true; i++) {
-			String[] queryValues = portletPreferences.getValues(
-				"queryValues" + i, null);
-
-			if (ArrayUtil.isEmpty(queryValues)) {
-				break;
-			}
-
-			boolean queryContains = GetterUtil.getBoolean(
-				portletPreferences.getValue(
-					"queryContains" + i, StringPool.BLANK));
-			boolean queryAndOperator = GetterUtil.getBoolean(
-				portletPreferences.getValue(
-					"queryAndOperator" + i, StringPool.BLANK));
-			String queryName = portletPreferences.getValue(
-				"queryName" + i, StringPool.BLANK);
-
-			if (Objects.equals(queryName, "keywords") && queryContains &&
-				(queryAndOperator || (queryValues.length == 1))) {
-
-				allKeywords = queryValues;
-			}
-		}
-
-		return allKeywords;
 	}
 
 	@Override
@@ -1135,20 +1092,15 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 		portletPreferences.store();
 	}
 
-	private void _setCategoriesAndKeywordsAndTags(
+	private void _setCategoriesAndTags(
 		AssetEntryQuery assetEntryQuery, PortletPreferences portletPreferences,
 		long[] scopeGroupIds, long[] overrideAllAssetCategoryIds,
-		String[] overrideAllKeywords, String[] overrideAllAssetTagNames) {
+		String[] overrideAllAssetTagNames) {
 
 		long[] allAssetCategoryIds = new long[0];
 		long[] anyAssetCategoryIds = new long[0];
 		long[] notAllAssetCategoryIds = new long[0];
 		long[] notAnyAssetCategoryIds = new long[0];
-
-		String[] allKeywords = new String[0];
-		String[] anyKeywords = new String[0];
-		String[] notAllKeywords = new String[0];
-		String[] notAnyKeywords = new String[0];
 
 		String[] allAssetTagNames = new String[0];
 		String[] anyAssetTagNames = new String[0];
@@ -1188,20 +1140,6 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 					notAnyAssetCategoryIds = assetCategoryIds;
 				}
 			}
-			else if (Objects.equals(queryName, "keywords")) {
-				if (queryContains && queryAndOperator) {
-					allKeywords = queryValues;
-				}
-				else if (queryContains && !queryAndOperator) {
-					anyKeywords = queryValues;
-				}
-				else if (!queryContains && queryAndOperator) {
-					notAllKeywords = queryValues;
-				}
-				else {
-					notAnyKeywords = queryValues;
-				}
-			}
 			else {
 				if (queryContains && queryAndOperator) {
 					allAssetTagNames = queryValues;
@@ -1226,12 +1164,6 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 
 		assetEntryQuery.setAllCategoryIds(allAssetCategoryIds);
 
-		if (overrideAllKeywords != null) {
-			allKeywords = overrideAllKeywords;
-		}
-
-		assetEntryQuery.setAllKeywords(allKeywords);
-
 		if (overrideAllAssetTagNames != null) {
 			allAssetTagNames = overrideAllAssetTagNames;
 		}
@@ -1249,16 +1181,12 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 
 		assetEntryQuery.setAnyCategoryIds(anyAssetCategoryIds);
 
-		assetEntryQuery.setAnyKeywords(anyKeywords);
-
 		long[] anyAssetTagIds = _assetTagLocalService.getTagIds(
 			siteGroupIds, anyAssetTagNames);
 
 		assetEntryQuery.setAnyTagIds(anyAssetTagIds);
 
 		assetEntryQuery.setNotAllCategoryIds(notAllAssetCategoryIds);
-
-		assetEntryQuery.setNotAllKeywords(notAllKeywords);
 
 		for (String assetTagName : notAllAssetTagNames) {
 			long[] notAllAssetTagIds = _assetTagLocalService.getTagIds(
@@ -1268,8 +1196,6 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 		}
 
 		assetEntryQuery.setNotAnyCategoryIds(notAnyAssetCategoryIds);
-
-		assetEntryQuery.setNotAnyKeywords(notAnyKeywords);
 
 		long[] notAnyAssetTagIds = _assetTagLocalService.getTagIds(
 			siteGroupIds, notAnyAssetTagNames);
