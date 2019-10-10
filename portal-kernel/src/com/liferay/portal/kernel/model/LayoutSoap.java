5dfd02352096cc72a406b5c409d83d2bc61323d1
lily.chi <lily.chi@liferay.com>
2019-10-10 15:00:38 +0800

    Revert "LPS-99915 Build Service."
    This reverts commit f79868fafae0856f01b1a8fa42ec1c6b9b83ee45.

--- portal-kernel/src/com/liferay/portal/kernel/model/LayoutSoap.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/LayoutSoap.java
@@ -32,7 +32,6 @@ public class LayoutSoap implements Serializable {
 		LayoutSoap soapModel = new LayoutSoap();
 
 		soapModel.setMvccVersion(model.getMvccVersion());
-		soapModel.setCtCollectionId(model.getCtCollectionId());
 		soapModel.setUuid(model.getUuid());
 		soapModel.setPlid(model.getPlid());
 		soapModel.setGroupId(model.getGroupId());
@@ -127,14 +126,6 @@ public class LayoutSoap implements Serializable {
 
 	public void setMvccVersion(long mvccVersion) {
 		_mvccVersion = mvccVersion;
-	}
-
-	public long getCtCollectionId() {
-		return _ctCollectionId;
-	}
-
-	public void setCtCollectionId(long ctCollectionId) {
-		_ctCollectionId = ctCollectionId;
 	}
 
 	public String getUuid() {
@@ -428,7 +419,6 @@ public class LayoutSoap implements Serializable {
 	}
 
 	private long _mvccVersion;
-	private long _ctCollectionId;
 	private String _uuid;
 	private long _plid;
 	private long _groupId;
