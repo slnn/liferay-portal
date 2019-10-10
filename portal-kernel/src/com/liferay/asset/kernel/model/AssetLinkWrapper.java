2eb2778e84dcb8db845ee266a0114c1683cc6101
lily.chi <lily.chi@liferay.com>
2019-10-10 15:02:30 +0800

    Revert "LPS-102437 Build Service."
    This reverts commit 01dd12d55729c10b0e42fd7c748879edc7c8ef6c.

--- portal-kernel/src/com/liferay/asset/kernel/model/AssetLinkWrapper.java
+++ portal-kernel/src/com/liferay/asset/kernel/model/AssetLinkWrapper.java
@@ -42,7 +42,6 @@ public class AssetLinkWrapper
 	public Map<String, Object> getModelAttributes() {
 		Map<String, Object> attributes = new HashMap<String, Object>();
 
-		attributes.put("mvccVersion", getMvccVersion());
 		attributes.put("linkId", getLinkId());
 		attributes.put("companyId", getCompanyId());
 		attributes.put("userId", getUserId());
@@ -58,12 +57,6 @@ public class AssetLinkWrapper
 
 	@Override
 	public void setModelAttributes(Map<String, Object> attributes) {
-		Long mvccVersion = (Long)attributes.get("mvccVersion");
-
-		if (mvccVersion != null) {
-			setMvccVersion(mvccVersion);
-		}
-
 		Long linkId = (Long)attributes.get("linkId");
 
 		if (linkId != null) {
@@ -167,16 +160,6 @@ public class AssetLinkWrapper
 	@Override
 	public long getLinkId() {
 		return model.getLinkId();
-	}
-
-	/**
-	 * Returns the mvcc version of this asset link.
-	 *
-	 * @return the mvcc version of this asset link
-	 */
-	@Override
-	public long getMvccVersion() {
-		return model.getMvccVersion();
 	}
 
 	/**
@@ -297,16 +280,6 @@ public class AssetLinkWrapper
 	@Override
 	public void setLinkId(long linkId) {
 		model.setLinkId(linkId);
-	}
-
-	/**
-	 * Sets the mvcc version of this asset link.
-	 *
-	 * @param mvccVersion the mvcc version of this asset link
-	 */
-	@Override
-	public void setMvccVersion(long mvccVersion) {
-		model.setMvccVersion(mvccVersion);
 	}
 
 	/**
