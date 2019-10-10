388128fafc13b7676c85f846cb76b618445abb57
lily.chi <lily.chi@liferay.com>
2019-10-10 13:53:41 +0800

    Revert "LPS-102671 Apply treePath."
    This reverts commit 0f4650d212900222dfada561101d552d65f25463.

--- modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetCategoryIndexerIndexedFieldsTest.java
+++ modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetCategoryIndexerIndexedFieldsTest.java
@@ -175,6 +175,9 @@ public class AssetCategoryIndexerIndexedFieldsTest {
 		map.put(
 			"assetCategoryTitle_ja_JP",
 			StringUtil.lowerCase(assetCategory.getName()));
+		map.put(
+			"leftCategoryId",
+			String.valueOf(assetCategory.getLeftCategoryId()));
 		map.put("name_sortable", StringUtil.lowerCase(assetCategory.getName()));
 		map.put(
 			"parentCategoryId",
@@ -182,7 +185,6 @@ public class AssetCategoryIndexerIndexedFieldsTest {
 		map.put("title_ja_JP", assetCategory.getName());
 		map.put(
 			"title_sortable", StringUtil.lowerCase(assetCategory.getName()));
-		map.put("treePath", assetCategory.getTreePath());
 
 		indexedFieldsFixture.populateUID(
 			AssetCategory.class.getName(), assetCategory.getCategoryId(), map);
