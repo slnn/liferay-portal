314c705aa1bebff35d3439759801499498261532
lily.chi <lily.chi@liferay.com>
2019-10-10 13:53:41 +0800

    Revert "LPS-102671 Apply treePath."
    This reverts commit 0f4650d212900222dfada561101d552d65f25463.

--- portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetCategoryLeftCategoryIdComparator.java
+++ portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetCategoryLeftCategoryIdComparator.java
@@ -12,47 +12,46 @@
  * details.
  */
 
-package com.liferay.asset.categories.admin.web.internal.util;
+package com.liferay.portlet.asset.util.comparator;
 
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 /**
- * @author Preston Crary
+ * @author Shuyang Zhou
  */
-public class AssetCategoryTreePathComparator
+public class AssetCategoryLeftCategoryIdComparator
 	extends OrderByComparator<AssetCategory> {
 
-	public static final AssetCategoryTreePathComparator INSTANCE_ASCENDING =
-		new AssetCategoryTreePathComparator(true);
+	public static final String ORDER_BY_ASC = "leftCategoryId ASC";
 
-	public static final AssetCategoryTreePathComparator INSTANCE_DESCENDING =
-		new AssetCategoryTreePathComparator(false);
+	public static final String ORDER_BY_DESC = "leftCategoryId DESC";
 
-	public static final String ORDER_BY_ASC = "treePath ASC";
+	public static final String[] ORDER_BY_FIELDS = {"leftCategoryId"};
 
-	public static final String ORDER_BY_DESC = "treePath DESC";
+	public AssetCategoryLeftCategoryIdComparator() {
+		this(false);
+	}
 
-	public static final String[] ORDER_BY_FIELDS = {"treePath"};
-
-	public static AssetCategoryTreePathComparator getInstance(
-		boolean ascending) {
-
-		if (ascending) {
-			return INSTANCE_ASCENDING;
-		}
-
-		return INSTANCE_DESCENDING;
+	public AssetCategoryLeftCategoryIdComparator(boolean ascending) {
+		_ascending = ascending;
 	}
 
 	@Override
 	public int compare(
 		AssetCategory assetCategory1, AssetCategory assetCategory2) {
 
-		String treePath1 = assetCategory1.getTreePath();
-		String treePath2 = assetCategory2.getTreePath();
+		long leftCategoryId1 = assetCategory1.getLeftCategoryId();
+		long leftCategoryId2 = assetCategory2.getLeftCategoryId();
 
-		int value = treePath1.compareTo(treePath2);
+		int value = 0;
+
+		if (leftCategoryId1 < leftCategoryId2) {
+			value = -1;
+		}
+		else if (leftCategoryId1 > leftCategoryId2) {
+			value = 1;
+		}
 
 		if (_ascending) {
 			return value;
@@ -78,10 +77,6 @@ public class AssetCategoryTreePathComparator
 	@Override
 	public boolean isAscending() {
 		return _ascending;
-	}
-
-	private AssetCategoryTreePathComparator(boolean ascending) {
-		_ascending = ascending;
 	}
 
 	private final boolean _ascending;
