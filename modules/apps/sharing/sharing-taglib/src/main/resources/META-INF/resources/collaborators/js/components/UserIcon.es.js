70e9300501df4680b551ad34aab6f55530edb9fd
lily.chi <lily.chi@liferay.com>
2019-10-11 10:32:22 +0800

    Revert "LPS-101411 Using classNames"
    This reverts commit b8a3a42188b7353c4312129e21a63835e6a3b0e3.

--- modules/apps/sharing/sharing-taglib/src/main/resources/META-INF/resources/collaborators/js/components/UserIcon.es.js
+++ modules/apps/sharing/sharing-taglib/src/main/resources/META-INF/resources/collaborators/js/components/UserIcon.es.js
@@ -16,16 +16,15 @@ import ClayIcon from '@clayui/icon';
 import ClaySticker from '@clayui/sticker';
 import PropTypes from 'prop-types';
 import React from 'react';
-import classNames from 'classnames';
 
 const UserIcon = ({fullName = '', portraitURL = '', userId, size = 'lg'}) => {
 	const stickerColor = parseInt(userId, 10) % 10;
 
 	return (
 		<ClaySticker
-			className={classNames('sticker-use-icon', {
-				[`user-icon-color-${stickerColor}`]: !portraitURL
-			})}
+			className={`sticker-use-icon ${
+				!portraitURL ? 'user-icon-color-' + stickerColor : ''
+			}`}
 			displayType="secondary"
 			shape="circle"
 			size={size}
