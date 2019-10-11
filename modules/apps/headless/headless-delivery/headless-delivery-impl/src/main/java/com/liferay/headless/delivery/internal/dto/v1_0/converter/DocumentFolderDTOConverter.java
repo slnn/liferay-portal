673effc3db7a2655fca2b78c1c1a7f89e7cbddc4
lily.chi <lily.chi@liferay.com>
2019-10-11 13:32:05 +0800

    Revert "LPS-98387 do we need to check if user is null?"
    This reverts commit 19940497fb8dd9926450a641757860640a7a4051.

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/DocumentFolderDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/DocumentFolderDTOConverter.java
@@ -21,6 +21,7 @@ import com.liferay.headless.delivery.dto.v1_0.converter.DTOConverter;
 import com.liferay.headless.delivery.dto.v1_0.converter.DTOConverterContext;
 import com.liferay.headless.delivery.internal.dto.v1_0.util.CreatorUtil;
 import com.liferay.headless.delivery.internal.dto.v1_0.util.CustomFieldsUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.Portal;
@@ -50,6 +51,8 @@ public class DocumentFolderDTOConverter implements DTOConverter {
 		Folder folder = _dlAppService.getFolder(
 			dtoConverterContext.getResourcePrimKey());
 
+		User user = dtoConverterContext.getUser();
+
 		return new DocumentFolder() {
 			{
 				creator = CreatorUtil.toCreator(
@@ -68,7 +71,7 @@ public class DocumentFolderDTOConverter implements DTOConverter {
 					folder.getRepositoryId(), folder.getFolderId());
 				siteId = folder.getGroupId();
 				subscribed = _subscriptionLocalService.isSubscribed(
-					folder.getCompanyId(), dtoConverterContext.getUserId(),
+					folder.getCompanyId(), user.getUserId(),
 					DLFolder.class.getName(), folder.getFolderId());
 			}
 		};
