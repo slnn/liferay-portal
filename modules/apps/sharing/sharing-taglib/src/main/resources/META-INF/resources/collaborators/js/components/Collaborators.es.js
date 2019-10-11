2bfd107f694e4c2a6c782cb429b77ac3e47a9f24
lily.chi <lily.chi@liferay.com>
2019-10-11 10:33:50 +0800

    Revert "LPS-101411 Doesn't render without collaborators"
    This reverts commit e429b5ac992964ccb75887e792b84024ae2fbe3c.

--- modules/apps/sharing/sharing-taglib/src/main/resources/META-INF/resources/collaborators/js/components/Collaborators.es.js
+++ modules/apps/sharing/sharing-taglib/src/main/resources/META-INF/resources/collaborators/js/components/Collaborators.es.js
@@ -51,15 +51,9 @@ const Collaborators = ({collaboratorsResourceURL, portletNamespace}) => {
 		});
 	};
 
-	if (!data) {
-		return <ClayLoadingIndicator />;
-	}
+	if (!data) return <ClayLoadingIndicator />;
 
 	const {owner, total, manageCollaboratorsURL, collaborators} = data;
-
-	if (total < 1) {
-		return;
-	}
 
 	const moreCollaboratorsCount = total - collaborators.length;
 
