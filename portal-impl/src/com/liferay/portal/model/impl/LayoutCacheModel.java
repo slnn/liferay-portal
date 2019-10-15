db004a2800d3f9d3c64047a88da70d2df4e19811
lily.chi <lily.chi@liferay.com>
2019-10-15 09:37:26 +0800

    Revert "LPS-102202 Auto"
    This reverts commit 05e2d1b47072952905790ca2879a90e30499efb6.

--- portal-impl/src/com/liferay/portal/model/impl/LayoutCacheModel.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutCacheModel.java
@@ -76,7 +76,7 @@ public class LayoutCacheModel
 
 	@Override
 	public String toString() {
-		StringBundler sb = new StringBundler(75);
+		StringBundler sb = new StringBundler(73);
 
 		sb.append("{mvccVersion=");
 		sb.append(mvccVersion);
@@ -146,8 +146,6 @@ public class LayoutCacheModel
 		sb.append(layoutPrototypeLinkEnabled);
 		sb.append(", sourcePrototypeLayoutUuid=");
 		sb.append(sourcePrototypeLayoutUuid);
-		sb.append(", masterLayoutPageTemplateEntryId=");
-		sb.append(masterLayoutPageTemplateEntryId);
 		sb.append(", publishDate=");
 		sb.append(publishDate);
 		sb.append(", lastPublishDate=");
@@ -304,9 +302,6 @@ public class LayoutCacheModel
 			layoutImpl.setSourcePrototypeLayoutUuid(sourcePrototypeLayoutUuid);
 		}
 
-		layoutImpl.setMasterLayoutPageTemplateEntryId(
-			masterLayoutPageTemplateEntryId);
-
 		if (publishDate == Long.MIN_VALUE) {
 			layoutImpl.setPublishDate(null);
 		}
@@ -378,8 +373,6 @@ public class LayoutCacheModel
 
 		layoutPrototypeLinkEnabled = objectInput.readBoolean();
 		sourcePrototypeLayoutUuid = objectInput.readUTF();
-
-		masterLayoutPageTemplateEntryId = objectInput.readLong();
 		publishDate = objectInput.readLong();
 		lastPublishDate = objectInput.readLong();
 	}
@@ -528,7 +521,6 @@ public class LayoutCacheModel
 			objectOutput.writeUTF(sourcePrototypeLayoutUuid);
 		}
 
-		objectOutput.writeLong(masterLayoutPageTemplateEntryId);
 		objectOutput.writeLong(publishDate);
 		objectOutput.writeLong(lastPublishDate);
 	}
@@ -567,7 +559,6 @@ public class LayoutCacheModel
 	public String layoutPrototypeUuid;
 	public boolean layoutPrototypeLinkEnabled;
 	public String sourcePrototypeLayoutUuid;
-	public long masterLayoutPageTemplateEntryId;
 	public long publishDate;
 	public long lastPublishDate;
 
