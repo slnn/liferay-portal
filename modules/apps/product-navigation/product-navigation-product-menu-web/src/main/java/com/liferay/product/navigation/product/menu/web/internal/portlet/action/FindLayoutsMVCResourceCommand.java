2fd74883a418176606f0a510012603ceec8b99cc
lily.chi <lily.chi@liferay.com>
2019-10-16 10:40:06 +0800

    Revert "LPS-102650 Return path element for each layout"
    This reverts commit e2c3d836ea705aad6031faa8341a82f154c4ff91.

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/portlet/action/FindLayoutsMVCResourceCommand.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/portlet/action/FindLayoutsMVCResourceCommand.java
@@ -14,11 +14,13 @@
 
 package com.liferay.product.navigation.product.menu.web.internal.portlet.action;
 
-import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
@@ -27,16 +29,13 @@ import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ContentTypes;
-import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.product.navigation.product.menu.web.internal.constants.ProductNavigationProductMenuPortletKeys;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -103,13 +102,24 @@ public class FindLayoutsMVCResourceCommand extends BaseMVCResourceCommand {
 			0, 10, null);
 
 		for (Layout layout : layouts) {
-			JSONArray layoutPathJSONArray = _getLayoutPathJSONArray(
-				layout, themeDisplay.getLocale());
+			StringBundler sb = new StringBundler(5);
+
+			sb.append(layout.getName(themeDisplay.getLocale()));
+			sb.append(StringPool.SPACE);
+			sb.append(StringPool.OPEN_PARENTHESIS);
+
+			if (layout.isPrivateLayout()) {
+				sb.append(
+					LanguageUtil.get(themeDisplay.getLocale(), "private"));
+			}
+			else {
+				sb.append(LanguageUtil.get(themeDisplay.getLocale(), "public"));
+			}
+
+			sb.append(StringPool.CLOSE_PARENTHESIS);
 
 			JSONObject layoutJSONObject = JSONUtil.put(
-				"name", layout.getName(themeDisplay.getLocale())
-			).put(
-				"path", layoutPathJSONArray
+				"name", sb.toString()
 			).put(
 				"url", _portal.getLayoutFullURL(layout, themeDisplay)
 			);
@@ -132,22 +142,6 @@ public class FindLayoutsMVCResourceCommand extends BaseMVCResourceCommand {
 		jsonObject.put("totalCount", totalCount);
 
 		ServletResponseUtil.write(httpServletResponse, jsonObject.toString());
-	}
-
-	private JSONArray _getLayoutPathJSONArray(Layout layout, Locale locale)
-		throws PortalException {
-
-		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
-
-		List<Layout> ancestors = layout.getAncestors();
-
-		Collections.reverse(ancestors);
-
-		for (Layout ancestor : ancestors) {
-			jsonArray.put(HtmlUtil.escape(ancestor.getName(locale)));
-		}
-
-		return jsonArray;
 	}
 
 	@Reference
