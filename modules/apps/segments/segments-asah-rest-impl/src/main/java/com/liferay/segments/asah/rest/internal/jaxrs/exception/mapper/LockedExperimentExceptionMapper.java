069d86339b3da58d3663c8f8139261949746cc88
lily.chi <lily.chi@liferay.com>
2019-10-16 10:49:44 +0800

    Revert "LPS-84119 Sort"
    This reverts commit 0e0f26c1bb82a4f2e662334ed67b2c6b3484ff09.

--- modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/jaxrs/exception/mapper/LockedExperimentExceptionMapper.java
+++ modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/jaxrs/exception/mapper/LockedExperimentExceptionMapper.java
@@ -45,10 +45,10 @@ public class LockedExperimentExceptionMapper
 
 		return Response.status(
 			400
-		).entity(
-			lockedSegmentsExperimentException.getMessage()
 		).type(
 			MediaType.TEXT_PLAIN
+		).entity(
+			lockedSegmentsExperimentException.getMessage()
 		).build();
 	}
 
