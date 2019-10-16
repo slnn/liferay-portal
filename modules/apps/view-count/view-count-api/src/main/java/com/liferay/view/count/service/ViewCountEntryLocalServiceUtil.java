009bdd3303d76ddab2e5563709724a07fc14720b
Samuel Tran <samuel.tran@liferay.com>
2019-10-16 13:27:47 -0700

    LPS-101084 view-count - Regen buildService

--- modules/apps/view-count/view-count-api/src/main/java/com/liferay/view/count/service/ViewCountEntryLocalServiceUtil.java
+++ modules/apps/view-count/view-count-api/src/main/java/com/liferay/view/count/service/ViewCountEntryLocalServiceUtil.java
@@ -39,17 +39,6 @@ public class ViewCountEntryLocalServiceUtil {
 	 */
 
 	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify or reference this interface directly. Always use {@link ViewCountEntryLocalServiceUtil} to access the view count entry local service. Add custom service methods to <code>com.liferay.view.count.service.impl.ViewCountEntryLocalServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface.
-	 */
-	public static com.liferay.view.count.model.ViewCountEntry addViewCountEntry(
-		long companyId, long classNameId, long classPK) {
-
-		return getService().addViewCountEntry(companyId, classNameId, classPK);
-	}
-
-	/**
 	 * Adds the view count entry to the database. Also notifies the appropriate model listeners.
 	 *
 	 * @param viewCountEntry the view count entry
@@ -298,8 +287,7 @@ public class ViewCountEntryLocalServiceUtil {
 	}
 
 	public static void removeViewCount(
-			long companyId, long classNameId, long classPK)
-		throws com.liferay.portal.kernel.exception.PortalException {
+		long companyId, long classNameId, long classPK) {
 
 		getService().removeViewCount(companyId, classNameId, classPK);
 	}
