fd0447e4487b7a63adf8fd08320da338335daa37
lily.chi <lily.chi@liferay.com>
2019-10-17 10:17:34 +0800

    Revert "LPS-102957 Adjust app, since we have moved a lot of code to page template web"
    This reverts commit 82df65e4c84e94e53163dc8890618c3f9b29180b.

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -825,6 +825,21 @@ public class LayoutsAdminDisplayContext {
 						});
 				}
 
+				if (!(stagingGroupHelper.isLocalLiveGroup(group) ||
+					  stagingGroupHelper.isRemoteLiveGroup(group))) {
+
+					add(
+						navigationItem -> {
+							navigationItem.setActive(
+								Objects.equals(getTabs1(), "page-templates"));
+							navigationItem.setHref(
+								getPortletURL(), "tabs1", "page-templates");
+							navigationItem.setLabel(
+								LanguageUtil.get(
+									_httpServletRequest, "page-templates"));
+						});
+				}
+
 				if (!group.isCompany() &&
 					!(stagingGroupHelper.isLocalLiveGroup(group) ||
 					  stagingGroupHelper.isRemoteLiveGroup(group))) {
