739c4e64d6784c9c7b16346c231e418b98c77516
lily.chi <lily.chi@liferay.com>
2019-10-21 16:29:38 +0800

    Revert "LPS-103350 Adding a keyword filter using [contains][all] produces incorrect result"
    This reverts commit 967e179b5aeb08d71c536e3b9da69beb97dc96be.

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -362,7 +362,7 @@ public class AssetPublisherDisplayContext {
 			_assetEntryQuery = _assetPublisherHelper.getAssetEntryQuery(
 				_portletPreferences, _themeDisplay.getScopeGroupId(),
 				_themeDisplay.getLayout(), getAllAssetCategoryIds(),
-				getAllAssetTagNames(), getAllKeywords());
+				getAllKeywords(), getAllAssetTagNames());
 		}
 
 		_assetEntryQuery.setEnablePermissions(isEnablePermissions());
