50ff6d77b3f993f261d9b61316d4ec00c3676566
lily.chi <lily.chi@liferay.com>
2019-11-25 16:59:58 +0800

    LPS-74148 Extract logic, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -995,10 +995,8 @@ public class DataFactory {
 		_defaultDLDDMStructureVersionModel = newDDMStructureVersionModel(
 			_defaultDLDDMStructureModel);
 
-		_defaultDLDDMStructureLayoutModel = newDDMStructureLayoutModel(
-			_globalGroupId, _defaultUserId,
-			_defaultDLDDMStructureVersionModel.getStructureVersionId(),
-			_dlDDMStructureLayoutContent);
+		_defaultDLDDMStructureLayoutModel = newDefaultDLDDMStructureLayoutModel(
+			_defaultDLDDMStructureVersionModel);
 
 		_defaultJournalDDMStructureModel = newDDMStructureModel(
 			_globalGroupId, _defaultUserId,
@@ -2033,6 +2031,15 @@ public class DataFactory {
 		ddmTemplateVersionModelImpl.setStatusDate(nextFutureDate());
 
 		return ddmTemplateVersionModelImpl;
+	}
+
+	public DDMStructureLayoutModel newDefaultDLDDMStructureLayoutModel(
+		DDMStructureVersionModel defaultDLDDMStructureVersionModel) {
+
+		return newDDMStructureLayoutModel(
+			_globalGroupId, _defaultUserId,
+			defaultDLDDMStructureVersionModel.getStructureVersionId(),
+			_dlDDMStructureLayoutContent);
 	}
 
 	public DDMStructureModel newDefaultDLDDMStructureModel() {
