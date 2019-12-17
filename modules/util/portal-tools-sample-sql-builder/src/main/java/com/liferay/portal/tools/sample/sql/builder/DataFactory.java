48304f0428537a9b7299a0f1c846e4704393cdda
lily.chi <lily.chi@liferay.com>
2019-12-17 10:51:25 +0800

    LPS-74148 Make these models generation consistent with other models

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -375,11 +375,6 @@ public class DataFactory {
 
 		initAssetCategoryModels();
 		initAssetTagModels();
-		_commerceCurrencyModel = newCommerceCurrencyModel();
-
-		_commerceCatalogModel = newCommerceCatalogModel();
-
-		_commerceChannelModel = newCommerceChannelModel();
 
 		initCommerceProductModels();
 
@@ -545,18 +540,6 @@ public class DataFactory {
 
 	public Collection<ClassNameModel> getClassNameModels() {
 		return _classNameModels.values();
-	}
-
-	public CommerceCatalogModel getCommerceCatalogModel() {
-		return _commerceCatalogModel;
-	}
-
-	public CommerceChannelModel getCommerceChannelModel() {
-		return _commerceChannelModel;
-	}
-
-	public CommerceCurrencyModel getCommerceCurrencyModel() {
-		return _commerceCurrencyModel;
 	}
 
 	public long getCounterNext() {
@@ -4472,11 +4455,8 @@ public class DataFactory {
 		new HashMap<>();
 	private final long _commerceCatalogGroupId;
 	private final long _commerceCatalogId;
-	private final CommerceCatalogModel _commerceCatalogModel;
 	private final long _commerceChannelGroupId;
 	private final long _commerceChannelId;
-	private final CommerceChannelModel _commerceChannelModel;
-	private final CommerceCurrencyModel _commerceCurrencyModel;
 	private final long _companyId;
 	private final SimpleCounter _counter;
 	private List<CPDefinitionLocalizationModel> _cpDefinitionLocalizationModels;
