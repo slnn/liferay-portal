555697f49fbca41d90e14261a632653e2be6d802
lily.chi <lily.chi@liferay.com>
2020-05-13 20:55:00 +0800

    LPS-113497 Fix rebase compile issue according to LPS-105380 [master:216100ecdd285be63ca9753955be52fd52c65cef]

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2115,7 +2115,7 @@ public class DataFactory {
 
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
 			List<LayoutModel> layoutModels)
-		throws IOException {
+		throws Exception {
 
 		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
 			new ArrayList<>();
