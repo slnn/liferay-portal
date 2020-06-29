b4a93948a6a52bc5668256288c52262e40f141c2
lily.chi <lily.chi@liferay.com>
2020-06-29 17:21:20 +0800

    LPS-116057 Set propertiese to config data about change list

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
@@ -57,6 +57,21 @@ public interface BenchmarksPropsKeys {
 	public static final String MAX_CONTENT_LAYOUT_COUNT =
 		"sample.sql.max.content.layout.count";
 
+	public static final String MAX_CT_COUNT =
+		"sample.sql.max.change.list.count";
+
+	public static final String MAX_CT_JOURNAL_ARTICLE_COUNT =
+		"sample.sql.max.ct.journal.article.count";
+
+	public static final String MAX_CT_JOURNAL_FOLDER_COUNT =
+		"sample.sql.max.ct.journal.folder.count";
+
+	public static final String MAX_CT_PAGE_COUNT =
+		"sample.sql.max.ct.page.count";
+
+	public static final String MAX_CT_WEBCONTENT_DISPLAY_COUNT =
+		"sample.sql.max.ct.webcontent.display.count";
+
 	public static final String MAX_DDL_CUSTOM_FIELD_COUNT =
 		"sample.sql.max.ddl.custom.field.count";
 
