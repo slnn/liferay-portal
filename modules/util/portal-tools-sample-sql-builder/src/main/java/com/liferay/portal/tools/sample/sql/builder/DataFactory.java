5cb2ba42766496fe5bb5b2718c903e2f8fa81b0f
lily.chi <lily.chi@liferay.com>
2020-07-02 16:13:25 +0800

    LPS-113497 Update according to current head of master

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4214,6 +4214,7 @@ public class DataFactory {
 		fragmentEntryLinkModel.setFragmentEntryId(0);
 		fragmentEntryLinkModel.setClassNameId(getClassNameId(Layout.class));
 		fragmentEntryLinkModel.setClassPK(layoutModel.getPlid());
+		fragmentEntryLinkModel.setPlid(layoutModel.getPlid());
 		fragmentEntryLinkModel.setRendererKey(renderKey);
 		fragmentEntryLinkModel.setConfiguration(configuration);
 		fragmentEntryLinkModel.setCss(css);
