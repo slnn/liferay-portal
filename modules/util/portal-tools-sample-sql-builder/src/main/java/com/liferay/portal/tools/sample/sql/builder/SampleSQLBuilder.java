80a5456d361c7283764485521d0e2d4f15a36995
lily.chi <lily.chi@liferay.com>
2020-07-09 17:28:33 +0800

    LPS-105694 Move to WikiDataFactory

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
@@ -262,6 +262,8 @@ public class SampleSQLBuilder {
 							"journalDataFactory", new JournalDataFactory()
 						).put(
 							"messageBoardDataFactory", new MessageBoardDataFactory()
+						).put(
+							"wikiDataFactory", new WikiDataFactory()
 						).build(),
 						sampleSQLWriter);
 				}
