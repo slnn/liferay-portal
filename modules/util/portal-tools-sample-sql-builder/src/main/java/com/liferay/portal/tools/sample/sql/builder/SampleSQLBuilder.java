bc3b288380b57e9fd0b61d9072dea5d8ef3158a3
lily.chi <lily.chi@liferay.com>
2020-07-09 17:10:21 +0800

    LPS-105694 Move to MessageBoardDataFactory

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
@@ -368,6 +368,8 @@ public class SampleSQLBuilder {
 			"dLDataFactory", new DLDataFactory()
 		).put(
 			"journalDataFactory", new JournalDataFactory()
+		).put(
+			"messageBoardDataFactory", new MessageBoardDataFactory()
 		).build();
 	}
 
