f1a46a978f7641f4e73e9ec70ef74b9c0e961696
Lily.Chi <lily.chi@liferay.com>
2020-08-17 15:29:46 +0800

    Revert "LPS-117064 Sort"
    This reverts commit 41e6baede92d2b0251b2d7449971df073a666ae1.

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/StyledLayoutStructureItem.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/StyledLayoutStructureItem.java
@@ -320,7 +320,7 @@ public abstract class StyledLayoutStructureItem extends LayoutStructureItem {
 		Object configValue = configJSONObject.get(propertyKey);
 		Object styleValue = stylesJSONObject.get(propertyKey);
 
-		if ((configValue != null) && (styleValue == null)) {
+		if ((styleValue == null) && (configValue != null)) {
 			return configValue;
 		}
 
