c11916ce7eae5a55de6edc66392672d77c3aa5b5
lily.chi <lily.chi@liferay.com>
2020-08-20 09:12:19 +0800

    Revert "LPS-117064 Good enough"
    This reverts commit 3e21c8d9ec06663821f29c4102419ee081587cf1.

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/FragmentLayoutStructureItem.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/FragmentLayoutStructureItem.java
@@ -100,8 +100,7 @@ public class FragmentLayoutStructureItem extends StyledLayoutStructureItem {
 
 				_fragmentConfigurationJSONObject =
 					editablesJSONObject.getJSONObject(
-						"com.liferay.fragment.entry.processor.freemarker." +
-							"FreeMarkerFragmentEntryProcessor");
+						_KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR);
 			}
 			catch (Exception exception) {
 				_log.error("Unable to parse editable values", exception);
@@ -118,6 +117,10 @@ public class FragmentLayoutStructureItem extends StyledLayoutStructureItem {
 				itemConfigJSONObject.getLong("fragmentEntryLinkId"));
 		}
 	}
+
+	private static final String _KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR =
+		"com.liferay.fragment.entry.processor.freemarker." +
+			"FreeMarkerFragmentEntryProcessor";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		FragmentLayoutStructureItem.class);
