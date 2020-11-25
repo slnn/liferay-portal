fa2acb85100e6d56282ccaf66456cee5c7133cb2
Dante Wang <dante.wang@liferay.com>
2020-11-25 15:20:50 +0800

    LPS-120376 SF, clean up

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsValues.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsValues.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.tools.sample.sql.builder;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -26,8 +24,6 @@ import java.io.Reader;
 
 import java.time.ZoneId;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.TimeZone;
 
@@ -208,24 +204,6 @@ public class BenchmarksPropsValues {
 			}
 			catch (Exception exception) {
 				throw new ExceptionInInitializerError(exception);
-			}
-
-			List<String> propertyNames = new ArrayList<>(
-				properties.stringPropertyNames());
-
-			propertyNames.sort(null);
-
-			StringBundler sb = new StringBundler(propertyNames.size() * 4);
-
-			for (String propertyName : propertyNames) {
-				if (!propertyName.startsWith("sample.sql")) {
-					continue;
-				}
-
-				sb.append(propertyName);
-				sb.append(StringPool.EQUAL);
-				sb.append(properties.getProperty(propertyName));
-				sb.append(StringPool.NEW_LINE);
 			}
 
 			_properties = properties;
