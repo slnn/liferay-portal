b10804713fc4a1976f8bcfa8bf3164ef166c0a21
lily.chi <lily.chi@liferay.com>
2020-12-04 10:57:54 +0800

    Revert "LPS-122340 Implement DefaultDataStorage using the new service and deprecate JSONDataStorage."
    This reverts commit aae944636d5e2abae701bbf73d323b23c1f5ec93.

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/storage/JSONDataStorage.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/storage/JSONDataStorage.java
@@ -14,9 +14,28 @@
 
 package com.liferay.data.engine.rest.internal.storage;
 
+import com.liferay.data.engine.rest.dto.v2_0.DataRecord;
+import com.liferay.data.engine.rest.dto.v2_0.DataRecordCollection;
+import com.liferay.data.engine.rest.internal.dto.v2_0.util.DataRecordCollectionUtil;
+import com.liferay.data.engine.rest.internal.dto.v2_0.util.MapToDDMFormValuesConverterUtil;
+import com.liferay.data.engine.rest.internal.storage.util.DataStorageUtil;
 import com.liferay.data.engine.storage.DataStorage;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeResponse;
+import com.liferay.dynamic.data.mapping.model.DDMContent;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMContentLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Portal;
 
 import java.util.Map;
 
@@ -35,24 +54,30 @@ public class JSONDataStorage implements DataStorage {
 
 	@Override
 	public long delete(long dataStorageId) throws Exception {
-		if (_log.isWarnEnabled()) {
-			_log.warn(
-				"JSON data storage is deprecated, using default data storage");
+		DDMContent ddmContent = _ddmContentLocalService.fetchDDMContent(
+			dataStorageId);
+
+		if (ddmContent != null) {
+			_ddmContentLocalService.deleteDDMContent(ddmContent);
 		}
 
-		return _dataStorage.delete(dataStorageId);
+		return dataStorageId;
 	}
 
 	@Override
 	public Map<String, Object> get(long dataDefinitionId, long dataStorageId)
 		throws Exception {
 
-		if (_log.isWarnEnabled()) {
-			_log.warn(
-				"JSON data storage is deprecated, using default data storage");
-		}
+		DDMContent ddmContent = _ddmContentLocalService.getContent(
+			dataStorageId);
 
-		return _dataStorage.get(dataDefinitionId, dataStorageId);
+		DDMStructure ddmStructure = _ddmStructureLocalService.getStructure(
+			dataDefinitionId);
+
+		return DataStorageUtil.toDataRecordValues(
+			_deserializeDDMFormValues(
+				ddmContent.getData(), ddmStructure.getFullHierarchyDDMForm()),
+			ddmStructure);
 	}
 
 	@Override
@@ -61,19 +86,72 @@ public class JSONDataStorage implements DataStorage {
 			long siteId)
 		throws Exception {
 
-		if (_log.isWarnEnabled()) {
-			_log.warn(
-				"JSON data storage is deprecated, using default data storage");
-		}
+		DataRecordCollection dataRecordCollection =
+			DataRecordCollectionUtil.toDataRecordCollection(
+				_ddlRecordSetLocalService.getRecordSet(dataRecordCollectionId));
 
-		return _dataStorage.save(
-			dataRecordCollectionId, dataRecordValues, siteId);
+		DDMStructure ddmStructure = _ddmStructureLocalService.getStructure(
+			dataRecordCollection.getDataDefinitionId());
+
+		DDMContent ddmContent = _ddmContentLocalService.addContent(
+			PrincipalThreadLocal.getUserId(), siteId,
+			DataRecord.class.getName(), null,
+			_serializeDDMFormValues(
+				MapToDDMFormValuesConverterUtil.toDDMFormValues(
+					dataRecordValues, ddmStructure.getFullHierarchyDDMForm(),
+					null)),
+			new ServiceContext() {
+				{
+					setScopeGroupId(siteId);
+					setUserId(PrincipalThreadLocal.getUserId());
+				}
+			});
+
+		return ddmContent.getPrimaryKey();
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		JSONDataStorage.class);
+	private DDMFormValues _deserializeDDMFormValues(
+		String content, DDMForm ddmForm) {
 
-	@Reference(target = "(data.storage.type=default)")
-	private DataStorage _dataStorage;
+		DDMFormValuesDeserializerDeserializeRequest.Builder builder =
+			DDMFormValuesDeserializerDeserializeRequest.Builder.newBuilder(
+				content, ddmForm);
+
+		DDMFormValuesDeserializerDeserializeResponse
+			ddmFormValuesDeserializerDeserializeResponse =
+				_jsonDDMFormValuesDeserializer.deserialize(builder.build());
+
+		return ddmFormValuesDeserializerDeserializeResponse.getDDMFormValues();
+	}
+
+	private String _serializeDDMFormValues(DDMFormValues ddmFormValues) {
+		DDMFormValuesSerializerSerializeRequest.Builder builder =
+			DDMFormValuesSerializerSerializeRequest.Builder.newBuilder(
+				ddmFormValues);
+
+		DDMFormValuesSerializerSerializeResponse
+			ddmFormValuesSerializerSerializeResponse =
+				_jsonDDMFormValuesSerializer.serialize(builder.build());
+
+		return ddmFormValuesSerializerSerializeResponse.getContent();
+	}
+
+	@Reference
+	private DDLRecordSetLocalService _ddlRecordSetLocalService;
+
+	@Reference
+	private DDMContentLocalService _ddmContentLocalService;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference(target = "(ddm.form.values.deserializer.type=json)")
+	private DDMFormValuesDeserializer _jsonDDMFormValuesDeserializer;
+
+	@Reference(target = "(ddm.form.values.serializer.type=json)")
+	private DDMFormValuesSerializer _jsonDDMFormValuesSerializer;
+
+	@Reference
+	private Portal _portal;
 
 }
