1560330b4ff89df6e051779f4b6c46718f2f3284
lily.chi <lily.chi@liferay.com>
2021-01-27 15:51:59 +0800

    LPS-127055 Init the _sampleUserId field during createing sampleUserModel, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -359,7 +359,6 @@ public class DataFactory {
 		_defaultJournalDDMTemplateId = _counter.get();
 		_globalGroupId = _counter.get();
 		_guestGroupId = _counter.get();
-		_sampleUserId = _counter.get();
 		_userPersonalSiteGroupId = _counter.get();
 
 		_dlDDMStructureContent = _readFile("ddm_structure_basic_document.json");
@@ -3721,6 +3720,8 @@ public class DataFactory {
 	}
 
 	public UserModel newSampleUserModel() {
+		_sampleUserId = _counter.get();
+
 		return newUserModel(
 			_sampleUserId, _SAMPLE_USER_NAME, _SAMPLE_USER_NAME,
 			_SAMPLE_USER_NAME, false);
@@ -5310,7 +5311,7 @@ public class DataFactory {
 	private RoleModel _ownerRoleModel;
 	private RoleModel _powerUserRoleModel;
 	private final SimpleCounter _resourcePermissionCounter;
-	private final long _sampleUserId;
+	private long _sampleUserId;
 	private final Format _simpleDateFormat;
 	private RoleModel _siteMemberRoleModel;
 	private final SimpleCounter _socialActivityCounter;
