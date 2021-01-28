87729f74bd75581abc275f2eeaf69b274f30d533
lily.chi <lily.chi@liferay.com>
2021-01-28 21:10:25 +0800

    LPS-127055 Inline

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -3919,10 +3919,8 @@ public class DataFactory {
 	}
 
 	public GroupModel newUserPersonalSiteGroupModel() {
-		_userPersonalSiteGroupId = _counter.get();
-
 		return newGroupModel(
-			_userPersonalSiteGroupId, getClassNameId(UserPersonalSite.class),
+			_counter.get(), getClassNameId(UserPersonalSite.class),
 			_defaultUserId, GroupConstants.USER_PERSONAL_SITE, false);
 	}
 
@@ -5383,7 +5381,6 @@ public class DataFactory {
 	private RoleModel _siteMemberRoleModel;
 	private final SimpleCounter _socialActivityCounter;
 	private final SimpleCounter _timeCounter;
-	private long _userPersonalSiteGroupId;
 	private RoleModel _userRoleModel;
 	private final SimpleCounter _userScreenNameCounter;
 
