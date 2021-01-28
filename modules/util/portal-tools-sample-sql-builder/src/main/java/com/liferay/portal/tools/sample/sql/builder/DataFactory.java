bdf480f60b67cb4a819a90dbd565faccc40e80d0
lily.chi <lily.chi@liferay.com>
2021-01-28 15:51:53 +0800

    LPS-127055 Init the _defaultJournalDDMStructureVersionId field during creating DDMStructureVersionModel, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -354,7 +354,6 @@ public class DataFactory {
 		_companyId = _counter.get();
 		_defaultDLDDMStructureId = _counter.get();
 		_defaultDLDDMStructureVersionId = _counter.get();
-		_defaultJournalDDMStructureVersionId = _counter.get();
 		_defaultJournalDDMTemplateId = _counter.get();
 
 		_dlDDMStructureContent = _readFile("ddm_structure_basic_document.json");
@@ -2172,6 +2171,8 @@ public class DataFactory {
 
 	public DDMStructureVersionModel newDefaultJournalDDMStructureVersionModel(
 		DDMStructureModel ddmStructureModel) {
+
+		_defaultJournalDDMStructureVersionId = _counter.get();
 
 		return newDDMStructureVersionModel(
 			ddmStructureModel, _defaultJournalDDMStructureVersionId);
@@ -5295,7 +5296,7 @@ public class DataFactory {
 	private final long _defaultDLDDMStructureId;
 	private final long _defaultDLDDMStructureVersionId;
 	private long _defaultJournalDDMStructureId;
-	private final long _defaultJournalDDMStructureVersionId;
+	private long _defaultJournalDDMStructureVersionId;
 	private final long _defaultJournalDDMTemplateId;
 	private long _defaultUserId;
 	private final String _dlDDMStructureContent;
