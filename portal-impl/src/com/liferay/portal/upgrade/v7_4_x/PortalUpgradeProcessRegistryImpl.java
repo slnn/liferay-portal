d504916b3f3532253cc846dc32beca7d4735cb55
lily.chi <lily.chi@liferay.com>
2021-03-17 14:56:15 +0800

    Revert "LPS-124338 Drop preferences column and upgrade."
    This reverts commit 9c05f33df7aa26a4afa04457b32bec6184775c44.

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -46,9 +46,6 @@ public class PortalUpgradeProcessRegistryImpl
 		upgradeProcesses.put(new Version(11, 0, 0), new UpgradeAssetEntry());
 
 		upgradeProcesses.put(new Version(11, 0, 1), new UpgradeLayout());
-
-		upgradeProcesses.put(
-			new Version(12, 0, 0), new UpgradePortalPreferences());
 	}
 
 }
