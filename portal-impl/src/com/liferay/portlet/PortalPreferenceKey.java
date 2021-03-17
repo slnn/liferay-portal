3c2cfebbdd9effdd1c682122f703ac5046b4c0e6
lily.chi <lily.chi@liferay.com>
2021-03-17 14:50:39 +0800

    Revert "LPS-124338 Standard toString format."
    This reverts commit d8872c244b3ce9e69fe16a4ae56eb1c3694b840c.

--- portal-impl/src/com/liferay/portlet/PortalPreferenceKey.java
+++ portal-impl/src/com/liferay/portlet/PortalPreferenceKey.java
@@ -87,15 +87,11 @@ public class PortalPreferenceKey {
 
 	@Override
 	public String toString() {
-		StringBundler sb = new StringBundler(5);
+		if (Validator.isNull(_namespace)) {
+			return _key;
+		}
 
-		sb.append("{namespace=");
-		sb.append(_namespace);
-		sb.append(", key=");
-		sb.append(_key);
-		sb.append("}");
-
-		return sb.toString();
+		return StringBundler.concat(_namespace, StringPool.POUND, _key);
 	}
 
 	private final String _key;
