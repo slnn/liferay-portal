694552420401ded95745ff01eb860cb80ac5098b
Preston-Crary <preston.crary@liferay.com>
2021-03-18 13:27:12 -0700

    LPS-129409 Add single value get/update methods.

--- portal-impl/src/com/liferay/portal/service/impl/PortalPreferenceValueLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortalPreferenceValueLocalServiceImpl.java
@@ -17,15 +17,21 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.PortalPreferenceValue;
 import com.liferay.portal.kernel.model.PortalPreferences;
+import com.liferay.portal.kernel.service.SQLStateAcceptor;
 import com.liferay.portal.kernel.service.persistence.PortalPreferenceValuePersistence;
+import com.liferay.portal.kernel.spring.aop.Property;
+import com.liferay.portal.kernel.spring.aop.Retry;
 import com.liferay.portal.service.base.PortalPreferenceValueLocalServiceBaseImpl;
 import com.liferay.portlet.PortalPreferenceKey;
 import com.liferay.portlet.PortalPreferencesImpl;
+import com.liferay.portlet.internal.PreferenceUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * @author Preston Crary
@@ -77,6 +83,114 @@ public class PortalPreferenceValueLocalServiceImpl
 		return new PortalPreferencesImpl(
 			portalPreferences.getOwnerId(), portalPreferences.getOwnerType(),
 			preferenceMap, signedIn);
+	}
+
+	@Override
+	public String getPreferenceValue(
+		long ownerId, int ownerType, String namespace, String key, int index,
+		String defaultValue) {
+
+		PortalPreferences portalPreferences =
+			portalPreferencesPersistence.fetchByO_O(ownerId, ownerType);
+
+		if (portalPreferences == null) {
+			return defaultValue;
+		}
+
+		PortalPreferenceValue portalPreferenceValue =
+			portalPreferenceValuePersistence.fetchByP_I_K_N(
+				portalPreferences.getPortalPreferencesId(), index, key,
+				namespace);
+
+		if (portalPreferenceValue == null) {
+			return defaultValue;
+		}
+
+		String value = PreferenceUtil.getActualValue(
+			portalPreferenceValue.getValue());
+
+		if (value == null) {
+			return defaultValue;
+		}
+
+		return value;
+	}
+
+	@Override
+	@Retry(
+		acceptor = SQLStateAcceptor.class,
+		properties = {
+			@Property(
+				name = SQLStateAcceptor.SQLSTATE,
+				value = SQLStateAcceptor.SQLSTATE_INTEGRITY_CONSTRAINT_VIOLATION + "," + SQLStateAcceptor.SQLSTATE_TRANSACTION_ROLLBACK
+			)
+		}
+	)
+	public void updatePreferenceValue(
+		long ownerId, int ownerType, String namespace, String key, int index,
+		Function<String, String> valueFunction) {
+
+		PortalPreferences portalPreferences =
+			portalPreferencesPersistence.fetchByO_O(ownerId, ownerType);
+
+		if (portalPreferences == null) {
+			long portalPreferencesId = counterLocalService.increment();
+
+			portalPreferences = portalPreferencesPersistence.create(
+				portalPreferencesId);
+
+			portalPreferences.setOwnerId(ownerId);
+			portalPreferences.setOwnerType(ownerType);
+
+			portalPreferences = portalPreferencesPersistence.update(
+				portalPreferences);
+		}
+
+		PortalPreferenceValue portalPreferenceValue =
+			portalPreferenceValuePersistence.fetchByP_I_K_N(
+				portalPreferences.getPortalPreferencesId(), index, key,
+				namespace);
+
+		String originalValue = null;
+
+		if (portalPreferenceValue != null) {
+			originalValue = portalPreferenceValue.getValue();
+		}
+
+		String newValue = valueFunction.apply(
+			PreferenceUtil.getActualValue(originalValue));
+
+		if (newValue == null) {
+			if (portalPreferenceValue != null) {
+				portalPreferenceValuePersistence.remove(portalPreferenceValue);
+			}
+
+			return;
+		}
+
+		newValue = PreferenceUtil.getXMLSafeValue(newValue);
+
+		if (Objects.equals(originalValue, newValue)) {
+			return;
+		}
+
+		if (portalPreferenceValue == null) {
+			long portalPreferenceValueId = counterLocalService.increment(
+				PortalPreferenceValue.class.getName());
+
+			portalPreferenceValue = portalPreferenceValuePersistence.create(
+				portalPreferenceValueId);
+
+			portalPreferenceValue.setPortalPreferencesId(
+				portalPreferences.getPortalPreferencesId());
+			portalPreferenceValue.setIndex(index);
+			portalPreferenceValue.setKey(key);
+			portalPreferenceValue.setNamespace(namespace);
+		}
+
+		portalPreferenceValue.setValue(newValue);
+
+		portalPreferenceValuePersistence.update(portalPreferenceValue);
 	}
 
 	protected static Map<PortalPreferenceKey, List<PortalPreferenceValue>>
