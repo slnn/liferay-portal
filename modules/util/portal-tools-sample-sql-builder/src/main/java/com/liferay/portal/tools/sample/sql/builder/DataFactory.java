8da469068c28fd91dcf9ed90209f7da6f67c9a1c
lily.chi <lily.chi@liferay.com>
2021-03-30 15:27:43 +0800

    LPS-127055 Correct message count for MBStatsUserModel

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4859,7 +4859,27 @@ public class DataFactory {
 		return mbMessageModels;
 	}
 
-	public MBStatsUserModel newMBStatsUserModel(long groupId) {
+	public MBStatsUserModel newMBStatsUserModel(
+		long groupId, CompanyModel companyModel) {
+
+		int messageCount = 0;
+
+		String webId = companyModel.getWebId();
+
+		if (webId.equals("liferay.com")) {
+			messageCount =
+				BenchmarksPropsValues.MAX_MB_CATEGORY_COUNT *
+					BenchmarksPropsValues.MAX_MB_THREAD_COUNT *
+						BenchmarksPropsValues.MAX_MB_MESSAGE_COUNT;
+		}
+		else {
+			messageCount =
+				BenchmarksPropsValues.MAX_VIRTUAL_INSTANCE_MB_CATEGORY_COUNT *
+					BenchmarksPropsValues.MAX_VIRTUAL_INSTANCE_MB_THREAD_COUNT *
+						BenchmarksPropsValues.
+							MAX_VIRTUAL_INSTANCE_MB_MESSAGE_COUNT;
+		}
+
 		MBStatsUserModel mbStatsUserModel = new MBStatsUserModelImpl();
 
 		// PK fields
@@ -4876,10 +4896,7 @@ public class DataFactory {
 
 		// Other fields
 
-		mbStatsUserModel.setMessageCount(
-			BenchmarksPropsValues.MAX_MB_CATEGORY_COUNT *
-				BenchmarksPropsValues.MAX_MB_THREAD_COUNT *
-					BenchmarksPropsValues.MAX_MB_MESSAGE_COUNT);
+		mbStatsUserModel.setMessageCount(messageCount);
 		mbStatsUserModel.setLastPostDate(new Date());
 
 		return mbStatsUserModel;
