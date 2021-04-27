c87613cb7cbde61adb11870f14381eaff52c994e
lily.chi <lily.chi@liferay.com>
2021-04-27 20:46:22 +0800

    LPS-131273 Set "searchLayoutCreated=true" in the Guest group's typeSettings to avoid portal creating the search page for the Guset group during startup automatically so that search bar be disabled for all group when sample.sql.search.bar.enabled=false be set for SampleSQLBuilder

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4135,9 +4135,15 @@ public class DataFactory {
 	}
 
 	public GroupModel newGuestGroupModel() {
+		String typeSettings = "";
+
+		if (!BenchmarksPropsValues.SEARCH_BAR_ENABLED) {
+			typeSettings = "searchLayoutCreated=true";
+		}
+
 		return newGroupModel(
 			_guestGroupId, getClassNameId(Group.class), _guestGroupId,
-			GroupConstants.GUEST, true);
+			GroupConstants.GUEST, 0, true, typeSettings);
 	}
 
 	public UserModel newGuestUserModel() {
@@ -6413,12 +6419,20 @@ public class DataFactory {
 		long groupId, long classNameId, long classPK, String name,
 		boolean site) {
 
-		return newGroupModel(groupId, classNameId, classPK, name, 0, site);
+		return newGroupModel(groupId, classNameId, classPK, name, 0, site, "");
 	}
 
 	protected GroupModel newGroupModel(
 		long groupId, long classNameId, long classPK, String name, int type,
 		boolean site) {
+
+		return newGroupModel(
+			groupId, classNameId, classPK, name, type, site, "");
+	}
+
+	protected GroupModel newGroupModel(
+		long groupId, long classNameId, long classPK, String name, int type,
+		boolean site, String typeSettings) {
 
 		GroupModel groupModel = new GroupModelImpl();
 
@@ -6444,6 +6458,7 @@ public class DataFactory {
 		groupModel.setGroupKey(name);
 		groupModel.setName(name);
 		groupModel.setType(type);
+		groupModel.setTypeSettings(typeSettings);
 		groupModel.setManualMembership(true);
 		groupModel.setMembershipRestriction(
 			GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION);
