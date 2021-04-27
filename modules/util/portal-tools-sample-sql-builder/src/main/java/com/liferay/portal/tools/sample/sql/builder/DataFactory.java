aaaf208ef729f62a80385a3c24c05c0cc591b468
Dante Wang <dante.wang@liferay.com>
2021-04-27 15:31:10 +0800

    LPS-131255 SF, for model field, set it directly

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -6460,6 +6460,7 @@ public class DataFactory {
 
 		layoutModel.setLayoutId(simpleCounter.get());
 		layoutModel.setParentLayoutId(parentLayoutId);
+		layoutModel.setPrivateLayout(privateLayout);
 		layoutModel.setName(
 			"<?xml version=\"1.0\"?><root><name>" + name + "</name></root>");
 		layoutModel.setType(LayoutConstants.TYPE_PORTLET);
@@ -6481,7 +6482,6 @@ public class DataFactory {
 		if (privateLayout) {
 			typeSettingsUnicodeProperties.setProperty(
 				"privateLayout", String.valueOf(privateLayout));
-			layoutModel.setPrivateLayout(true);
 		}
 
 		layoutModel.setTypeSettings(
