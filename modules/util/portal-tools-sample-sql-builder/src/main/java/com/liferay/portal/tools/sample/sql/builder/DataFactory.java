daf927e4b50fb6fd3ede4b1fdd223cec22812ca7
Dante Wang <dante.wang@liferay.com>
2021-04-27 15:22:57 +0800

    LPS-131255 SF, inline

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2354,7 +2354,23 @@ public class DataFactory {
 			_newCounterModel(
 				SocialActivity.class.getName(), _socialActivityCounter.get()));
 
-		counterModels.addAll(_newLayoutCounterModels());
+		long totalLayoutId = 0;
+
+		for (Map.Entry<String, SimpleCounter> entry :
+				_layoutCounters.entrySet()) {
+
+			SimpleCounter simpleCounter = entry.getValue();
+
+			long currentLayoutId = simpleCounter.get();
+
+			counterModels.add(
+				_newCounterModel(entry.getKey(), currentLayoutId));
+
+			totalLayoutId += currentLayoutId;
+		}
+
+		counterModels.add(
+			_newCounterModel(Layout.class.getName(), totalLayoutId + 1));
 
 		return counterModels;
 	}
@@ -7355,30 +7371,6 @@ public class DataFactory {
 		counterModel.setCurrentId(currentId);
 
 		return counterModel;
-	}
-
-	private List<CounterModel> _newLayoutCounterModels() {
-		List<CounterModel> layoutCounterModels = new ArrayList<>();
-
-		long totalLayoutId = 0;
-
-		for (Map.Entry<String, SimpleCounter> entry :
-				_layoutCounters.entrySet()) {
-
-			SimpleCounter simpleCounter = entry.getValue();
-
-			long currentLayoutId = simpleCounter.get();
-
-			layoutCounterModels.add(
-				_newCounterModel(entry.getKey(), currentLayoutId));
-
-			totalLayoutId += currentLayoutId;
-		}
-
-		layoutCounterModels.add(
-			_newCounterModel(Layout.class.getName(), totalLayoutId + 1));
-
-		return layoutCounterModels;
 	}
 
 	private String _readFile(InputStream inputStream) throws Exception {
