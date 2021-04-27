fef2f21a9e8f3be26cabbb92e24c093aceff2373
lily.chi <lily.chi@liferay.com>
2021-04-27 12:50:54 +0800

    LPS-131255 Enable the privateLayout of Layout when it be enabled in the typesettings, fix for duplicate entry issue on layoutId for Commerce data

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -6571,6 +6571,7 @@ public class DataFactory {
 		if (privateLayout) {
 			typeSettingsUnicodeProperties.setProperty(
 				"privateLayout", String.valueOf(privateLayout));
+			layoutModel.setPrivateLayout(true);
 		}
 
 		layoutModel.setTypeSettings(
