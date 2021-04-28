2d7b07a6163b0633a6a036ca3c42dbf49cb22518
lily.chi <lily.chi@liferay.com>
2021-04-28 17:28:53 +0800

    LPS-113497 Add generation of image fragmentEntryLinkModel for each content page layout, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -3842,6 +3842,8 @@ public class DataFactory {
 		Map<String, String> nameSpaces = HashMapBuilder.put(
 			_HEADING_RENDER_KEY, StringUtil.randomId()
 		).put(
+			_IMAGE_RENDER_KEY, StringUtil.randomId()
+		).put(
 			_PARAGRAPH_RENDER_KEY, StringUtil.randomId()
 		).build();
 
@@ -3867,6 +3869,15 @@ public class DataFactory {
 					_readFile("paragraph_configuration.json"),
 					_replaceReleaseInfo(_readFile("paragraph_editValue.json")),
 					0, nameSpaces.get(_PARAGRAPH_RENDER_KEY)));
+
+			fragmentEntryLinkModels.add(
+				newFragmentEntryLinkModel(
+					layoutModel, _IMAGE_RENDER_KEY, "",
+					_readFile(
+						_getFragmentComponentInputStream("image", "html")),
+					_readFile("image_configuration.json"),
+					_readFile("image_editValue.json"), 0,
+					nameSpaces.get(_IMAGE_RENDER_KEY)));
 		}
 
 		return fragmentEntryLinkModels;
@@ -7170,6 +7181,8 @@ public class DataFactory {
 		System.currentTimeMillis() + Time.YEAR;
 
 	private static final String _HEADING_RENDER_KEY = "BASIC_COMPONENT-heading";
+
+	private static final String _IMAGE_RENDER_KEY = "BASIC_COMPONENT-image";
 
 	private static final String _JOURNAL_STRUCTURE_KEY = "BASIC-WEB-CONTENT";
 
