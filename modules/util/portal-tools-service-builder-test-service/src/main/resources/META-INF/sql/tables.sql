db1b568330e55f835c2a218bd76584ea31a181fb
lily.chi <lily.chi@liferay.com>
2021-04-30 15:28:44 +0800

    Revert "LPS-131251 Auto generated"
    This reverts commit 4724a6126d8f6a48f9022534df45ca336e53ed71.

--- modules/util/portal-tools-service-builder-test-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/util/portal-tools-service-builder-test-service/src/main/resources/META-INF/sql/tables.sql
@@ -16,12 +16,6 @@ create table CacheDisabledEntry (
 	name VARCHAR(75) null
 );
 
-create table CacheFieldEntry (
-	cacheFieldEntryId LONG not null primary key,
-	groupId LONG,
-	name VARCHAR(75) null
-);
-
 create table DSLQueryEntry (
 	dslQueryEntryId LONG not null primary key,
 	name VARCHAR(75) null
