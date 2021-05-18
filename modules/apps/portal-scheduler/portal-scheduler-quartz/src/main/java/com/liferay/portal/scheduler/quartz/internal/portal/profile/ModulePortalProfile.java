6747fe58d1b673b0a2ec531e3e73937cf7b9b66d
Dante Wang <dante.wang@liferay.com>
2021-05-18 10:54:39 +0800

    LPS-132380 Register dummy service for TriggerFactory when scheduler.enabled=false

--- modules/apps/portal-scheduler/portal-scheduler-quartz/src/main/java/com/liferay/portal/scheduler/quartz/internal/portal/profile/ModulePortalProfile.java
+++ modules/apps/portal-scheduler/portal-scheduler-quartz/src/main/java/com/liferay/portal/scheduler/quartz/internal/portal/profile/ModulePortalProfile.java
@@ -14,9 +14,12 @@
 
 package com.liferay.portal.scheduler.quartz.internal.portal.profile;
 
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ProxyFactory;
 import com.liferay.portal.profile.BaseDSModulePortalProfile;
 import com.liferay.portal.profile.PortalProfile;
 import com.liferay.portal.scheduler.quartz.internal.QuartzSchedulerEngine;
@@ -30,6 +33,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -55,6 +59,13 @@ public class ModulePortalProfile extends BaseDSModulePortalProfile {
 		}
 		else {
 			supportedPortalProfileNames = Collections.emptySet();
+
+			BundleContext bundleContext = componentContext.getBundleContext();
+
+			bundleContext.registerService(
+				TriggerFactory.class,
+				ProxyFactory.newDummyInstance(TriggerFactory.class),
+				new HashMapDictionary<>());
 		}
 
 		init(
