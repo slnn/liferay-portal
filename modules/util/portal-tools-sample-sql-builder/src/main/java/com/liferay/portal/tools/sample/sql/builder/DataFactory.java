71fb3a82060dc61e1d14fe93e2d90bec738e412e
lily.chi <lily.chi@liferay.com>
2021-05-26 13:31:48 +0800

    LPS-131255 Use Layout counter to generate the plid of Layout like portal did

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -394,6 +394,7 @@ public class DataFactory {
 
 		_timeCounter = new SimpleCounter();
 		_futureDateCounter = new SimpleCounter();
+		_layoutCounter = new SimpleCounter();
 		_resourcePermissionCounter = new SimpleCounter();
 		_socialActivityCounter = new SimpleCounter();
 		_userScreenNameCounter = new SimpleCounter();
@@ -2257,7 +2258,7 @@ public class DataFactory {
 
 		// PK fields
 
-		layoutModel.setPlid(_counter.get());
+		layoutModel.setPlid(_layoutCounter.get());
 
 		// Group instance
 
@@ -2349,23 +2350,17 @@ public class DataFactory {
 			_newCounterModel(
 				SocialActivity.class.getName(), _socialActivityCounter.get()));
 
-		long totalLayoutId = 0;
-
 		for (Map.Entry<String, SimpleCounter> entry :
 				_layoutCounters.entrySet()) {
 
 			SimpleCounter simpleCounter = entry.getValue();
 
-			long currentLayoutId = simpleCounter.get();
-
 			counterModels.add(
-				_newCounterModel(entry.getKey(), currentLayoutId));
-
-			totalLayoutId += currentLayoutId;
+				_newCounterModel(entry.getKey(), simpleCounter.get()));
 		}
 
 		counterModels.add(
-			_newCounterModel(Layout.class.getName(), totalLayoutId + 1));
+			_newCounterModel(Layout.class.getName(), _layoutCounter.get()));
 
 		return counterModels;
 	}
@@ -5689,6 +5684,10 @@ public class DataFactory {
 		return userName;
 	}
 
+	public void setCompanyId(long companyId) {
+		_companyId = companyId;
+	}
+
 	public CProductModel setCProductModelPublishedCPDefinitionId(
 		CProductModel cProductModel,
 		List<CPDefinitionModel> cpDefinitionModels) {
@@ -5712,10 +5711,6 @@ public class DataFactory {
 		}
 
 		return cProductModel;
-	}
-
-	public void setCompanyId(long companyId) {
-		_companyId = companyId;
 	}
 
 	public String toInsertSQL(BaseModel<?> baseModel) {
@@ -6513,7 +6508,7 @@ public class DataFactory {
 
 		// PK fields
 
-		layoutModel.setPlid(_counter.get());
+		layoutModel.setPlid(_layoutCounter.get());
 
 		// Group instance
 
@@ -7310,7 +7305,7 @@ public class DataFactory {
 
 		// PK fields
 
-		layoutModel.setPlid(_counter.get());
+		layoutModel.setPlid(_layoutCounter.get());
 
 		// Group instance
 
@@ -7455,6 +7450,7 @@ public class DataFactory {
 	private final String _journalDDMStructureContent;
 	private final String _journalDDMStructureLayoutContent;
 	private List<String> _lastNames;
+	private final SimpleCounter _layoutCounter;
 	private final Map<String, SimpleCounter> _layoutCounters = new HashMap<>();
 	private final String _layoutPageTemplateStructureRelData;
 	private RoleModel _ownerRoleModel;
