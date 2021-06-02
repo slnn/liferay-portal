ab45eed52b958d025bed41a86cc6e30e492451d7
lily.chi <lily.chi@liferay.com>
2021-06-02 13:00:26 +0800

    LPS-127055 Generate role model for Account Manager as Portlet's ResourcePermission need its roleId

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.tools.sample.sql.builder;
 
+import com.liferay.account.constants.AccountRoleConstants;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.account.model.AccountEntryModel;
 import com.liferay.account.model.AccountEntryUserRelModel;
@@ -5481,6 +5482,12 @@ public class DataFactory {
 
 		roleModels.add(_userRoleModel);
 
+		// Account Manager
+
+		_accountManagerRoleModel = newRoleModel(
+			AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER,
+			RoleConstants.TYPE_ORGANIZATION);
+
 		return roleModels;
 	}
 
@@ -7469,6 +7476,7 @@ public class DataFactory {
 		new PortletPreferencesFactoryImpl();
 
 	private long _accountId;
+	private RoleModel _accountManagerRoleModel;
 	private RoleModel _administratorRoleModel;
 	private Map<Long, SimpleCounter>[] _assetCategoryCounters;
 	private final Map<Long, List<AssetCategoryModel>>[]
