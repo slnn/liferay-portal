d05c39845a4a2f7faa298a726b6820316146d175
lily.chi <lily.chi@liferay.com>
2021-06-09 11:01:31 +0800

    LPS-113497 Add generation of LoginPortlet FragmentEntryLinkModel for each content page layout, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -3859,7 +3859,7 @@ public class DataFactory {
 
 		return fragmentEntryLinkModel;
 	}
-	
+
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
 			List<LayoutModel> layoutModels)
 		throws Exception {
@@ -3873,9 +3873,17 @@ public class DataFactory {
 			_IMAGE_RENDER_KEY, StringUtil.randomId()
 		).put(
 			_PARAGRAPH_RENDER_KEY, StringUtil.randomId()
+		).put(
+			"LoginPortlet", StringUtil.randomId()
 		).build();
 
 		for (LayoutModel layoutModel : layoutModels) {
+			fragmentEntryLinkModels.add(
+				newFragmentEntryLinkModel(
+					layoutModel, "", "", "", "",
+					_readFile("loginPortlet_editValue.json"), 0,
+					nameSpaces.get("LoginPortlet")));
+
 			fragmentEntryLinkModels.add(
 				newFragmentEntryLinkModel(
 					layoutModel, _HEADING_RENDER_KEY,
@@ -7162,7 +7170,7 @@ public class DataFactory {
 		System.currentTimeMillis() + Time.YEAR;
 
 	private static final String _HEADING_RENDER_KEY = "BASIC_COMPONENT-heading";
-	
+
 	private static final String _IMAGE_RENDER_KEY = "BASIC_COMPONENT-image";
 
 	private static final String _JOURNAL_STRUCTURE_KEY = "BASIC-WEB-CONTENT";
