6b30501e1b7db7e05248bbd61d1f071d0a740004
lily.chi <lily.chi@liferay.com>
2021-06-09 10:45:33 +0800

    LPS-113497 Add method to get the inputStream of fragment component, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -356,6 +356,8 @@ import java.lang.reflect.Method;
 
 import java.math.BigDecimal;
 
+import java.net.URL;
+
 import java.sql.Types;
 
 import java.text.Format;
@@ -7072,6 +7074,22 @@ public class DataFactory {
 		catch (ReflectiveOperationException reflectiveOperationException) {
 			ReflectionUtil.throwException(reflectiveOperationException);
 		}
+	}
+
+	private InputStream _getFragmentComponentInputStream(
+			String fragmentName, String suffix)
+		throws Exception {
+
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		URL url = classLoader.getResource(
+			StringBundler.concat(
+				"com/liferay/fragment/collection/contributor/basic/component",
+				"/dependencies/", fragmentName, "/index.", suffix));
+
+		return url.openStream();
 	}
 
 	private String _getMBDiscussionCombinedClassName(Class<?> clazz) {
