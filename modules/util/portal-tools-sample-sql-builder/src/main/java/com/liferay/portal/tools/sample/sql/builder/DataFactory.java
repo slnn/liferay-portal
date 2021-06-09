8230dfe0f65e5be0502b108d2ae7caedf7b7038b
Dante Wang <dante.wang@liferay.com>
2021-06-09 11:21:14 +0800

    LPS-113497 SF, no need to use map

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -263,7 +263,6 @@ import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
@@ -3865,22 +3864,17 @@ public class DataFactory {
 		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
 			new ArrayList<>();
 
-		Map<String, String> nameSpaces = HashMapBuilder.put(
-			_HEADING_RENDER_KEY, StringUtil.randomId()
-		).put(
-			_IMAGE_RENDER_KEY, StringUtil.randomId()
-		).put(
-			_PARAGRAPH_RENDER_KEY, StringUtil.randomId()
-		).put(
-			"LoginPortlet", StringUtil.randomId()
-		).build();
+		String headingRenderNamespace = StringUtil.randomId();
+		String imageRenderNamespace = StringUtil.randomId();
+		String paragraphRenderNamespace = StringUtil.randomId();
+		String loginPortletNamespace = StringUtil.randomId();
 
 		for (LayoutModel layoutModel : layoutModels) {
 			fragmentEntryLinkModels.add(
 				newFragmentEntryLinkModel(
 					layoutModel, "", "", "", "",
 					_readFile("loginPortlet_editValue.json"), 0,
-					nameSpaces.get("LoginPortlet")));
+					loginPortletNamespace));
 
 			fragmentEntryLinkModels.add(
 				newFragmentEntryLinkModel(
@@ -3891,7 +3885,7 @@ public class DataFactory {
 						_getFragmentComponentInputStream("heading", "html")),
 					_readFile("heading_configuration.json"),
 					_readFile("heading_editValue.json"), 0,
-					nameSpaces.get(_HEADING_RENDER_KEY)));
+					headingRenderNamespace));
 
 			fragmentEntryLinkModels.add(
 				newFragmentEntryLinkModel(
@@ -3902,7 +3896,7 @@ public class DataFactory {
 						_getFragmentComponentInputStream("paragraph", "html")),
 					_readFile("paragraph_configuration.json"),
 					_replaceReleaseInfo(_readFile("paragraph_editValue.json")),
-					0, nameSpaces.get(_PARAGRAPH_RENDER_KEY)));
+					0, paragraphRenderNamespace));
 
 			fragmentEntryLinkModels.add(
 				newFragmentEntryLinkModel(
@@ -3911,7 +3905,7 @@ public class DataFactory {
 						_getFragmentComponentInputStream("image", "html")),
 					_readFile("image_configuration.json"),
 					_readFile("image_editValue.json"), 0,
-					nameSpaces.get(_IMAGE_RENDER_KEY)));
+					imageRenderNamespace));
 		}
 
 		return fragmentEntryLinkModels;
