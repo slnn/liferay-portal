07d411521a6b7ce58433bd9a6b0322b4b1e574b9
lily.chi <lily.chi@liferay.com>
2021-06-29 13:41:10 +0800

    LPS-127055 Generate data for ClassNameModel, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -424,6 +424,8 @@ public class DataFactory {
 		models.add(_getMBDiscussionCombinedClassName(BlogsEntry.class));
 		models.add(_getMBDiscussionCombinedClassName(WikiPage.class));
 
+		_readFiletoList("ClassNameModels.txt", models);
+
 		for (String model : models) {
 			ClassNameModel classNameModel = new ClassNameModelImpl();
 
@@ -7685,6 +7687,21 @@ public class DataFactory {
 
 	private String _readFile(String resourceName) throws Exception {
 		return _readFile(getResourceInputStream(resourceName));
+	}
+
+	private void _readFiletoList(String fileName, List<String> lines)
+		throws Exception {
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(
+					new InputStreamReader(getResourceInputStream(fileName)))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lines.add(line);
+			}
+		}
 	}
 
 	private String _replaceReleaseInfo(String resource) throws Exception {
