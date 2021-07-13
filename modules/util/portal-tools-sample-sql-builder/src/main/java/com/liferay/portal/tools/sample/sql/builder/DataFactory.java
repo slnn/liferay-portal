98d1485c77a1b91f6d65849860620089c5fb9c31
lily.chi <lily.chi@liferay.com>
2021-07-13 15:21:23 +0800

    LPS-135303 Set the default locale for DDMForm to avoid NPE

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -7808,6 +7808,8 @@ public class DataFactory {
 		String definition = structureElementRootElement.asXML();
 
 		DDMForm ddmForm = _deserialize(definition, xsdDDMFormDeserializer);
+		
+		ddmForm.setDefaultLocale(LocaleUtil.ENGLISH);
 
 		ddmForm = ddm.updateDDMFormDefaultLocale(ddmForm, locale);
 
