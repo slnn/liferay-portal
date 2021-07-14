87fdacc4b48787f2d0646b265ef8fb568bda8966
lily.chi <lily.chi@liferay.com>
2021-07-14 21:45:44 +0800

    LPS-135705 Generate data for RoleModel, prep next

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -3677,7 +3677,7 @@ public class DataFactory {
 
 			String definition = _ddmStructureDefinitionMap.get(
 				structureKey + ddmStructureModel.getClassName());
-			
+
 			definition = definition.replaceAll(StringPool.QUOTE, "\\\\\"");
 
 			definition = definition.replaceAll(StringPool.APOSTROPHE, "\\\\\'");
@@ -5764,6 +5764,48 @@ public class DataFactory {
 		_accountManagerRoleModel = newRoleModel(
 			AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER,
 			RoleConstants.TYPE_ORGANIZATION);
+
+		// Asset Library Administrator
+
+		_assetLibraryAdministratorRoleModel = newRoleModel(
+			"Asset Library Administrator", RoleConstants.TYPE_DEPOT);
+
+		roleModels.add(_assetLibraryAdministratorRoleModel);
+
+		// Asset Library Content Reviewer
+
+		_assetLibraryContentReviewerRoleModel = newRoleModel(
+			"Asset Library Content Reviewer", RoleConstants.TYPE_DEPOT);
+
+		roleModels.add(_assetLibraryContentReviewerRoleModel);
+
+		// Asset Library Owner
+
+		_assetLibraryOwnerRoleModel = newRoleModel(
+			"Asset Library Owner", RoleConstants.TYPE_DEPOT);
+
+		roleModels.add(_assetLibraryOwnerRoleModel);
+
+		// Portal Content Reviewer
+
+		_portalContentReviewerRoleModel = newRoleModel(
+			"Portal Content Reviewer", RoleConstants.TYPE_REGULAR);
+
+		roleModels.add(_portalContentReviewerRoleModel);
+
+		// Organization Content Reviewer
+
+		_organizationContentReviewerRoleModel = newRoleModel(
+			"Organization Content Reviewer", RoleConstants.TYPE_ORGANIZATION);
+
+		roleModels.add(_organizationContentReviewerRoleModel);
+
+		// Site Content Reviewer
+
+		_siteContentReviewerRoleModel = newRoleModel(
+			"Site Content Reviewer", RoleConstants.TYPE_SITE);
+
+		roleModels.add(_siteContentReviewerRoleModel);
 
 		return roleModels;
 	}
@@ -8096,6 +8138,9 @@ public class DataFactory {
 	private final long[] _assetClassNameIds;
 	private final Map<Long, Integer> _assetClassNameIdsIndexes =
 		new HashMap<>();
+	private RoleModel _assetLibraryAdministratorRoleModel;
+	private RoleModel _assetLibraryContentReviewerRoleModel;
+	private RoleModel _assetLibraryOwnerRoleModel;
 	private final Map<Long, Integer> _assetPublisherQueryStartIndexes =
 		new HashMap<>();
 	private Map<Long, SimpleCounter>[] _assetTagCounters;
@@ -8152,7 +8197,9 @@ public class DataFactory {
 	private final String _layoutPageTemplateStructureRelData;
 	private final SimpleCounter _layoutPlidCounter;
 	private final SimpleCounter _layoutSetIdCounter;
+	private RoleModel _organizationContentReviewerRoleModel;
 	private RoleModel _ownerRoleModel;
+	private RoleModel _portalContentReviewerRoleModel;
 	private final SimpleCounter _portletPreferenceValueIdCounter;
 	private final List<SampleSQLBuilderResourcePermissionModel>
 		_portletResourcePermissionModelList = new ArrayList<>();
@@ -8163,6 +8210,7 @@ public class DataFactory {
 	private final SimpleCounter _resourcePermissionIdCounter;
 	private long _sampleUserId;
 	private final Format _simpleDateFormat;
+	private RoleModel _siteContentReviewerRoleModel;
 	private RoleModel _siteMemberRoleModel;
 	private final SimpleCounter _socialActivityIdCounter;
 	private final SimpleCounter _timeCounter;
