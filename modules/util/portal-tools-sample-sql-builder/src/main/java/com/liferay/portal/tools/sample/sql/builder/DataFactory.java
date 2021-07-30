2ea47dcc64a089984c9eaa285e2d39ce31f8914b
lily.chi <lily.chi@liferay.com>
2021-07-30 16:04:58 +0800

    LPS-135298 SF,set accountId like companyId

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -5501,6 +5501,10 @@ public class DataFactory {
 		return userName;
 	}
 
+	public void setAccountId(long accountId) {
+		_accountId = accountId;
+	}
+
 	public void setCompanyId(long companyId) {
 		_companyId = companyId;
 	}
