76b9c4538ded668bcb62e0255cec9f2768065813
lily.chi <lily.chi@liferay.com>
2021-07-30 17:19:20 +0800

    LPS-135298 Set the webId like companyId, no need pass the companyModel

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -5388,12 +5388,10 @@ public class DataFactory {
 			getClassNameId(WikiPage.class), wikiPageModel.getResourcePrimKey());
 	}
 
-	public List<UserModel> newUserModels(CompanyModel companyModel) {
+	public List<UserModel> newUserModels() {
 		int userCount = 0;
 
-		String webId = companyModel.getWebId();
-
-		if (webId.equals("liferay.com")) {
+		if (_webId.equals("liferay.com")) {
 			userCount = BenchmarksPropsValues.MAX_USER_COUNT;
 		}
 		else {
@@ -5420,7 +5418,7 @@ public class DataFactory {
 			_defaultUserId, GroupConstants.USER_PERSONAL_SITE, false);
 	}
 
-	public VirtualHostModel newVirtualHostModel(CompanyModel companyModel) {
+	public VirtualHostModel newVirtualHostModel() {
 		VirtualHostModel virtualHostModel = new VirtualHostModelImpl();
 
 		//  PK fields
@@ -5433,14 +5431,12 @@ public class DataFactory {
 
 		// Other fields
 
-		String webId = companyModel.getWebId();
-
-		if (webId.equals("liferay.com")) {
+		if (_webId.equals("liferay.com")) {
 			virtualHostModel.setHostname(
 				BenchmarksPropsValues.VIRTUAL_HOST_NAME);
 		}
 		else {
-			virtualHostModel.setHostname(webId);
+			virtualHostModel.setHostname(_webId);
 		}
 
 		return virtualHostModel;
@@ -5507,6 +5503,10 @@ public class DataFactory {
 
 	public void setCompanyId(long companyId) {
 		_companyId = companyId;
+	}
+
+	public void setWebId(String webId) {
+		_webId = webId;
 	}
 
 	public String toInsertSQL(BaseModel<?> baseModel) {
@@ -7069,5 +7069,6 @@ public class DataFactory {
 	private final SimpleCounter _timeCounter;
 	private RoleModel _userRoleModel;
 	private final SimpleCounter _userScreenNameCounter;
+	private String _webId;
 
 }
