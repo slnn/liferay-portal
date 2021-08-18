682cd6cd7eb927f5a33b59dbf949814db3615729
lily.chi <lily.chi@liferay.com>
2021-08-18 15:41:25 +0800

    LPS-X (Integration test) No need company.csv since it will be generated on SampleSQLBuilder side

--- modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
+++ modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
@@ -302,8 +302,6 @@ public class CompanySampleDataGenerationTest {
 
 		try (LoggingTimer loggingTimer = new LoggingTimer(
 				outputDirFile.getAbsolutePath());
-			BufferedWriter companyBufferedWriter = Files.newBufferedWriter(
-				outputDirPath.resolve("company.csv"));
 			BufferedWriter classNameTableBufferedWriter =
 				Files.newBufferedWriter(
 					outputDirPath.resolve("classNameTable.csv"));
@@ -323,9 +321,6 @@ public class CompanySampleDataGenerationTest {
 			Collections.sort(keys);
 
 			for (String key : keys) {
-				companyBufferedWriter.append(CSVUtil.encode(key));
-				companyBufferedWriter.newLine();
-
 				hostBufferedWriter.append("127.0.0.1 ");
 				hostBufferedWriter.append(CSVUtil.encode(key));
 				hostBufferedWriter.newLine();
@@ -356,7 +351,6 @@ public class CompanySampleDataGenerationTest {
 
 			classNameTableBufferedWriter.flush();
 			companyTableBufferedWriter.flush();
-			companyBufferedWriter.flush();
 			hostBufferedWriter.flush();
 			roleTableBufferedWriter.flush();
 			userBufferedWriter.flush();
