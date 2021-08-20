741e1cfe2671dfe8aeff9b59f471ceee7f633d35
lily.chi <lily.chi@liferay.com>
2021-08-20 10:32:41 +0800

    LPS-X (Integration test) Remove guest user generation since it has been generated by Integration test

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4098,10 +4098,6 @@ public class DataFactory {
 			GroupConstants.GUEST, 0, typeSettings, true);
 	}
 
-	public UserModel newGuestUserModel() {
-		return newUserModel(_counter.get(), "Test", "Test", "Test", false);
-	}
-
 	public JournalArticleLocalizationModel newJournalArticleLocalizationModel(
 		JournalArticleModel journalArticleModel) {
 
