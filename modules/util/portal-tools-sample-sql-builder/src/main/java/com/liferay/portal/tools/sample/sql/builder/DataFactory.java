5d889a4414d36a8df43b90499c304dbef312e3db
lily.chi <lily.chi@liferay.com>
2021-08-20 10:55:08 +0800

    LPS-X (Integration test) Init these counters based on the output of Integration test

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -121,8 +121,6 @@ import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.constants.DDMStructureConstants;
 import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
 import com.liferay.dynamic.data.mapping.model.DDMContent;
-import com.liferay.dynamic.data.mapping.model.DDMField;
-import com.liferay.dynamic.data.mapping.model.DDMFieldAttribute;
 import com.liferay.dynamic.data.mapping.model.DDMFieldAttributeModel;
 import com.liferay.dynamic.data.mapping.model.DDMFieldModel;
 import com.liferay.dynamic.data.mapping.model.DDMStorageLinkModel;
@@ -154,7 +152,6 @@ import com.liferay.fragment.model.FragmentEntryModel;
 import com.liferay.fragment.model.impl.FragmentCollectionModelImpl;
 import com.liferay.fragment.model.impl.FragmentEntryLinkModelImpl;
 import com.liferay.fragment.model.impl.FragmentEntryModelImpl;
-import com.liferay.friendly.url.model.FriendlyURLEntryLocalization;
 import com.liferay.friendly.url.model.FriendlyURLEntryLocalizationModel;
 import com.liferay.friendly.url.model.FriendlyURLEntryMappingModel;
 import com.liferay.friendly.url.model.FriendlyURLEntryModel;
@@ -392,22 +389,22 @@ public class DataFactory {
 
 		_defaultUserIdMap = _initDefaultUserIdMap();
 
-		int groupCount =
-			BenchmarksPropsValues.MAX_GROUP_COUNT +
-				BenchmarksPropsValues.MAX_COMMERCE_GROUP_COUNT;
+		_countersMap = _initCountersMap();
 
-		int totalGroupCount = groupCount * _maxVirtualInstanceCount;
-
-		_counter = new SimpleCounter(totalGroupCount + 1);
+		_counter = new SimpleCounter(_countersMap.get(Counter.class.getName()));
 
 		_groupCounter = new SimpleCounter(1);
 
 		_timeCounter = new SimpleCounter();
 		_futureDateCounter = new SimpleCounter();
-		_layoutPlidCounter = new SimpleCounter();
-		_layoutSetIdCounter = new SimpleCounter();
-		_portletPreferenceValueIdCounter = new SimpleCounter();
-		_resourcePermissionIdCounter = new SimpleCounter();
+		_layoutPlidCounter = new SimpleCounter(
+			_countersMap.get(Layout.class.getName()));
+		_layoutSetIdCounter = new SimpleCounter(
+			_countersMap.get(LayoutSet.class.getName()));
+		_portletPreferenceValueIdCounter = new SimpleCounter(
+			_countersMap.get(PortletPreferenceValue.class.getName()));
+		_resourcePermissionIdCounter = new SimpleCounter(
+			_countersMap.get(ResourcePermission.class.getName()));
 		_socialActivityIdCounter = new SimpleCounter();
 		_userScreenNameCounter = new SimpleCounter();
 
@@ -2296,14 +2293,6 @@ public class DataFactory {
 
 		counterModels.add(
 			_newCounterModel(Counter.class.getName(), _counter.get()));
-		counterModels.add(
-			_newCounterModel(DDMField.class.getName(), _counter.get()));
-		counterModels.add(
-			_newCounterModel(
-				DDMFieldAttribute.class.getName(), _counter.get()));
-		counterModels.add(
-			_newCounterModel(
-				FriendlyURLEntryLocalization.class.getName(), _counter.get()));
 		counterModels.add(
 			_newCounterModel(
 				PortletPreferenceValue.class.getName(),
@@ -6843,6 +6832,24 @@ public class DataFactory {
 		return sampleSQLBuilderCompanyModels;
 	}
 
+	private Map<String, Long> _initCountersMap() throws Exception {
+		Map<String, Long> countersMap = new HashMap<>();
+
+		UnsyncBufferedReader unsyncBufferedReader = new UnsyncBufferedReader(
+			new InputStreamReader(
+				getResourceInputStream("csv/counterTable.csv")));
+
+		String line = null;
+
+		while ((line = unsyncBufferedReader.readLine()) != null) {
+			String[] items = line.split(",");
+
+			countersMap.put(items[0], GetterUtil.getLong(items[1]));
+		}
+
+		return countersMap;
+	}
+
 	private Map<Long, Long> _initDefaultUserIdMap() throws Exception {
 		Map<Long, Long> defautUserIdMap = new HashMap<>();
 
@@ -7004,6 +7011,7 @@ public class DataFactory {
 		new HashMap<>();
 	private long _companyId;
 	private final SimpleCounter _counter;
+	private final Map<String, Long> _countersMap;
 	private final Map<Long, CPInstanceModel> _cpInstanceModels =
 		new HashMap<>();
 	private final PortletPreferencesImpl
