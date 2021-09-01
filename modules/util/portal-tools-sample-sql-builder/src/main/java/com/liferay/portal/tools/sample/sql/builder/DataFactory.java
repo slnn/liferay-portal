aa221fd1a2586098286724038a910bc8f8fcca5e
lily.chi <lily.chi@liferay.com>
2021-09-01 15:07:44 +0800

    LPS-X (Integration test) Fix typo

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -3971,7 +3971,7 @@ public class DataFactory {
 
 	public GroupModel newGlobalGroupModel() {
 		SampleSQLBuilderGroupModel sampleSQLBuilderGlobalGroupModel =
-			_sampleSQLBuilderGuestGroupModelMap.get(_companyId);
+			_sampleSQLBuilderGlobalGroupModelMap.get(_companyId);
 
 		_globalGroupId = sampleSQLBuilderGlobalGroupModel.getGroupId();
 
@@ -7076,7 +7076,7 @@ public class DataFactory {
 					String groupKey = sampleSQLBuilderGroupModel.getGroupKey();
 
 					if (groupKey.equals(GroupConstants.GLOBAL)) {
-						_sampleSQLBuilderGobalGroupModelMap.put(
+						_sampleSQLBuilderGlobalGroupModelMap.put(
 							companyId, sampleSQLBuilderGroupModel);
 					}
 
@@ -7305,7 +7305,7 @@ public class DataFactory {
 	private final List<SampleSQLBuilderCompanyModel>
 		_sampleSQLBuilderCompanyModels;
 	private final Map<Long, SampleSQLBuilderGroupModel>
-		_sampleSQLBuilderGobalGroupModelMap = new HashMap<>();
+		_sampleSQLBuilderGlobalGroupModelMap = new HashMap<>();
 	private final Map<Long, SampleSQLBuilderGroupModel>
 		_sampleSQLBuilderGuestGroupModelMap = new HashMap<>();
 	private final Map<Long, List<SampleSQLBuilderRoleModel>>
