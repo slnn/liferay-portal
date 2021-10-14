e11a5d9f12d775d927cbfc42ca3581336dbf81f3
lily.chi <lily.chi@liferay.com>
2021-10-14 11:29:13 +0800

    LPS-X (Integration test Blog) Update the db table Counter column that created by integration test

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -5408,6 +5408,12 @@ public class DataFactory {
 			", 0, null);");
 	}
 
+	public Boolean updateCounter(CounterModel counterModel) {
+		Set<String> counterNames = _countersMap.keySet();
+
+		return counterNames.contains(counterModel.getName());
+	}
+
 	protected ObjectValuePair<String[], Integer>
 		getAssetPublisherAssetCategoriesQueryValues(
 			List<AssetCategoryModel> assetCategoryModels, int index) {
