5fe36e78417e448928a911bfc66c3309ec2b6619
Preston-Crary <preston.crary@liferay.com>
2021-10-29 15:08:31 -0700

    LPS-140646 Make projections consistent.

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsStatsUserLocalServiceImpl.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsStatsUserLocalServiceImpl.java
@@ -56,6 +56,7 @@ public class BlogsStatsUserLocalServiceImpl
 
 		List<Object[]> results = _blogsEntryPersistence.dslQuery(
 			DSLQueryFactoryUtil.select(
+				BlogsEntryTable.INSTANCE.groupId,
 				BlogsEntryTable.INSTANCE.userId, _lastPostDateExpression,
 				_entryCountExpression, _ratingsTotalEntriesExpression,
 				_ratingsAverageScoreExpression, _ratingsTotalScoreExpression
@@ -84,7 +85,7 @@ public class BlogsStatsUserLocalServiceImpl
 				start, end
 			));
 
-		return _getBlogsStatsUsersList(groupId, results);
+		return _getBlogsStatsUsersList(results);
 	}
 
 	@Override
@@ -93,6 +94,7 @@ public class BlogsStatsUserLocalServiceImpl
 
 		List<Object[]> results = _blogsEntryPersistence.dslQuery(
 			DSLQueryFactoryUtil.select(
+				BlogsEntryTable.INSTANCE.groupId,
 				BlogsEntryTable.INSTANCE.userId, _lastPostDateExpression,
 				_entryCountExpression, _ratingsTotalEntriesExpression,
 				_ratingsAverageScoreExpression, _ratingsTotalScoreExpression
@@ -121,7 +123,7 @@ public class BlogsStatsUserLocalServiceImpl
 				start, end
 			));
 
-		return _getBlogsStatsUsersList(groupId, results);
+		return _getBlogsStatsUsersList(results);
 	}
 
 	@Override
@@ -168,6 +170,7 @@ public class BlogsStatsUserLocalServiceImpl
 
 		List<Object[]> results = _blogsEntryPersistence.dslQuery(
 			DSLQueryFactoryUtil.select(
+				BlogsEntryTable.INSTANCE.groupId,
 				BlogsEntryTable.INSTANCE.userId, _lastPostDateExpression,
 				_entryCountExpression, _ratingsTotalEntriesExpression,
 				_ratingsAverageScoreExpression, _ratingsTotalScoreExpression
@@ -192,17 +195,16 @@ public class BlogsStatsUserLocalServiceImpl
 				BlogsEntryTable.INSTANCE.userId
 			));
 
-		List<BlogsStatsUser> blogsStatsUserList = _getBlogsStatsUsersList(
-			groupId, results);
+		List<BlogsStatsUser> blogsStatsUsers = _getBlogsStatsUsersList(results);
 
-		if (blogsStatsUserList.isEmpty()) {
+		if (blogsStatsUsers.isEmpty()) {
 			throw new NoSuchStatsUserException(
 				StringBundler.concat(
 					"No BlogsStatsUser exists with the key {groupId=", groupId,
 					", userId=", userId, "}"));
 		}
 
-		return blogsStatsUserList.get(0);
+		return blogsStatsUsers.get(0);
 	}
 
 	private List<BlogsStatsUser> _getBlogsStatsUsersList(
@@ -218,30 +220,6 @@ public class BlogsStatsUserLocalServiceImpl
 			Long ratingsTotalEntries = (Long)columns[4];
 			Double ratingsAverageScore = (Double)columns[5];
 			Double ratingsTotalScore = (Double)columns[6];
-
-			blogsStatsUsers.add(
-				new BlogsStatsUserImpl(
-					groupId, userId, lastPostDate, entryCount,
-					GetterUtil.get(ratingsTotalEntries, 0L),
-					GetterUtil.get(ratingsAverageScore, 0D),
-					GetterUtil.get(ratingsTotalScore, 0D)));
-		}
-
-		return blogsStatsUsers;
-	}
-
-	private List<BlogsStatsUser> _getBlogsStatsUsersList(
-		long groupId, List<Object[]> results) {
-
-		List<BlogsStatsUser> blogsStatsUsers = new ArrayList<>(results.size());
-
-		for (Object[] columns : results) {
-			Long userId = (Long)columns[0];
-			Date lastPostDate = (Date)columns[1];
-			Long entryCount = (Long)columns[2];
-			Long ratingsTotalEntries = (Long)columns[3];
-			Double ratingsAverageScore = (Double)columns[4];
-			Double ratingsTotalScore = (Double)columns[5];
 
 			blogsStatsUsers.add(
 				new BlogsStatsUserImpl(
