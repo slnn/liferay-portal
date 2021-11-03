aaa3bfda215f758f274f989ebd315acfdd070b61
lily.chi <lily.chi@liferay.com>
2021-11-03 15:51:23 +0800

    LPS-142065 Use a counter to set the name of DLFileEntry like portal did

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -389,6 +389,7 @@ public class DataFactory {
 		_counter = new SimpleCounter(
 			BenchmarksPropsValues.MAX_GROUP_COUNT +
 				BenchmarksPropsValues.MAX_COMMERCE_GROUP_COUNT + 1);
+		_dLFileEntryIdCounter = new SimpleCounter();
 		_timeCounter = new SimpleCounter();
 		_futureDateCounter = new SimpleCounter();
 		_layoutPlidCounter = new SimpleCounter();
@@ -2302,6 +2303,9 @@ public class DataFactory {
 				DDMFieldAttribute.class.getName(), _counter.get()));
 		counterModels.add(
 			_newCounterModel(
+				DLFileEntry.class.getName(), _dLFileEntryIdCounter.get()));
+		counterModels.add(
+			_newCounterModel(
 				FriendlyURLEntryLocalization.class.getName(), _counter.get()));
 		counterModels.add(
 			_newCounterModel(
@@ -3624,7 +3628,7 @@ public class DataFactory {
 
 		dlFileEntryModel.setRepositoryId(dlFolderModel.getRepositoryId());
 		dlFileEntryModel.setFolderId(dlFolderModel.getFolderId());
-		dlFileEntryModel.setName(name);
+		dlFileEntryModel.setName(String.valueOf(_dLFileEntryIdCounter.get()));
 		dlFileEntryModel.setFileName(name + "." + extension);
 		dlFileEntryModel.setExtension(extension);
 		dlFileEntryModel.setMimeType(mimeType);
@@ -7288,6 +7292,7 @@ public class DataFactory {
 	private long _defaultUserId;
 	private final String _dlDDMStructureContent;
 	private final String _dlDDMStructureLayoutContent;
+	private final SimpleCounter _dLFileEntryIdCounter;
 	private List<String> _firstNames;
 	private final SimpleCounter _futureDateCounter;
 	private long _globalGroupId;
