9bed1d0e88fa8a31aa78aaac6e80568dc5c34662
lily.chi <lily.chi@liferay.com>
2021-11-19 11:58:06 +0800

    LPS-X (Integration test AssetPublisher) Use map instead of array here since the groupId is not start from 1 with the data that generated by the integration test

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -466,10 +466,7 @@ public class DataFactory {
 		}
 
 		if (_assetCategoryCounters == null) {
-			_assetCategoryCounters =
-				(Map<Long, SimpleCounter>[])new HashMap<?, ?>
-					[(_maxVirtualInstanceCount + 1) *
-						BenchmarksPropsValues.MAX_GROUP_COUNT];
+			_assetCategoryCounters = new HashMap<>();
 		}
 
 		SimpleCounter counter = getSimpleCounter(
@@ -510,9 +507,7 @@ public class DataFactory {
 		}
 
 		if (_assetTagCounters == null) {
-			_assetTagCounters = (Map<Long, SimpleCounter>[])new HashMap<?, ?>
-				[(_maxVirtualInstanceCount + 1) *
-					BenchmarksPropsValues.MAX_GROUP_COUNT];
+			_assetTagCounters = new HashMap<>();
 		}
 
 		SimpleCounter counter = getSimpleCounter(
@@ -5530,16 +5525,16 @@ public class DataFactory {
 	}
 
 	protected SimpleCounter getSimpleCounter(
-		Map<Long, SimpleCounter>[] simpleCountersArray, long groupId,
+		Map<Long, Map<Long, SimpleCounter>> simpleCountersMap, long groupId,
 		long classNameId) {
 
-		Map<Long, SimpleCounter> simpleCounters =
-			simpleCountersArray[(int)groupId - 1];
+		Map<Long, SimpleCounter> simpleCounters = simpleCountersMap.get(
+			groupId);
 
 		if (simpleCounters == null) {
 			simpleCounters = new HashMap<>();
 
-			simpleCountersArray[(int)groupId - 1] = simpleCounters;
+			simpleCountersMap.put(groupId, simpleCounters);
 		}
 
 		SimpleCounter simpleCounter = simpleCounters.get(classNameId);
@@ -7299,7 +7294,7 @@ public class DataFactory {
 		new PortletPreferencesFactoryImpl();
 
 	private RoleModel _administratorRoleModel;
-	private Map<Long, SimpleCounter>[] _assetCategoryCounters;
+	private Map<Long, Map<Long, SimpleCounter>> _assetCategoryCounters;
 	private final Map<Long, Map<Long, List<AssetCategoryModel>>>
 		_assetCategoryModelsMaps = new HashMap<>();
 	private final long[] _assetClassNameIds;
@@ -7307,7 +7302,7 @@ public class DataFactory {
 		new HashMap<>();
 	private final Map<Long, Integer> _assetPublisherQueryStartIndexes =
 		new HashMap<>();
-	private Map<Long, SimpleCounter>[] _assetTagCounters;
+	private Map<Long, Map<Long, SimpleCounter>> _assetTagCounters;
 	private final Map<Long, Map<Long, List<AssetTagModel>>>
 		_assetTagModelsMaps = new HashMap<>();
 	private final Map<String, ClassNameModel> _classNameModels =
