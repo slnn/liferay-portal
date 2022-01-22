3dcd0f0e70f9b22157805833f04defd3198f538e
lily.chi <lily.chi@liferay.com>
2022-01-22 16:28:50 +0800

    LPS-145373 Export the data of table Counter, prep next

--- modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
+++ modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
@@ -15,6 +15,7 @@
 package com.liferay.company.sample.data.generation.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -278,6 +279,19 @@ public class CompanySampleDataGenerationTest {
 		companyTableBufferedWriter.newLine();
 	}
 
+	private void _exportCounterTableData(
+			BufferedWriter counterTableBufferedWriter)
+		throws Exception {
+
+		for (String name : _counterLocalService.getNames()) {
+			counterTableBufferedWriter.append(String.valueOf(name));
+			counterTableBufferedWriter.append(StringPool.COMMA);
+			counterTableBufferedWriter.append(
+				String.valueOf(_counterLocalService.increment(name)));
+			counterTableBufferedWriter.newLine();
+		}
+	}
+
 	private void _exportCSVs() throws Exception {
 		String outputDir = PropsUtil.get("sample.data.output.dir");
 
@@ -305,6 +319,8 @@ public class CompanySampleDataGenerationTest {
 					outputDirPath.resolve("classNameTable.csv"));
 			BufferedWriter companyTableBufferedWriter = Files.newBufferedWriter(
 				outputDirPath.resolve("companyTable.csv"));
+			BufferedWriter counterTableBufferedWriter = Files.newBufferedWriter(
+				outputDirPath.resolve("counterTable.csv"));
 			BufferedWriter defaultUserIdBufferedWriter =
 				Files.newBufferedWriter(
 					outputDirPath.resolve("defaultUserId.csv"));
@@ -353,10 +369,12 @@ public class CompanySampleDataGenerationTest {
 					company.getCompanyId(), defaultUserIdBufferedWriter);
 			}
 
+			_exportCounterTableData(counterTableBufferedWriter);
 			_exportClassNameTableData(classNameTableBufferedWriter);
 
 			classNameTableBufferedWriter.flush();
 			companyTableBufferedWriter.flush();
+			counterTableBufferedWriter.flush();
 			defaultUserIdBufferedWriter.flush();
 			groupTableBufferedWriter.flush();
 			hostBufferedWriter.flush();
@@ -465,6 +483,9 @@ public class CompanySampleDataGenerationTest {
 
 	@Inject
 	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private CounterLocalService _counterLocalService;
 
 	private final Map<String, List<String>> _csvMap = new ConcurrentHashMap<>();
 	private ExecutorService _executorService;
