53d53ccb2a384129ae73c479ac9394fa87072423
lily.chi <lily.chi@liferay.com>
2022-02-21 22:36:13 +0800

    LPS-145373 Simplify, use FileWriter instead of Files.newBufferedWriter() here to make sure these writers will append data in the target file instead of overwriting since these writers are created in a loop.

--- modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
+++ modules/test/company-sample-data-generation-test/src/testIntegration/java/com/liferay/company/sample/data/generation/test/CompanySampleDataGenerationTest.java
@@ -47,10 +47,10 @@ import com.liferay.portal.util.PropsUtil;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 import java.net.InetSocketAddress;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -243,6 +243,17 @@ public class CompanySampleDataGenerationTest {
 		}
 	}
 
+	private void _exportCompanyData(String hostName) throws Exception {
+		Path companyCSVFilePath = _outputDirPath.resolve(_COMPANY_CSV);
+
+		try (BufferedWriter companyBufferedWriter = new BufferedWriter(
+				new FileWriter(companyCSVFilePath.toFile(), true))) {
+
+			companyBufferedWriter.append(hostName);
+			companyBufferedWriter.newLine();
+		}
+	}
+
 	private void _exportCSVs() throws Exception {
 		String outputDir = PropsUtil.get("sample.data.output.dir");
 
@@ -250,10 +261,10 @@ public class CompanySampleDataGenerationTest {
 			return;
 		}
 
-		Path outputDirPath = Paths.get(
+		_outputDirPath = Paths.get(
 			PropsUtil.get(PropsKeys.LIFERAY_HOME), outputDir);
 
-		File outputDirFile = outputDirPath.toFile();
+		File outputDirFile = _outputDirPath.toFile();
 
 		if (outputDirFile.exists()) {
 			FileUtil.deltree(outputDirFile);
@@ -262,42 +273,51 @@ public class CompanySampleDataGenerationTest {
 		outputDirFile.mkdir();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer(
-				outputDirFile.getAbsolutePath());
-			BufferedWriter companyBufferedWriter = Files.newBufferedWriter(
-				outputDirPath.resolve(_COMPANY_CSV));
-			BufferedWriter hostBufferedWriter = Files.newBufferedWriter(
-				outputDirPath.resolve(_HOST_CSV));
-			BufferedWriter userBufferedWriter = Files.newBufferedWriter(
-				outputDirPath.resolve(_USER_CSV))) {
+				outputDirFile.getAbsolutePath())) {
 
 			List<String> keys = new ArrayList<>(_csvMap.keySet());
 
 			Collections.sort(keys);
 
 			for (String key : keys) {
-				companyBufferedWriter.append(key);
-				companyBufferedWriter.newLine();
+				_exportCompanyData(key);
 
-				hostBufferedWriter.append(_PORTAL_SERVER_IP_ADDRESS);
-				hostBufferedWriter.append(StringPool.SPACE);
-				hostBufferedWriter.append(key);
-				hostBufferedWriter.newLine();
+				_exportHost(key);
 
-				List<String> screenNames = _csvMap.get(key);
-
-				Collections.sort(screenNames);
-
-				for (String screenName : screenNames) {
-					userBufferedWriter.append(key);
-					userBufferedWriter.append(StringPool.COMMA);
-					userBufferedWriter.append(screenName);
-					userBufferedWriter.newLine();
-				}
+				_exportUserData(key);
 			}
+		}
+	}
 
-			companyBufferedWriter.flush();
-			hostBufferedWriter.flush();
-			userBufferedWriter.flush();
+	private void _exportHost(String hostName) throws Exception {
+		Path hostCSVFilePath = _outputDirPath.resolve(_HOST_CSV);
+
+		try (BufferedWriter hostBufferedWriter = new BufferedWriter(
+				new FileWriter(hostCSVFilePath.toFile(), true))) {
+
+			hostBufferedWriter.append(_PORTAL_SERVER_IP_ADDRESS);
+			hostBufferedWriter.append(StringPool.SPACE);
+			hostBufferedWriter.append(hostName);
+			hostBufferedWriter.newLine();
+		}
+	}
+
+	private void _exportUserData(String hostName) throws Exception {
+		List<String> screenNames = _csvMap.get(hostName);
+
+		Collections.sort(screenNames);
+
+		Path userCSVFilePath = _outputDirPath.resolve(_USER_CSV);
+
+		try (BufferedWriter userBufferedWriter = new BufferedWriter(
+				new FileWriter(userCSVFilePath.toFile(), true))) {
+
+			for (String screenName : screenNames) {
+				userBufferedWriter.append(hostName);
+				userBufferedWriter.append(StringPool.COMMA);
+				userBufferedWriter.append(screenName);
+				userBufferedWriter.newLine();
+			}
 		}
 	}
 
@@ -340,6 +360,7 @@ public class CompanySampleDataGenerationTest {
 	private final Map<String, List<String>> _csvMap = new ConcurrentHashMap<>();
 	private ExecutorService _executorService;
 	private AtomicReference<InetSocketAddress> _originalAtomicReference;
+	private Path _outputDirPath;
 
 	@Inject
 	private Portal _portal;
