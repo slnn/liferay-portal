2a1ca3a5e46809c303dfa6b9057c0849755e11ce
lily.chi <lily.chi@liferay.com>
2022-03-25 11:47:38 +0800

    LPS-150076 Simplify, no need repeat set ThemeDisplay for SearchBarPortletDisplayContextBuilder in SearchBarPortlet just use the default value of SearchBarPortletDisplayContextBuilder since they are same one under this situation

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/SearchBarPortlet.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/SearchBarPortlet.java
@@ -239,8 +239,6 @@ public class SearchBarPortlet extends MVCPortlet {
 				scopeParameterName, renderRequest)
 		).setSearchScopePreference(
 			searchBarPortletPreferences.getSearchScopePreference()
-		).setThemeDisplay(
-			themeDisplay
 		).build();
 	}
 
