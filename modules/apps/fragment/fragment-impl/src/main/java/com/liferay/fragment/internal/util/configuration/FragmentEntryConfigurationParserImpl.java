7dc9358979fc70b50961bd7c948bdf50ee09d0b7
lily.chi <lily.chi@liferay.com>
2022-04-28 15:37:46 +0800

    LPS-152440 Apply to FragmentEntryConfigurationParserImpl

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
@@ -237,23 +237,16 @@ public class FragmentEntryConfigurationParserImpl
 
 		value = GetterUtil.getString(value);
 
+		JSONObject valueJSONObject = JSONUtil.getValidJSONObject(value);
+
 		if (fragmentConfigurationField.isLocalizable() &&
-			JSONUtil.isValid(value)) {
+			(valueJSONObject != null)) {
 
-			try {
-				JSONObject valueJSONObject = JSONFactoryUtil.createJSONObject(
-					value);
-
-				value = valueJSONObject.getString(
-					LocaleUtil.toLanguageId(locale),
-					valueJSONObject.getString(
-						LocaleUtil.toLanguageId(LocaleUtil.getSiteDefault()),
-						fragmentConfigurationField.getDefaultValue()));
-			}
-			catch (JSONException jsonException) {
-				_log.error(
-					"Unable to parse configuration value JSON", jsonException);
-			}
+			value = valueJSONObject.getString(
+				LocaleUtil.toLanguageId(locale),
+				valueJSONObject.getString(
+					LocaleUtil.toLanguageId(LocaleUtil.getSiteDefault()),
+					fragmentConfigurationField.getDefaultValue()));
 		}
 		else if (Validator.isNull(value)) {
 			value = fragmentConfigurationField.getDefaultValue();
