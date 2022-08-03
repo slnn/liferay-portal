52eb2a9e3e6d7c750db2dd7184acaec5de956e11
Dante Wang <dante.wang@liferay.com>
2022-08-03 13:32:42 +0800

    LPS-159586 Break circular dependency between CommercePriceEntryLocalServiceImpl and CommerceTierPriceEntryLocalServiceImpl by moving logic to an AopService and use it

--- modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceEntryLocalServiceImpl.java
+++ modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceEntryLocalServiceImpl.java
@@ -918,38 +918,6 @@ public class CommercePriceEntryLocalServiceImpl
 
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
-	public CommercePriceEntry setHasTierPrice(
-			long commercePriceEntryId, boolean hasTierPrice)
-		throws PortalException {
-
-		CommercePriceEntry commercePriceEntry =
-			commercePriceEntryPersistence.findByPrimaryKey(
-				commercePriceEntryId);
-
-		commercePriceEntry.setHasTierPrice(hasTierPrice);
-		commercePriceEntry.setBulkPricing(true);
-
-		return commercePriceEntryPersistence.update(commercePriceEntry);
-	}
-
-	@Override
-	public CommercePriceEntry setHasTierPrice(
-			long commercePriceEntryId, boolean hasTierPrice,
-			boolean bulkPricing)
-		throws PortalException {
-
-		CommercePriceEntry commercePriceEntry =
-			commercePriceEntryPersistence.findByPrimaryKey(
-				commercePriceEntryId);
-
-		commercePriceEntry.setHasTierPrice(hasTierPrice);
-		commercePriceEntry.setBulkPricing(bulkPricing);
-
-		return commercePriceEntryPersistence.update(commercePriceEntry);
-	}
-
-	@Indexable(type = IndexableType.REINDEX)
-	@Override
 	public CommercePriceEntry updateCommercePriceEntry(
 			long commercePriceEntryId, BigDecimal price, BigDecimal promoPrice,
 			boolean discountDiscovery, BigDecimal discountLevel1,
