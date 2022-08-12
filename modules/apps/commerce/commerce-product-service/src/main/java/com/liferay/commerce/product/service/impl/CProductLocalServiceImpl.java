ef72acffbc298958945b4a755ae500075cb7e049
lily.chi <lily.chi@liferay.com>
2022-08-12 23:54:27 +0800

    LPS-160098 Break the circular dependency between CProductLocalServiceImpl and CPDefinitionLocalServiceImpl by using the persistence class directly(Break1 B1)

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CProductLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CProductLocalServiceImpl.java
@@ -20,8 +20,8 @@ import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.product.service.CPDefinitionLinkLocalService;
-import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.base.CProductLocalServiceBaseImpl;
+import com.liferay.commerce.product.service.persistence.CPDefinitionPersistence;
 import com.liferay.commerce.product.service.persistence.CPInstancePersistence;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -80,7 +80,7 @@ public class CProductLocalServiceImpl extends CProductLocalServiceBaseImpl {
 
 		// Commerce product definitions
 
-		_cpDefinitionLocalService.deleteCPDefinitions(
+		_cpDefinitionPersistence.removeByC_S(
 			cProduct.getCProductId(), WorkflowConstants.STATUS_ANY);
 
 		// Commerce product definition links
@@ -201,7 +201,7 @@ public class CProductLocalServiceImpl extends CProductLocalServiceBaseImpl {
 	private CPDefinitionLinkLocalService _cpDefinitionLinkLocalService;
 
 	@Reference
-	private CPDefinitionLocalService _cpDefinitionLocalService;
+	private CPDefinitionPersistence _cpDefinitionPersistence;
 
 	@Reference
 	private UserLocalService _userLocalService;
