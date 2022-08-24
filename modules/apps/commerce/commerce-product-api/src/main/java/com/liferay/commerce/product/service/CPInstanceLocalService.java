62054f3f8bef09e26c0a8ab6002473fd9e78ef8c
lily.chi <lily.chi@liferay.com>
2022-08-24 17:05:48 +0800

    LPS-160098 Auto generated

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPInstanceLocalService.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPInstanceLocalService.java
@@ -257,10 +257,6 @@ public interface CPInstanceLocalService
 
 	public void checkCPInstances(long cpDefinitionId) throws PortalException;
 
-	public void checkCPInstances(
-			long userId, long cpDefinitionId, boolean ignoreSKUCombinations)
-		throws PortalException;
-
 	public void checkCPInstancesByDisplayDate(long cpDefinitionId)
 		throws PortalException;
 
