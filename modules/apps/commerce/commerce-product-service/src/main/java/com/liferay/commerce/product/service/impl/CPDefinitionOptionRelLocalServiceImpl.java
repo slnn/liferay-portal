b242abb285cdc5139cd24efcdc0e02c08bd484ce
lily.chi <lily.chi@liferay.com>
2022-08-25 20:14:41 +0800

    LPS-160098 Extract these logic into a method copyCPDefinitionAndPrepareCPDefinitionOptionRel(CPDefinitionOptionRel cpDefinitionOptionRel), prep next

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionRelLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionRelLocalServiceImpl.java
@@ -225,11 +225,9 @@ public class CPDefinitionOptionRelLocalServiceImpl
 			cpDefinitionId, cpOptionId, true, serviceContext);
 	}
 
-	@Indexable(type = IndexableType.DELETE)
-	@Override
-	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
-	public CPDefinitionOptionRel deleteCPDefinitionOptionRel(
-			CPDefinitionOptionRel cpDefinitionOptionRel)
+	public CPDefinitionOptionRel
+			copyCPDefinitionAndPrepareCPDefinitionOptionRel(
+				CPDefinitionOptionRel cpDefinitionOptionRel)
 		throws PortalException {
 
 		if (_cpDefinitionLocalService.isVersionable(
@@ -243,6 +241,19 @@ public class CPDefinitionOptionRelLocalServiceImpl
 				newCPDefinition.getCPDefinitionId(),
 				cpDefinitionOptionRel.getCPOptionId());
 		}
+
+		return cpDefinitionOptionRel;
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CPDefinitionOptionRel deleteCPDefinitionOptionRel(
+			CPDefinitionOptionRel cpDefinitionOptionRel)
+		throws PortalException {
+
+		cpDefinitionOptionRel = copyCPDefinitionAndPrepareCPDefinitionOptionRel(
+			cpDefinitionOptionRel);
 
 		// Commerce product definition option value rels
 
