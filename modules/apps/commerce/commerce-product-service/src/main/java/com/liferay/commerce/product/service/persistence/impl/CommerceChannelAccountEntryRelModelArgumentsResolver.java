e68bc308e7cc96b26200336b12f3e24db2379b7e
Lily.Chi <lily.chi@liferay.com>
2022-09-08 09:51:31 +0800

    LPS-X Auto generated

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/persistence/impl/CommerceChannelAccountEntryRelModelArgumentsResolver.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/persistence/impl/CommerceChannelAccountEntryRelModelArgumentsResolver.java
@@ -24,12 +24,21 @@ import com.liferay.portal.kernel.model.BaseModel;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * The arguments resolver class for retrieving value from CommerceChannelAccountEntryRel.
  *
  * @author Marco Leo
  * @generated
  */
+@Component(
+	immediate = true,
+	service = {
+		CommerceChannelAccountEntryRelModelArgumentsResolver.class,
+		ArgumentsResolver.class
+	}
+)
 public class CommerceChannelAccountEntryRelModelArgumentsResolver
 	implements ArgumentsResolver {
 
