774cb3deb8d48a237d3392fdc5de2256e697dc61
Lily.Chi <lily.chi@liferay.com>
2022-11-03 19:21:23 +0800

    LPS-167595 Use CPDefinitionOptionRelPersistence's related methods to replace these CPDefinitionOptionRelLocalService's in CPDefinitionLocalServiceImpl since these implementations of these CPDefinitionOptionRelLocalService's methods call the persistence methods, prepare to break the circular dependency issue between CPDefinitionLocalService and CPDefinitionOptionRelLocalService

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionLocalServiceImpl.java
@@ -1934,9 +1934,8 @@ public class CPDefinitionLocalServiceImpl
 
 	@Override
 	public boolean hasChildCPDefinitions(long cpDefinitionId) {
-		int count =
-			_cpDefinitionOptionRelLocalService.getCPDefinitionOptionRelsCount(
-				cpDefinitionId);
+		int count = _cpDefinitionOptionRelPersistence.countByCPDefinitionId(
+			cpDefinitionId);
 
 		if ((count <= 0) ||
 			!_cpDefinitionOptionRelLocalService.
@@ -2882,8 +2881,7 @@ public class CPDefinitionLocalServiceImpl
 		}
 
 		int cpDefinitionOptionRelsCount =
-			_cpDefinitionOptionRelLocalService.getCPDefinitionOptionRelsCount(
-				cpDefinitionId, true);
+			_cpDefinitionOptionRelPersistence.countByC_SC(cpDefinitionId, true);
 
 		if (cpDefinitionOptionRelsCount == 0) {
 			return;
