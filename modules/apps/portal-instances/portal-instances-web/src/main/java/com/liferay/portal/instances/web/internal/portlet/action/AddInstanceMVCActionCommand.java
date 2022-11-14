9a8b5606df8adede630ea526978e7fd20d23da71
Lily.Chi <lily.chi@liferay.com>
2022-11-14 17:32:36 +0800

    LPS-168390 Remove "immediate = true" from module portal-instances

--- modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/AddInstanceMVCActionCommand.java
+++ modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/AddInstanceMVCActionCommand.java
@@ -46,7 +46,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Víctor Galán Grande
  */
 @Component(
-	immediate = true,
 	property = {
 		"javax.portlet.name=" + PortalInstancesPortletKeys.PORTAL_INSTANCES,
 		"mvc.command.name=/portal_instances/add_instance"
