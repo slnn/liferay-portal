c0868af53010e23ed0bcc8ab31126592b44cb340
Lily.Chi <lily.chi@liferay.com>
2022-11-14 18:11:05 +0800

    LPS-168390 Remove "immediate = true" from module portal-osgi-web

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -118,7 +118,7 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 /**
  * @author Raymond Augé
  */
-@Component(immediate = true, service = PortletTracker.class)
+@Component(service = PortletTracker.class)
 public class PortletTracker
 	implements ServiceTrackerCustomizer
 		<Portlet, com.liferay.portal.kernel.model.Portlet> {
