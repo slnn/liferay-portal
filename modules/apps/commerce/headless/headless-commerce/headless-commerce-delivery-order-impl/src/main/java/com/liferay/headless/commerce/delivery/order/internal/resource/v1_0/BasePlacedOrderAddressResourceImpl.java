c6096b29dd99657b0cf9f293cd476944c1135bc0
Lily.Chi <lily.chi@liferay.com>
2023-02-09 15:56:25 +0800

    LPS-175224 Apply TransformUtil.transformToArray() in module:commerce/headless/headless-commerce-delivery-order-impl

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-impl/src/main/java/com/liferay/headless/commerce/delivery/order/internal/resource/v1_0/BasePlacedOrderAddressResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-impl/src/main/java/com/liferay/headless/commerce/delivery/order/internal/resource/v1_0/BasePlacedOrderAddressResourceImpl.java
@@ -254,7 +254,7 @@ public abstract class BasePlacedOrderAddressResourceImpl
 		return TransformUtil.transform(array, unsafeFunction, clazz);
 	}
 
-	protected <T, R, E extends Throwable> R[] transformToArray(
+	protected <T, R, E extends Throwable> Object transformToArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
 		Class<?> clazz) {
 
