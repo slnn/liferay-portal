c340625e6e7876cf133b515732d1dd43cce73e51
Lily.Chi <lily.chi@liferay.com>
2023-02-09 17:00:03 +0800

    LPS-175224 Apply TransformUtil.transformToArray() in module:bulk

--- modules/apps/bulk/bulk-rest-impl/src/main/java/com/liferay/bulk/rest/internal/resource/v1_0/BaseStatusResourceImpl.java
+++ modules/apps/bulk/bulk-rest-impl/src/main/java/com/liferay/bulk/rest/internal/resource/v1_0/BaseStatusResourceImpl.java
@@ -195,7 +195,7 @@ public abstract class BaseStatusResourceImpl implements StatusResource {
 		return TransformUtil.transform(array, unsafeFunction, clazz);
 	}
 
-	protected <T, R, E extends Throwable> R[] transformToArray(
+	protected <T, R, E extends Throwable> Object transformToArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
 		Class<?> clazz) {
 
