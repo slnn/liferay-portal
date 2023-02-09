0a05525c371c034e20485775af84c0c266bfbbc4
Lily.Chi <lily.chi@liferay.com>
2023-02-09 16:00:20 +0800

    LPS-175224 Apply TransformUtil.transformToArray() in module:headless/headless-admin-list-type

--- modules/apps/headless/headless-admin-list-type/headless-admin-list-type-impl/src/main/java/com/liferay/headless/admin/list/type/internal/resource/v1_0/BaseListTypeDefinitionResourceImpl.java
+++ modules/apps/headless/headless-admin-list-type/headless-admin-list-type-impl/src/main/java/com/liferay/headless/admin/list/type/internal/resource/v1_0/BaseListTypeDefinitionResourceImpl.java
@@ -904,7 +904,7 @@ public abstract class BaseListTypeDefinitionResourceImpl
 		return TransformUtil.transform(array, unsafeFunction, clazz);
 	}
 
-	protected <T, R, E extends Throwable> R[] transformToArray(
+	protected <T, R, E extends Throwable> Object transformToArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
 		Class<?> clazz) {
 
