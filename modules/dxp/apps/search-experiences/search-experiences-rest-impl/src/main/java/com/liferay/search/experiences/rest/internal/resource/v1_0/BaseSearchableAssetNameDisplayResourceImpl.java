f911bed519717ed75d0a2cab4a989f7db9639052
Lily.Chi <lily.chi@liferay.com>
2023-02-09 17:22:24 +0800

    LPS-175224 Apply TransformUtil.transformToArray() in dxp module:search-experiences

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/BaseSearchableAssetNameDisplayResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/BaseSearchableAssetNameDisplayResourceImpl.java
@@ -408,7 +408,7 @@ public abstract class BaseSearchableAssetNameDisplayResourceImpl
 		return TransformUtil.transform(array, unsafeFunction, clazz);
 	}
 
-	protected <T, R, E extends Throwable> R[] transformToArray(
+	protected <T, R, E extends Throwable> Object transformToArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
 		Class<?> clazz) {
 
