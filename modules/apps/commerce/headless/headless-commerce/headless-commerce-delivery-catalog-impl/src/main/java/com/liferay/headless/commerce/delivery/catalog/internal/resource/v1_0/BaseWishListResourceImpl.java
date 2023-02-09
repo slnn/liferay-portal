8fc5b6ab7ccccbc76a69104f3449066f6ae4f5ce
Lily.Chi <lily.chi@liferay.com>
2023-02-09 15:55:00 +0800

    LPS-175224 Apply TransformUtil.transformToArray() in module:commerce/headless/headless-commerce-delivery-catalog-impl

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/resource/v1_0/BaseWishListResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/resource/v1_0/BaseWishListResourceImpl.java
@@ -602,7 +602,7 @@ public abstract class BaseWishListResourceImpl
 		return TransformUtil.transform(array, unsafeFunction, clazz);
 	}
 
-	protected <T, R, E extends Throwable> R[] transformToArray(
+	protected <T, R, E extends Throwable> Object transformToArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
 		Class<?> clazz) {
 
