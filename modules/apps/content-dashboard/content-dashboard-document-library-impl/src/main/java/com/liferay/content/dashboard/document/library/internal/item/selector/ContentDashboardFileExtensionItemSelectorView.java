bc7b0cb4cb9935a2dbca349859f4ff7907c36349
Lily.Chi <lily.chi@liferay.com>
2023-03-28 14:08:59 +0800

    LPS-179646 Remove Stream usgaes in method body from ContentDashboardFileExtensionItemSelectorView

--- modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/selector/ContentDashboardFileExtensionItemSelectorView.java
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/selector/ContentDashboardFileExtensionItemSelectorView.java
@@ -22,18 +22,21 @@ import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
 import com.liferay.item.selector.criteria.file.criterion.FileExtensionItemSelectorCriterion;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.search.aggregation.Aggregations;
-import com.liferay.portal.search.aggregation.bucket.Bucket;
 import com.liferay.portal.search.aggregation.bucket.TermsAggregationResult;
 import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.SearchRequestBuilder;
@@ -42,15 +45,11 @@ import com.liferay.portal.search.searcher.Searcher;
 
 import java.io.IOException;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.portlet.PortletURL;
 
@@ -133,28 +132,18 @@ public class ContentDashboardFileExtensionItemSelectorView
 		List<String> existingFileExtensions = _getExistingFileExtensions(
 			themeDisplay.getRequest());
 
-		Stream<String> stream = existingFileExtensions.stream();
+		Set<String> otherFileExtension = SetUtil.fromList(
+			ListUtil.filter(
+				existingFileExtensions, _fileExtensionGroupsProvider::isOther));
 
-		Set<String> otherFileExtension = stream.filter(
-			_fileExtensionGroupsProvider::isOther
-		).collect(
-			Collectors.toSet()
-		);
-
-		Stream<FileExtensionGroupsProvider.FileExtensionGroup>
-			fileExtensionGroupsStream = fileExtensionGroups.stream();
-
-		return JSONUtil.putAll(
-			fileExtensionGroupsStream.map(
-				fileExtensionGroup -> fileExtensionGroup.toJSONObject(
-					SetUtil.fromArray(
-						servletRequest.getParameterValues(
-							"checkedFileExtensions")),
-					existingFileExtensions, _dlMimeTypeDisplayContext,
-					_language, themeDisplay.getLocale(), otherFileExtension)
-			).filter(
-				Objects::nonNull
-			).toArray());
+		return JSONUtil.toJSONArray(
+			fileExtensionGroups,
+			fileExtensionGroup -> fileExtensionGroup.toJSONObject(
+				SetUtil.fromArray(
+					servletRequest.getParameterValues("checkedFileExtensions")),
+				existingFileExtensions, _dlMimeTypeDisplayContext, _language,
+				themeDisplay.getLocale(), otherFileExtension),
+			_log);
 	}
 
 	private List<String> _getExistingFileExtensions(
@@ -189,16 +178,12 @@ public class ContentDashboardFileExtensionItemSelectorView
 			(TermsAggregationResult)searchResponse.getAggregationResult(
 				"extensions");
 
-		Collection<Bucket> buckets = termsAggregationResult.getBuckets();
-
-		Stream<Bucket> stream = buckets.stream();
-
-		return stream.map(
-			bucket -> bucket.getKey()
-		).collect(
-			Collectors.toList()
-		);
+		return TransformUtil.transform(
+			termsAggregationResult.getBuckets(), bucket -> bucket.getKey());
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ContentDashboardFileExtensionItemSelectorView.class);
 
 	private static final List<ItemSelectorReturnType>
 		_supportedItemSelectorReturnTypes = Collections.singletonList(
