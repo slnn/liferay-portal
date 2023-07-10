450705a6acb83ba927880e5bb78eaf5d827398a9
Lily.Chi <lily.chi@liferay.com>
2023-07-10 19:02:53 +0800

    LPS-X BuildService

---
+++ modules/apps/expando/expando-api/src/main/java/com/liferay/expando/service/ExpandoColumnServiceUtil.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.expando.service;
+
+/**
+ * Provides the remote service utility for ExpandoColumn. This utility wraps
+ * <code>com.liferay.expando.service.impl.ExpandoColumnServiceImpl</code> and is an
+ * access point for service operations in application layer code running on a
+ * remote server. Methods of this service are expected to have security checks
+ * based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see ExpandoColumnService
+ * @generated
+ */
+public class ExpandoColumnServiceUtil {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to <code>com.liferay.expando.service.impl.ExpandoColumnServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public static java.lang.String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static ExpandoColumnService getService() {
+		return _service;
+	}
+
+	public static void setService(ExpandoColumnService service) {
+		_service = service;
+	}
+
+	private static volatile ExpandoColumnService _service;
+
+}
