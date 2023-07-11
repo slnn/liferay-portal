b644de11f46563712874163bb9250041f11ae9b5
Lily.Chi <lily.chi@liferay.com>
2023-07-11 17:37:48 +0800

    LPS-X Copy the interface  ExpandoColumnPermission under portal-kernel and the implementation ExpandoColumnPermissionImpl under portal-impl to expando-api and expando-serice

---
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/internal/security/permission/ExpandoColumnPermissionImpl.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.expando.internal.security.permission;
+
+import com.liferay.expando.model.ExpandoColumn;
+import com.liferay.expando.service.ExpandoColumnLocalService;
+import com.liferay.expando.service.permission.ExpandoColumnPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(service = ExpandoColumnPermission.class)
+public class ExpandoColumnPermissionImpl implements ExpandoColumnPermission {
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, ExpandoColumn column,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, column, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, ExpandoColumn.class.getName(),
+				column.getColumnId(), actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long columnId, String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, columnId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, ExpandoColumn.class.getName(), columnId,
+				actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long companyId,
+			String className, String tableName, String columnName,
+			String actionId)
+		throws PortalException {
+
+		check(
+			permissionChecker,
+			_expandoColumnLocalService.getColumn(
+				companyId, className, tableName, columnName),
+			actionId);
+	}
+
+	@Override
+	public boolean contains(
+		PermissionChecker permissionChecker, ExpandoColumn column,
+		String actionId) {
+
+		return permissionChecker.hasPermission(
+			null, ExpandoColumn.class.getName(), column.getColumnId(),
+			actionId);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long columnId, String actionId)
+		throws PortalException {
+
+		return contains(
+			permissionChecker, _expandoColumnLocalService.getColumn(columnId),
+			actionId);
+	}
+
+	@Override
+	public boolean contains(
+		PermissionChecker permissionChecker, long companyId, String className,
+		String tableName, String columnName, String actionId) {
+
+		return contains(
+			permissionChecker,
+			_expandoColumnLocalService.getColumn(
+				companyId, className, tableName, columnName),
+			actionId);
+	}
+
+	@Reference
+	private ExpandoColumnLocalService _expandoColumnLocalService;
+
+}
