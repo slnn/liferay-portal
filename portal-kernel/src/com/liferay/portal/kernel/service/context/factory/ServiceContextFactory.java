dfc11de2940d02d8c80411c88c303b67cd3d0d3b
Lily.Chi <lily.chi@liferay.com>
2023-07-22 13:05:00 +0800

    LPS-X Add new interface ServiceContextFactory under portal-kernel, prep next

---
+++ portal-kernel/src/com/liferay/portal/kernel/service/context/factory/ServiceContextFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.service.context.factory;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import javax.portlet.PortletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Raymond Augé
+ */
+public interface ServiceContextFactory {
+
+	public ServiceContext getInstance(HttpServletRequest httpServletRequest)
+		throws PortalException;
+
+	public ServiceContext getInstance(PortletRequest portletRequest)
+		throws PortalException;
+
+	public ServiceContext getInstance(
+			String className, HttpServletRequest httpServletRequest)
+		throws PortalException;
+
+	public ServiceContext getInstance(
+			String className, PortletRequest portletRequest)
+		throws PortalException;
+
+}
