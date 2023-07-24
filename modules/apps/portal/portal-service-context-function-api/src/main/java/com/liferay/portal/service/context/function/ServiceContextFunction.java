7001e3b5063c38b4fa94bc0f8f743e96511bc80c
Lily.Chi <lily.chi@liferay.com>
2023-07-24 16:54:31 +0800

    LPS-191596 Move ServiceContextFunction from portal-kernel to module since it is only used in modules

--- modules/apps/portal/portal-service-context-function-api/src/main/java/com/liferay/portal/service/context/function/ServiceContextFunction.java
+++ modules/apps/portal/portal-service-context-function-api/src/main/java/com/liferay/portal/service/context/function/ServiceContextFunction.java
@@ -3,9 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.service;
+package com.liferay.portal.service.context.function;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 
 import java.util.function.Function;
 
