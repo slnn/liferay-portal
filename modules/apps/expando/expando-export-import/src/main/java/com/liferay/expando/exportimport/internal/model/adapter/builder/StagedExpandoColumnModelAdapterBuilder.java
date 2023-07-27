ebd376e7ebc900a7a7bf68e76aa48668e7994757
Lily.Chi <lily.chi@liferay.com>
2023-07-27 16:35:40 +0800

    LPS-190836 Apply

--- modules/apps/expando/expando-export-import/src/main/java/com/liferay/expando/exportimport/internal/model/adapter/builder/StagedExpandoColumnModelAdapterBuilder.java
+++ modules/apps/expando/expando-export-import/src/main/java/com/liferay/expando/exportimport/internal/model/adapter/builder/StagedExpandoColumnModelAdapterBuilder.java
@@ -6,8 +6,8 @@
 package com.liferay.expando.exportimport.internal.model.adapter.builder;
 
 import com.liferay.expando.exportimport.internal.model.adapter.StagedExpandoColumnImpl;
-import com.liferay.expando.kernel.model.adapter.StagedExpandoColumn;
 import com.liferay.expando.model.ExpandoColumn;
+import com.liferay.expando.model.adapter.StagedExpandoColumn;
 import com.liferay.portal.kernel.model.adapter.builder.ModelAdapterBuilder;
 
 import org.osgi.service.component.annotations.Component;
