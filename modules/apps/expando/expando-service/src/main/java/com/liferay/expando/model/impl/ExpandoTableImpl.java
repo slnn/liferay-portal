d1274a320d75fcf515c26829c52a8418f0d61932
Lily.Chi <lily.chi@liferay.com>
2023-07-28 16:51:48 +0800

    LPS-X Copy *Impl from portal-impl to expando-service and apply the new API in expando-api

--- modules/apps/expando/expando-service/src/main/java/com/liferay/expando/model/impl/ExpandoTableImpl.java
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/model/impl/ExpandoTableImpl.java
@@ -5,8 +5,20 @@
 
 package com.liferay.expando.model.impl;
 
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+
 /**
  * @author Brian Wing Shun Chan
  */
 public class ExpandoTableImpl extends ExpandoTableBaseImpl {
+
+	@Override
+	public boolean isDefaultTable() {
+		if (getName().equals(ExpandoTableConstants.DEFAULT_TABLE_NAME)) {
+			return true;
+		}
+
+		return false;
+	}
+
 }
