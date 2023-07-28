d064541f6107d28bcf795fcf8b1833b4ee95f335
Lily.Chi <lily.chi@liferay.com>
2023-07-28 19:54:33 +0800

    LPS-190835 Move StagedExpandoColumn from portal-kernel to modules

--- modules/apps/expando/expando-api/src/main/java/com/liferay/expando/model/adapter/StagedExpandoColumn.java
+++ modules/apps/expando/expando-api/src/main/java/com/liferay/expando/model/adapter/StagedExpandoColumn.java
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.expando.kernel.model.adapter;
+package com.liferay.expando.model.adapter;
 
-import com.liferay.expando.kernel.model.ExpandoColumn;
+import com.liferay.expando.model.ExpandoColumn;
 import com.liferay.portal.kernel.model.StagedModel;
 
 /**
