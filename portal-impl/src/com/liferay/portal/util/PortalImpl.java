2e58ec4160c79cd7fa86a339dbc0cae96a343723
Lily.Chi <lily.chi@liferay.com>
2023-08-03 15:16:54 +0800

    LPS-X Remove unused method getCurrentAndAncestorSiteGroups(long groupId) from Portal Interface, it was added by this commit 862cad1be074f76845b24ef5560bda8eba46019b of LPS-46261, added usage in this commit 68adb40351dd0da9d421d9259f38119ea712ea70 of LPS-46261, however, the only usage was removed by this commit 37896380bf9e8dfceaf7c4409448be129f4ae6c7 of LPS-46261

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -2024,13 +2024,6 @@ public class PortalImpl implements Portal {
 	}
 
 	@Override
-	public List<Group> getCurrentAndAncestorSiteGroups(long groupId)
-		throws PortalException {
-
-		return getCurrentAndAncestorSiteGroups(groupId, false);
-	}
-
-	@Override
 	public List<Group> getCurrentAndAncestorSiteGroups(
 			long groupId, boolean checkContentSharingWithChildrenEnabled)
 		throws PortalException {
