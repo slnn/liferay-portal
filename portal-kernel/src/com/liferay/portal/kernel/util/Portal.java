0fed94f4e8b65ef335e552cc41261ee6aa10a6a7
Lily.Chi <lily.chi@liferay.com>
2023-08-08 15:18:17 +0800

    LPS-193189 Remove unused method resetPortletParameters(String url, String portletId) from Portal Interface, it was added by this commit 9ca127409bf77e94c316843dd880576c0c266ba5 of LPS-45321, usage was removed by this commit cbfdc259c6c5cb209147efd1c238d1bacdea6014 of LPS-58210

--- portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
@@ -1130,8 +1130,6 @@ public interface Portal {
 
 	public void resetCDNHosts();
 
-	public String resetPortletParameters(String url, String portletId);
-
 	public void sendError(
 			Exception exception, ActionRequest actionRequest,
 			ActionResponse actionResponse)
