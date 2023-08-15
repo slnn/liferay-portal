5ece44595cc5aab2622bcdc3af83310a554766cb
Lily.Chi <lily.chi@liferay.com>
2023-08-15 17:40:15 +0800

    LPS-X Copy *Impl from portal-impl to expando-service and apply the new API in expando-api

--- modules/apps/expando/expando-service/src/main/java/com/liferay/expando/service/impl/ExpandoValueServiceImpl.java
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/service/impl/ExpandoValueServiceImpl.java
@@ -5,10 +5,29 @@
 
 package com.liferay.expando.service.impl;
 
+import com.liferay.expando.kernel.service.permission.ExpandoColumnPermissionUtil;
+import com.liferay.expando.model.ExpandoColumn;
+import com.liferay.expando.model.ExpandoValue;
+import com.liferay.expando.service.ExpandoColumnLocalService;
 import com.liferay.expando.service.base.ExpandoValueServiceBaseImpl;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.Serializable;
+
+import java.util.Collection;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -21,4 +40,131 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class ExpandoValueServiceImpl extends ExpandoValueServiceBaseImpl {
+
+	@Override
+	public ExpandoValue addValue(
+			long companyId, String className, String tableName,
+			String columnName, long classPK, Object data)
+		throws PortalException {
+
+		ExpandoColumnPermissionUtil.check(
+			getPermissionChecker(),
+			_expandoColumnLocalService.getColumn(
+				companyId, className, tableName, columnName),
+			ActionKeys.UPDATE);
+
+		return expandoValueLocalService.addValue(
+			companyId, className, tableName, columnName, classPK, data);
+	}
+
+	@JSONWebService(mode = JSONWebServiceMode.IGNORE)
+	@Override
+	public ExpandoValue addValue(
+			long companyId, String className, String tableName,
+			String columnName, long classPK, String data)
+		throws PortalException {
+
+		ExpandoColumnPermissionUtil.check(
+			getPermissionChecker(),
+			_expandoColumnLocalService.getColumn(
+				companyId, className, tableName, columnName),
+			ActionKeys.UPDATE);
+
+		return expandoValueLocalService.addValue(
+			companyId, className, tableName, columnName, classPK, data);
+	}
+
+	@Override
+	public void addValues(
+			long companyId, String className, String tableName, long classPK,
+			Map<String, Serializable> attributeValues)
+		throws PortalException {
+
+		for (Map.Entry<String, Serializable> entry :
+				attributeValues.entrySet()) {
+
+			addValue(
+				companyId, className, tableName, entry.getKey(), classPK,
+				entry.getValue());
+		}
+	}
+
+	@Override
+	public Map<String, Serializable> getData(
+			long companyId, String className, String tableName,
+			Collection<String> columnNames, long classPK)
+		throws PortalException {
+
+		Map<String, Serializable> attributeValues =
+			expandoValueLocalService.getData(
+				companyId, className, tableName, columnNames, classPK);
+
+		for (String columnName : columnNames) {
+			if (!ExpandoColumnPermissionUtil.contains(
+					getPermissionChecker(),
+					_expandoColumnLocalService.getColumn(
+						companyId, className, tableName, columnName),
+					ActionKeys.VIEW)) {
+
+				attributeValues.remove(columnName);
+			}
+		}
+
+		return attributeValues;
+	}
+
+	@Override
+	public Serializable getData(
+			long companyId, String className, String tableName,
+			String columnName, long classPK)
+		throws PortalException {
+
+		ExpandoColumn column = _expandoColumnLocalService.getColumn(
+			companyId, className, tableName, columnName);
+
+		if ((column != null) &&
+			ExpandoColumnPermissionUtil.contains(
+				getPermissionChecker(), column, ActionKeys.VIEW)) {
+
+			return expandoValueLocalService.getData(
+				companyId, className, tableName, columnName, classPK);
+		}
+
+		return null;
+	}
+
+	@Override
+	public JSONObject getJSONData(
+			long companyId, String className, String tableName,
+			String columnName, long classPK)
+		throws PortalException {
+
+		ExpandoColumn column = _expandoColumnLocalService.getColumn(
+			companyId, className, tableName, columnName);
+
+		if ((column == null) ||
+			!ExpandoColumnPermissionUtil.contains(
+				getPermissionChecker(), column, ActionKeys.VIEW)) {
+
+			return null;
+		}
+
+		String data = String.valueOf(
+			expandoValueLocalService.getData(
+				companyId, className, tableName, columnName, classPK));
+
+		if (Validator.isNull(data)) {
+			return null;
+		}
+
+		if (data.startsWith(StringPool.OPEN_CURLY_BRACE)) {
+			return JSONFactoryUtil.createJSONObject(data);
+		}
+
+		return JSONUtil.put("data", data);
+	}
+
+	@Reference
+	private ExpandoColumnLocalService _expandoColumnLocalService;
+
 }
