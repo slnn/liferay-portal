70d98f53bc375591310ba90f21ad738a7fd6e539
Lily.Chi <lily.chi@liferay.com>
2023-08-18 14:57:46 +0800

    LPS-190836 BuildService

---
+++ modules/apps/expando/expando-api/src/main/java/com/liferay/expando/model/ExpandoColumnTable.java
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.expando.model;
+
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.base.BaseTable;
+
+import java.sql.Types;
+
+import java.util.Date;
+
+/**
+ * The table class for the &quot;ExpandoColumn&quot; database table.
+ *
+ * @author Brian Wing Shun Chan
+ * @see ExpandoColumn
+ * @generated
+ */
+public class ExpandoColumnTable extends BaseTable<ExpandoColumnTable> {
+
+	public static final ExpandoColumnTable INSTANCE = new ExpandoColumnTable();
+
+	public final Column<ExpandoColumnTable, Long> mvccVersion = createColumn(
+		"mvccVersion", Long.class, Types.BIGINT, Column.FLAG_NULLITY);
+	public final Column<ExpandoColumnTable, Long> ctCollectionId = createColumn(
+		"ctCollectionId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<ExpandoColumnTable, Long> columnId = createColumn(
+		"columnId", Long.class, Types.BIGINT, Column.FLAG_PRIMARY);
+	public final Column<ExpandoColumnTable, Long> companyId = createColumn(
+		"companyId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, Date> modifiedDate = createColumn(
+		"modifiedDate", Date.class, Types.TIMESTAMP, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, Long> tableId = createColumn(
+		"tableId", Long.class, Types.BIGINT, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, String> name = createColumn(
+		"name", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, Integer> type = createColumn(
+		"type_", Integer.class, Types.INTEGER, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, String> defaultData = createColumn(
+		"defaultData", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+	public final Column<ExpandoColumnTable, String> typeSettings = createColumn(
+		"typeSettings", String.class, Types.VARCHAR, Column.FLAG_DEFAULT);
+
+	private ExpandoColumnTable() {
+		super("ExpandoColumn", ExpandoColumnTable::new);
+	}
+
+}
