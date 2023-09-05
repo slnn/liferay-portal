099e18804d62dc004783b66c15286276ce88e78e
Lily.Chi <lily.chi@liferay.com>
2023-09-05 13:05:11 +0800

    LPS-190836 SF, change these method to private methods and rename

--- modules/apps/expando/expando-service/src/main/java/com/liferay/expando/service/impl/ExpandoColumnLocalServiceImpl.java
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/service/impl/ExpandoColumnLocalServiceImpl.java
@@ -72,7 +72,7 @@ public class ExpandoColumnLocalServiceImpl
 
 		ExpandoTable table = _expandoTablePersistence.findByPrimaryKey(tableId);
 
-		ExpandoValue value = validate(0, tableId, name, type, defaultData);
+		ExpandoValue value = _validate(0, tableId, name, type, defaultData);
 
 		long columnId = counterLocalService.increment();
 
@@ -97,7 +97,7 @@ public class ExpandoColumnLocalServiceImpl
 
 	@Override
 	public void deleteColumn(ExpandoColumn column) throws PortalException {
-		addDeletionSystemEvent(column);
+		_addDeletionSystemEvent(column);
 
 		expandoColumnPersistence.remove(column);
 
@@ -377,7 +377,7 @@ public class ExpandoColumnLocalServiceImpl
 		ExpandoColumn column = expandoColumnPersistence.findByPrimaryKey(
 			columnId);
 
-		ExpandoValue value = validate(
+		ExpandoValue value = _validate(
 			columnId, column.getTableId(), name, type, defaultData);
 
 		column.setName(name);
@@ -399,7 +399,7 @@ public class ExpandoColumnLocalServiceImpl
 		return expandoColumnPersistence.update(column);
 	}
 
-	protected void addDeletionSystemEvent(ExpandoColumn expandoColumn) {
+	private void _addDeletionSystemEvent(ExpandoColumn expandoColumn) {
 		StagedExpandoColumn stagedExpandoColumn = ModelAdapterUtil.adapt(
 			expandoColumn, ExpandoColumn.class, StagedExpandoColumn.class);
 
@@ -425,7 +425,7 @@ public class ExpandoColumnLocalServiceImpl
 		}
 	}
 
-	protected ExpandoValue validate(
+	private ExpandoValue _validate(
 			long columnId, long tableId, String name, int type,
 			Object defaultData)
 		throws PortalException {
