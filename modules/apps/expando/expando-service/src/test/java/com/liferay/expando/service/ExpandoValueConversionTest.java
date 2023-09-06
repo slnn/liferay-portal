d04c7755f666aa30eef6594f3bdb0eb3439e38aa
Lily.Chi <lily.chi@liferay.com>
2023-09-06 15:37:40 +0800

    LPS-190836 Update the junit test to make it use Reflection to invoke the target private method instead of extending the local service impl class

--- modules/apps/expando/expando-service/src/test/java/com/liferay/expando/service/ExpandoValueConversionTest.java
+++ modules/apps/expando/expando-service/src/test/java/com/liferay/expando/service/ExpandoValueConversionTest.java
@@ -7,8 +7,13 @@ package com.liferay.expando.service;
 
 import com.liferay.expando.kernel.model.ExpandoColumnConstants;
 import com.liferay.expando.service.impl.ExpandoValueLocalServiceImpl;
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import java.math.BigDecimal;
 
@@ -1082,11 +1087,25 @@ public class ExpandoValueConversionTest {
 
 	private final Converter _converter = new Converter();
 
-	private static class Converter extends ExpandoValueLocalServiceImpl {
+	private static class Converter {
 
-		@Override
 		public <T> T convertType(int type, Object data) {
-			return super.convertType(type, data);
+			Method method = ReflectionTestUtil.getMethod(
+				ExpandoValueLocalServiceImpl.class, "_convertType",
+				new Class<?>[] {int.class, Object.class});
+
+			try {
+				data = method.invoke(
+					new ExpandoValueLocalServiceImpl(), type, data);
+			}
+			catch (IllegalAccessException illegalAccessException) {
+			}
+			catch (InvocationTargetException invocationTargetException) {
+				ReflectionUtil.throwException(
+					invocationTargetException.getCause());
+			}
+
+			return (T)data;
 		}
 
 	}
