9afecbcfffab85fda22ce4329daa761e5ecb4d06
Lily.Chi <lily.chi@liferay.com>
2023-09-18 17:12:04 +0800

    LPS-190836 Move ExpandoColumnPermissionUtil from portal-kernel to expando-api and apply the new api of Expando

--- modules/apps/expando/expando-api/src/main/java/com/liferay/expando/service/permission/ExpandoColumnPermissionUtil.java
+++ modules/apps/expando/expando-api/src/main/java/com/liferay/expando/service/permission/ExpandoColumnPermissionUtil.java
@@ -1,14 +1,14 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.expando.kernel.service.permission;
+package com.liferay.expando.service.permission;
 
-import com.liferay.expando.kernel.exception.NoSuchColumnException;
-import com.liferay.expando.kernel.model.ExpandoColumn;
+import com.liferay.expando.exception.NoSuchColumnException;
 import com.liferay.expando.kernel.model.ExpandoTableConstants;
-import com.liferay.expando.kernel.service.ExpandoColumnLocalServiceUtil;
+import com.liferay.expando.model.ExpandoColumn;
+import com.liferay.expando.service.ExpandoColumnLocalServiceUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
