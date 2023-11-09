4bc6a2e3858d23959d45cfb6505c3d4fe44dfa8d
Lily.Chi <lily.chi@liferay.com>
2023-11-09 14:30:37 +0800

    LPS-X Apply to SubscriptionModel to make the users more even

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -5586,17 +5586,20 @@ public class DataFactory {
 		BlogsEntryModel blogsEntryModel) {
 
 		return newSubscriptionModel(
-			getClassNameId(BlogsEntry.class), blogsEntryModel.getEntryId());
+			getClassNameId(BlogsEntry.class), blogsEntryModel.getEntryId(),
+			blogsEntryModel.getUserId(), blogsEntryModel.getUserName());
 	}
 
 	public SubscriptionModel newSubscriptionModel(MBThreadModel mBThreadModel) {
 		return newSubscriptionModel(
-			getClassNameId(MBThread.class), mBThreadModel.getThreadId());
+			getClassNameId(MBThread.class), mBThreadModel.getThreadId(),
+			mBThreadModel.getUserId(), mBThreadModel.getUserName());
 	}
 
 	public SubscriptionModel newSubscriptionModel(WikiPageModel wikiPageModel) {
 		return newSubscriptionModel(
-			getClassNameId(WikiPage.class), wikiPageModel.getResourcePrimKey());
+			getClassNameId(WikiPage.class), wikiPageModel.getResourcePrimKey(),
+			wikiPageModel.getUserId(), wikiPageModel.getUserName());
 	}
 
 	public List<UserModel> newUserModels() {
@@ -6927,7 +6930,7 @@ public class DataFactory {
 	}
 
 	protected SubscriptionModel newSubscriptionModel(
-		long classNameId, long classPK) {
+		long classNameId, long classPK, long userId, String userName) {
 
 		SubscriptionModel subscriptionModel = new SubscriptionModelImpl();
 
@@ -6938,8 +6941,8 @@ public class DataFactory {
 		// Audit fields
 
 		subscriptionModel.setCompanyId(_companyId);
-		subscriptionModel.setUserId(_sampleUserId);
-		subscriptionModel.setUserName(_SAMPLE_USER_NAME);
+		subscriptionModel.setUserId(userId);
+		subscriptionModel.setUserName(userName);
 		subscriptionModel.setCreateDate(new Date());
 		subscriptionModel.setModifiedDate(new Date());
 
