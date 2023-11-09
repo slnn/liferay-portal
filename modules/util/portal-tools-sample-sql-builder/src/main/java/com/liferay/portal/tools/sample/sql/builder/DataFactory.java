6729f1ba669bdedc8fe16cd1716e665ca370e593
Lily.Chi <lily.chi@liferay.com>
2023-11-09 16:58:47 +0800

    LPS-X Use MBThreadModel's data to set User for MBThreadFlagModel

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4870,8 +4870,8 @@ public class DataFactory {
 		// Audit fields
 
 		mbThreadFlagModel.setCompanyId(_companyId);
-		mbThreadFlagModel.setUserId(_sampleUserId);
-		mbThreadFlagModel.setUserName(_SAMPLE_USER_NAME);
+		mbThreadFlagModel.setUserId(mbThreadModel.getUserId());
+		mbThreadFlagModel.setUserName(mbThreadModel.getUserName());
 		mbThreadFlagModel.setCreateDate(new Date());
 		mbThreadFlagModel.setModifiedDate(new Date());
 
