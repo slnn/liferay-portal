2b29d9d77dddb755fff605ab228348905ca98bca
Lily.Chi <lily.chi@liferay.com>
2023-11-15 14:19:34 +0800

    LPS-X Move class DefaultConfigurationAction to portal-impl

--- portal-impl/src/com/liferay/portal/portlet/DefaultConfigurationAction.java
+++ portal-impl/src/com/liferay/portal/portlet/DefaultConfigurationAction.java
@@ -3,10 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.portlet;
+package com.liferay.portal.portlet;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.ResourceServingConfigurationAction;
 import com.liferay.portal.kernel.settings.PortletPreferencesSettings;
 import com.liferay.portal.kernel.settings.Settings;
 import com.liferay.portal.kernel.util.StringUtil;
