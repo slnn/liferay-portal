a08d6639503759b41ecd6f4066924aed0ea22682
Lily.Chi <lily.chi@liferay.com>
2023-11-15 14:05:06 +0800

    LPS-X Move SettingsConfigurationAction to portal-impl

--- portal-impl/src/com/liferay/portal/portlet/SettingsConfigurationAction.java
+++ portal-impl/src/com/liferay/portal/portlet/SettingsConfigurationAction.java
@@ -3,11 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.portlet;
+package com.liferay.portal.portlet;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.LiferayPortlet;
+import com.liferay.portal.kernel.portlet.PortletConfigFactoryUtil;
+import com.liferay.portal.kernel.portlet.PortletConfigurationLayoutUtil;
+import com.liferay.portal.kernel.portlet.ResourceServingConfigurationAction;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
