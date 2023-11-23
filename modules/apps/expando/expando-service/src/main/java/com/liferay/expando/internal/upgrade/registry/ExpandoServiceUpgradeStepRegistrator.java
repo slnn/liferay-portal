e195124692832031413e8d67ea8c8981d0655305
Lily.Chi <lily.chi@liferay.com>
2023-11-23 10:21:43 +0800

    LPS-190836 Add a UpgradeStepRegistrator for expando servce module and register a DummyUpgradeStep which aims to trigger the updateIndexes process in UpgradeExecutor and  update the schema version from 0.0.1 to 1.0.0 as ReleaseDAO.addRelease() will add an item with schema version 0.0.1 in Release_ table, however, 0.0.1 means the reserved schema version for non upgrade modules, and the upgrade process will be stopped with incorrect status even all the upgrades have been executed successfully

---
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/internal/upgrade/registry/ExpandoServiceUpgradeStepRegistrator.java
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.expando.internal.upgrade.registry;
+
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = UpgradeStepRegistrator.class)
+public class ExpandoServiceUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("0.0.1", "1.0.0", new DummyUpgradeStep());
+	}
+
+}
