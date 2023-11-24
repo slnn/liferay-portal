4361c7eec80f028869c53be10f828be20beef829
Lily.Chi <lily.chi@liferay.com>
2023-11-24 20:38:05 +0800

    LPS-X We should set privateLayout = true in the search page's setTypeSettings even its privateLayout is false to make it consistent with portal's action

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -6594,9 +6594,14 @@ public class DataFactory {
 			}
 		}
 
-		if (privateLayout) {
-			typeSettingsUnicodeProperties.setProperty(
-				"privateLayout", String.valueOf(privateLayout));
+		if (name.equals("search")) {
+			typeSettingsUnicodeProperties.setProperty("privateLayout", "true");
+		}
+		else {
+			if (privateLayout) {
+				typeSettingsUnicodeProperties.setProperty(
+					"privateLayout", String.valueOf(privateLayout));
+			}
 		}
 
 		layoutModel.setTypeSettings(
