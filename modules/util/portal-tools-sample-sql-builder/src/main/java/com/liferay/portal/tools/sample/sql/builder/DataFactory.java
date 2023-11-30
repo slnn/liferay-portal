09f623270382ae73a068c66250656e367189b322
Lily.Chi <lily.chi@liferay.com>
2023-11-30 16:20:08 +0800

    LPS-203325 SF, add comments according to the service.xml make it more consistent to other models in DataFactory

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -6881,8 +6881,13 @@ public class DataFactory {
 		PortletPreferenceValueModel portletPreferenceValueModel =
 			new PortletPreferenceValueModelImpl();
 
+		// PK fields
+
 		portletPreferenceValueModel.setPortletPreferenceValueId(
 			_portletPreferenceValueIdCounter.get());
+
+		// Other fields
+
 		portletPreferenceValueModel.setPortletPreferencesId(
 			portletPreferencesModel.getPortletPreferencesId());
 		portletPreferenceValueModel.setName(name);
