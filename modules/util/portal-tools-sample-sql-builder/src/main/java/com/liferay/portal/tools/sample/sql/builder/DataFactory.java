bea3cd466bdf6f98ef3b123c4034b5f93ac26cd0
Lily.Chi <lily.chi@liferay.com>
2024-01-02 23:14:46 +0800

    LPS-203325 Generate missing PortletPreferenceValue for the search page

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -5054,6 +5054,17 @@ public class DataFactory {
 		return portletPreferencesModels;
 	}
 
+	public List<PortletPreferenceValueModel> newPortletPreferenceValueModels(
+		PortletPreferencesModel portletPreferencesModel) {
+
+		return ListUtil.fromArray(
+			newPortletPreferenceValueModel(
+				0, "destination", portletPreferencesModel, "/search"),
+			newPortletPreferenceValueModel(
+				0, "portletSetupPortletDecoratorId", portletPreferencesModel,
+				"barebone"));
+	}
+
 	public List<ReleaseModel> newReleaseModels() throws Exception {
 		List<ReleaseModel> releases = new ArrayList<>();
 
