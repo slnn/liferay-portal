e359c573c6090607686766779a9f5f13cdb7e3cb
Lily.Chi <lily.chi@liferay.com>
2024-11-24 22:02:51 +0800

    LPD-33643 Extract the HttpUtil and HttpResponse to a new module, prep next

--- modules/util/portal-test-http-util/src/main/java/com/liferay/portal/test/http/util/HttpUtil.java
+++ modules/util/portal-test-http-util/src/main/java/com/liferay/portal/test/http/util/HttpUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.tools.benchmarks.http;
+package com.liferay.portal.test.http.util;
 
 import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.lang.CentralizedThreadLocal;
