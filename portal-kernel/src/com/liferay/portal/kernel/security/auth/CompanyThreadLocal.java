e2152a78918672d17d17948997495473a31dffeb
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-01-05 01:49:38 +0100

    LPD-45295 This was the original behavior, do the safeClosable first to be able to restore the value and remove later to make the initial value set lazy when the get method is called

--- portal-kernel/src/com/liferay/portal/kernel/security/auth/CompanyThreadLocal.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/auth/CompanyThreadLocal.java
@@ -224,6 +224,8 @@ public class CompanyThreadLocal {
 
 				safeCloseables.add(
 					companyCentralizedThreadLocal.setWithSafeCloseable(null));
+
+				companyCentralizedThreadLocal.remove();
 			}
 		}
 
