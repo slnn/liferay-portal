49ad8de109d5d29b06c96dd7b7b5bc8919c498d7
Lily.Chi <lily.chi@liferay.com>
2025-02-13 16:25:44 +0800

    LPD-44229 | LPD-48289 Sort, according to the order of service.xml

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -7677,7 +7677,6 @@ public class DataFactory {
 		// PK fields
 
 		fragmentEntryLinkModel.setFragmentEntryLinkId(_counter.get());
-		fragmentEntryLinkModel.setSegmentsExperienceId(segmentsExperienceId);
 
 		// Group instance
 
@@ -7694,17 +7693,19 @@ public class DataFactory {
 		// Other fields
 
 		fragmentEntryLinkModel.setFragmentEntryId(0);
+		fragmentEntryLinkModel.setSegmentsExperienceId(segmentsExperienceId);
 		fragmentEntryLinkModel.setClassNameId(getClassNameId(Layout.class));
 		fragmentEntryLinkModel.setClassPK(layoutModel.getPlid());
+		fragmentEntryLinkModel.setPlid(layoutModel.getPlid());
 		fragmentEntryLinkModel.setCss(
 			_readFile(_getFragmentComponentInputStream("heading", "css")));
 		fragmentEntryLinkModel.setHtml(
 			_readFile(_getFragmentComponentInputStream("heading", "html")));
+		fragmentEntryLinkModel.setJs(StringPool.BLANK);
 		fragmentEntryLinkModel.setConfiguration(
 			_readFile(
 				"fragment_component/fragment_component_heading_configuration." +
 					"json"));
-		fragmentEntryLinkModel.setJs(StringPool.BLANK);
 
 		String editValue = _readFile(
 			"fragment_component/fragment_component_heading_editValue_" +
@@ -7721,7 +7722,6 @@ public class DataFactory {
 				"${journalArticleTitle}", journalArticleModel.getUrlTitle()));
 
 		fragmentEntryLinkModel.setNamespace(StringUtil.randomId());
-		fragmentEntryLinkModel.setPlid(layoutModel.getPlid());
 		fragmentEntryLinkModel.setPosition(0);
 		fragmentEntryLinkModel.setRendererKey(
 			_FRAGMENT_COMPONENT_RENDER_KEY_HEADING);
