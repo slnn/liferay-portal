dca71c4a73352b522e8616b71dd21a84890f595d
shuyangzhou <shuyang.zhou@liferay.com>
2025-11-06 09:02:38 -0800

    LPD-X Avoid unnecessary NoSuch*Exception

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/WorkflowedModelDocumentContributor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/index/contributor/WorkflowedModelDocumentContributor.java
@@ -6,9 +6,6 @@
 package com.liferay.portal.search.internal.spi.model.index.contributor;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.WorkflowedModel;
@@ -51,23 +48,17 @@ public class WorkflowedModelDocumentContributor
 	private String _getUserExternalReferenceCode(long userId) {
 		String userExternalReferenceCode = StringPool.BLANK;
 
-		try {
-			User user = userLocalService.getUser(userId);
-
-			userExternalReferenceCode = user.getExternalReferenceCode();
+		if (userId == 0) {
+			return userExternalReferenceCode;
 		}
-		catch (PortalException portalException) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to get user " + userId + " while indexing document",
-					portalException);
-			}
+
+		User user = userLocalService.fetchUser(userId);
+
+		if (user != null) {
+			userExternalReferenceCode = user.getExternalReferenceCode();
 		}
 
 		return userExternalReferenceCode;
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		WorkflowedModelDocumentContributor.class);
 
 }
