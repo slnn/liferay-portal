f1ca17cbf6aa9c14c87a4f3f46dd0ed3845a8ed4
shuyangzhou <shuyang.zhou@liferay.com>
2025-11-08 15:56:29 -0800

    LPD-X Remove deadcode added by 67ce6e11

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
@@ -377,8 +377,6 @@ public class ObjectEntryModelDocumentContributor
 		document.add(
 			new Field("objectEntryContent", objectContentHelper.getContent()));
 
-		objectContentHelper.getLocalizedContentMap();
-
 		document.addKeyword("objectEntryId", objectEntry.getObjectEntryId());
 		document.add(
 			new Field("objectEntryTitle", objectEntry.getTitleValue()));
