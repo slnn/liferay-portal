38a2fca82428496d2ec3f67367491d466a858018
Lily.Chi <lily.chi@liferay.com>
2026-02-06 23:24:37 +0800

    LPD-X

--- portal-kernel/src/com/liferay/portal/kernel/display/context/BaseDisplayContextProvider.java
+++ portal-kernel/src/com/liferay/portal/kernel/display/context/BaseDisplayContextProvider.java
@@ -5,138 +5,33 @@
 
 package com.liferay.portal.kernel.display.context;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
-
-import java.util.Iterator;
-import java.util.SortedSet;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ConcurrentSkipListSet;
-
+import java.util.Collection;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Iván Zaera
  */
 public class BaseDisplayContextProvider<T extends DisplayContextFactory>
-	implements DisplayContextProvider {
+		implements DisplayContextProvider {
 
 	public BaseDisplayContextProvider(Class<T> displayContextFactoryClass) {
-		_serviceTracker = new ServiceTracker<>(
-			_bundleContext,
-			SystemBundleUtil.createFilter(
-				"(objectClass=" + displayContextFactoryClass.getName() + ")"),
-			new DisplayContextFactoryServiceTrackerCustomizer());
-
-		_serviceTracker.open();
+		// We track by class name as the key, or a specific property
+		_serviceTrackerMap = ServiceTrackerMapFactory.open(
+				_bundleContext, displayContextFactoryClass, null);
 	}
 
 	public void destroy() {
-		_serviceTracker.close();
+		_serviceTrackerMap.close();
 	}
 
 	public Iterable<T> getDisplayContextFactories() {
-		return new DisplayContextFactoriesIterable<>(
-			_displayContextFactoryReferences);
+		// ServiceTrackerMap.values() returns a Collection of the services
+		return _serviceTrackerMap.values();
 	}
 
-	private final BundleContext _bundleContext =
-		SystemBundleUtil.getBundleContext();
-	private final SortedSet<DisplayContextFactoryReference<T>>
-		_displayContextFactoryReferences = new ConcurrentSkipListSet<>();
-	private final ConcurrentMap<T, DisplayContextFactoryReference<T>>
-		_displayContextFactoryReferencesMap = new ConcurrentHashMap<>();
-	private final ServiceTracker<T, T> _serviceTracker;
-
-	private static class DisplayContextFactoriesIterable
-		<T extends DisplayContextFactory>
-			implements Iterable<T>, Iterator<T> {
-
-		public DisplayContextFactoriesIterable(
-			Iterable<DisplayContextFactoryReference<T>> iterable) {
-
-			_iterator = iterable.iterator();
-		}
-
-		@Override
-		public boolean hasNext() {
-			return _iterator.hasNext();
-		}
-
-		@Override
-		public Iterator<T> iterator() {
-			return this;
-		}
-
-		@Override
-		public T next() {
-			DisplayContextFactoryReference<T> displayContextFactoryReference =
-				_iterator.next();
-
-			return displayContextFactoryReference.getDisplayContextFactory();
-		}
-
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException("Iterator is read-only");
-		}
-
-		private final Iterator<DisplayContextFactoryReference<T>> _iterator;
-
-	}
-
-	private class DisplayContextFactoryServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer<T, T> {
-
-		@Override
-		public T addingService(ServiceReference<T> serviceReference) {
-			T displayContextFactory = _bundleContext.getService(
-				serviceReference);
-
-			DisplayContextFactoryReference<T> displayContextFactoryReference =
-				new DisplayContextFactoryReference<>(
-					displayContextFactory, serviceReference);
-
-			_displayContextFactoryReferences.add(
-				displayContextFactoryReference);
-
-			_displayContextFactoryReferencesMap.put(
-				displayContextFactory, displayContextFactoryReference);
-
-			return displayContextFactory;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<T> serviceReference, T displayContextFactory) {
-
-			DisplayContextFactoryReference<T> displayContextFactoryReference =
-				_displayContextFactoryReferencesMap.get(displayContextFactory);
-
-			removedService(
-				displayContextFactoryReference.getServiceReference(),
-				displayContextFactoryReference.getDisplayContextFactory());
-
-			addingService(serviceReference);
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<T> serviceReference, T displayContextFactory) {
-
-			DisplayContextFactoryReference<T> displayContextFactoryReference =
-				_displayContextFactoryReferencesMap.remove(
-					displayContextFactory);
-
-			_displayContextFactoryReferences.remove(
-				displayContextFactoryReference);
-
-			_bundleContext.ungetService(serviceReference);
-		}
-
-	}
-
+	private final BundleContext _bundleContext = SystemBundleUtil.getBundleContext();
+	private final ServiceTrackerMap<String, T> _serviceTrackerMap;
 }
