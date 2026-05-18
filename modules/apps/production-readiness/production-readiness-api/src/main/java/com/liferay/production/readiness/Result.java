454b176688d487567c93c3f70610d4df5f794741
Lily.Chi <lily.chi@liferay.com>
2026-05-18 14:22:19 +0900

    LPD-87223 Fix Result.java formatting

--- modules/apps/production-readiness/production-readiness-api/src/main/java/com/liferay/production/readiness/Result.java
+++ modules/apps/production-readiness/production-readiness-api/src/main/java/com/liferay/production/readiness/Result.java
@@ -2,24 +2,13 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
- 
-package com.liferay.production.readiness;
 
-import org.osgi.annotation.versioning.ProviderType;
+package com.liferay.production.readiness;
 
 /**
  * @author Lily Chi
  */
-@ProviderType
 public class Result {
-
-	public enum Status {
-		FAIL, PASS
-	}
-
-	public enum Severity {
-		CRITICAL, HIGH, LOW, MEDIUM
-	}
 
 	public Result(
 		Status status, Severity severity, String category, String currentValue,
@@ -45,9 +34,10 @@ public class Result {
 	}
 
 	public String getDocsLink() {
-		if(_docsLink == null){
+		if (_docsLink == null) {
 			return _DEFAULT_DOCS_LINK;
 		}
+
 		return _docsLink;
 	}
 
@@ -71,6 +61,23 @@ public class Result {
 		return _status;
 	}
 
+	public enum Severity {
+
+		CRITICAL, HIGH, LOW, MEDIUM
+
+	}
+
+	public enum Status {
+
+		FAIL, PASS
+
+	}
+
+	private static final String _DEFAULT_DOCS_LINK =
+		"https://www.liferay.com/documents/10182/3292406/Liferay+DXP+7." +
+			"4+Deployment+Checklist.pdf/f3464a36-c0f0-6708-37dd-efe7b8270403?" +
+				"t=1643744619710";
+
 	private final String _category;
 	private final String _currentValue;
 	private final String _docsLink;
@@ -79,8 +86,5 @@ public class Result {
 	private final String _recommendedValue;
 	private final Severity _severity;
 	private final Status _status;
-
-	private static final String _DEFAULT_DOCS_LINK =
-			"https://www.liferay.com/documents/10182/3292406/Liferay+DXP+7.4+Deployment+Checklist.pdf/f3464a36-c0f0-6708-37dd-efe7b8270403?t=1643744619710";
 
 }
